fb52de1ec4836db0d34a54fb3f0ba06ac7ffbfd3
BzZz <adelheid.kaszubi@gmail.com>
2023-11-24 22:03:21 +0100

    Create bzzz_food_xmaspack.lua
    https://bzzz.tebex.io/package/5413980

---
+++ bzzz_usableitems/bzzz_food_xmaspack.lua
@@ -0,0 +1,131 @@
+-- add to bzzz_usableitems V2		
+		
+		-------  FOOD  -------
+		["gingerbread"] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 18905, -- You can find BoneIndex at https://wiki.gtanet.work/index.php?title=Bones
+                propName = "bzzz_food_xmas_gingerbread_a", -- You can find props at https://forge.plebmasters.de/objects
+                xPos = 0.16, yPos = 0.04, zPos = 0.03, xRot = 18.0, yRot = 164.0, zRot = -5.0
+            }
+        },
+		["xlollipop_a"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_food_xmas_lollipop_a", 
+                xPos = 0.16, yPos = 0.02, zPos = 0.03, xRot = -73.0, yRot = 146.0, zRot = -5.0
+            }
+        },
+		["xlollipop_b"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_food_xmas_lollipop_b", 
+                xPos = 0.16, yPos = 0.02, zPos = 0.03, xRot = -73.0, yRot = 146.0, zRot = -5.0
+            }
+        },
+		["xlollipop_c"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_food_xmas_lollipop_c", 
+                xPos = 0.16, yPos = 0.02, zPos = 0.03, xRot = -73.0, yRot = 146.0, zRot = -5.0
+            }
+        },
+		["xlollipop_d"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_food_xmas_lollipop_d", 
+                xPos = 0.16, yPos = 0.02, zPos = 0.03, xRot = -73.0, yRot = 146.0, zRot = -5.0
+            }
+        },
+		["xlollipop_e"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_food_xmas_lollipop_e", 
+                xPos = 0.16, yPos = 0.02, zPos = 0.03, xRot = -73.0, yRot = 146.0, zRot = -5.0
+            }
+        },
+		["macaroon"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_food_xmas_macaroon_a", 
+                xPos = 0.15, yPos = 0.07, zPos = 0.00, xRot = -38.0, yRot = -7.0, zRot = 7.0
+            }
+        },
+		
+		
+		
+		
+		
+		
+		-------  DRINK  -------
+		
+		["mug_a"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_intdrink",
+                animationName = "loop_bottle"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_food_xmas_mug_a", 
+                xPos = 0.09, yPos = -0.01, zPos = 0.08, xRot = -44.0, yRot = 137.0, zRot = 9.0
+            }
+        },
+		["mug_b"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_intdrink",
+                animationName = "loop_bottle"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_food_xmas_mug_b", 
+                xPos = 0.09, yPos = -0.01, zPos = 0.08, xRot = -44.0, yRot = 137.0, zRot = 9.0
+            },
+		 },
+		["mulled_wine"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_intdrink",
+                animationName = "loop_bottle"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_food_xmas_mulled_wine_a", 
+                xPos = 0.13, yPos = 0.03, zPos = 0.05, xRot = -110.0, yRot = -47.0, zRot = 7.0
+            }
+        },
