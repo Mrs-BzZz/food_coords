cc0c8974639275cb0ca1a67249aa12bdbae1e3a6
BzZz <adelheid.kaszubi@gmail.com>
2023-11-24 22:02:46 +0100

    Create bzzz_food_xmaspack.lua
    https://bzzz.tebex.io/package/5413980

---
+++ ox_inventory/bzzz_food_xmaspack.lua
@@ -0,0 +1,176 @@
+---------------------------------------bzzz_food_xmaspack----------------------------------	
+	
+	['gingerbread'] = {
+        label = 'Gingerbread',
+		weight = 250,
+		stack = true,
+        close = true,
+		description = "something sweet",
+		client = {
+			status = { hunger = 25000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+            prop = { model = `bzzz_food_xmas_gingerbread_a`, pos = vec3(0.02, 0.00, -0.01), rot = vec3(362.00, -8.00, 432.00) },
+			bone = 60309,
+			usetime = 5500,
+			notification = 'MHH tasty'
+		},
+	},
+	
+	['xlollipop_a'] = {
+        label = 'Xmas Lollipop Red',
+        weight = 250,
+		stack = true,
+        close = true,
+		description = "something sweet",
+		client = {
+			status = { hunger = 5000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = 'bzzz_food_xmas_lollipop_a',
+			pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0) },
+			bone = 60309,
+			usetime = 8500,
+			notification = 'MHH tasty'
+		},
+	},
+
+	['xlollipop_b'] = {
+        label = 'Xmas Lollipop Green',
+        weight = 250,
+		stack = true,
+        close = true,
+		description = "something sweet",
+		client = {
+			status = { hunger = 5000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = 'bzzz_food_xmas_lollipop_b',
+			pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0) },
+			bone = 60309,
+			usetime = 8500,
+			notification = 'MHH tasty'
+		},
+	},
+	['xlollipop_c'] = {
+        label = 'Xmas Lollipop Purple',
+        weight = 250,
+		stack = true,
+        close = true,
+		description = "something sweet",
+		client = {
+			status = { hunger = 5000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = 'bzzz_food_xmas_lollipop_c',
+			pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0) },
+			bone = 60309,
+			usetime = 8500,
+			notification = 'MHH tasty'
+		},
+	},
+
+	['xlollipop_d'] = {
+        label = 'Xmas Lollipop Yellow',
+        weight = 250,
+		stack = true,
+        close = true,
+		description = "something sweet",
+		client = {
+			status = { hunger = 5000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = 'bzzz_food_xmas_lollipop_d',
+			pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0) },
+			bone = 60309,
+			usetime = 8500,
+			notification = 'MHH tasty'
+		},
+	},
+	['xlollipop_e'] = {
+        label = 'Xmas Lollipop Blue',
+        weight = 250,
+		stack = true,
+        close = true,
+		description = "something sweet",
+		client = {
+			status = { hunger = 5000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = 'bzzz_food_xmas_lollipop_e',
+			pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0) },
+			bone = 60309,
+			usetime = 8500,
+			notification = 'MHH tasty'
+		},
+	},
+	
+	['macaroon'] = {
+        label = 'Macaroon',
+        weight = 250,
+		stack = true,
+        close = true,
+		description = "something sweet",
+		client = {
+			status = { hunger = 25000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = 'bzzz_food_xmas_macaroon_a',
+			pos = vec3(0.00, 0.00, 0.00), rot = vec3(0.00, 0.00, 0.00) },
+			bone = 60309,
+			usetime = 4500,
+			notification = 'MHH tasty'
+		},
+	},
+	
+	['mug_a'] = {
+        label = 'Coffee Cup',
+        weight = 250,
+		stack = true,
+        close = true,
+		--description = "something sweet",
+		client = {
+			status = { thirst = 20000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = 'bzzz_food_xmas_mug_a',
+			pos = vec3(-0.01, 0.02, 0.00), rot = vec3(0.00, 0.0, 0.0) },
+			bone = 60309,
+			usetime = 23500,
+			notification = 'MHH tasty'
+		},
+	},
+	
+	['mug_b'] = {
+        label = 'Tea Cup',
+        weight = 250,
+		stack = true,
+        close = true,
+		--description = "something sweet",
+		client = {
+			status = { thirst = 20000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = 'bzzz_food_xmas_mug_b',
+			pos = vec3(0.00, 0.01, 0.00), rot = vec3(0.0, 0.0, 0.0) },
+			bone = 60309,
+			usetime = 23500,
+			notification = 'MHH tasty'
+		},
+	},
+
+	['mulled_wine'] = {
+        label = 'Mulled Wine',
+        weight = 250,
+		stack = true,
+        close = true,
+		--description = "something sweet",
+		client = {
+			status = { thirst = 20000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = 'bzzz_food_xmas_mug_b',
+			pos = vec3(0.00, 0.01, 0.00), rot = vec3(0.0, 0.0, 0.0) },
+			bone = 60309,
+			usetime = 23500,
+			notification = 'MHH tasty'
+		},
+	},
+	
+	
+	--------------------------------- E N D E ------------------------------------------------------
+	
+	
+	
+
+	
