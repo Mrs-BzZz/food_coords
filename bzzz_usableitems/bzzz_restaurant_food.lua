b41eb1eab1cc26615d604fb5e0b4bd96587bbacf
BzZz <adelheid.kaszubi@gmail.com>
2023-11-24 22:07:12 +0100

    Create bzzz_restaurant_food.lua
    https://bzzz.tebex.io/package/5612432

---
+++ bzzz_usableitems/bzzz_restaurant_food.lua
@@ -0,0 +1,237 @@
+	-- insert bzzz_usableitems into Config.Items 
+    -- "res_food_a" already exists in the config
+	
+	
+		
+	
+	FoodPlates = { -- Food which uses special animation for eating, like when you eating from plate or box :D chewing is included too
+        ['res_food_a'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_restaurant_food_a", 
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_restaurant_fork_a", 
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+        ['res_food_b'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_restaurant_food_b", 
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_restaurant_fork_b", 
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+        ['res_food_c'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_restaurant_food_c", 
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_restaurant_fork_c", 
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+        ['res_food_d'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_restaurant_food_d", 
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005,
+                propName = "bzzz_restaurant_fork_d", 
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+        ['res_food_e'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_restaurant_food_e", 
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_restaurant_fork_e", 
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+        ['res_food_f'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_restaurant_food_f", 
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_restaurant_fork_a", 
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+        ['res_food_g'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905,
+                propName = "bzzz_restaurant_food_g",
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_restaurant_fork_d", 
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+        ['res_food_h'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_restaurant_food_h", 
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_restaurant_fork_f", 
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+        ['res_food_i'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_restaurant_food_i",
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_restaurant_fork_h", 
+                xPos = 0.11, yPos = 0.05, zPos = -0.02, xRot = 78.0, yRot = 150.0, zRot = 188.0
+            }
+        },
+        ['res_food_j'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_restaurant_food_j",
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_restaurant_material_shrimp_a",
+                xPos = 0.14, yPos = 0.06, zPos = -0.05, xRot = 24.0, yRot = 282.0, zRot = 176.0
+            }
+        },
+        ['res_food_k'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905,
+                propName = "bzzz_restaurant_food_k",
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005,
+                propName = "bzzz_restaurant_fork_e",
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+        ['res_food_l'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@restaurant@eat",
+                animationNameEat = "bzzz_restaurant_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905,
+                propName = "bzzz_restaurant_food_l",
+                xPos = 0.08, yPos = -0.04, zPos = 0.07, xRot = -30.0, yRot = 10.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 57005,
+                propName = "bzzz_restaurant_fork_g",
+                xPos = 0.1, yPos = 0.05, zPos = 0.0, xRot = -117.0, yRot = -68.0, zRot = -43.0
+            }
+        },
+
+    },
