3d1d922e4d782335b2354f25cdd1bb93bb2430e7
BzZz <adelheid.kaszubi@gmail.com>
2023-11-24 22:05:02 +0100

    Create bzzz_juices.lua
    https://bzzz.tebex.io/package/5470219

---
+++ bzzz_usableitems/bzzz_juices.lua
@@ -0,0 +1,298 @@
+--------- DRINK ---------
+["glass_strawberry"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_strawberry", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_plum"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_plum", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_pineapple"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_pineapple", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_pear"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_pear", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_peach"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_peach", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_orange"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_orange", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_kiwi"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_kiwi", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_grapefruit"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_grapefruit", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_currant"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_currant", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_cherry"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_cherry", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_banana"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_banana", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+["glass_apple"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink",
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_glass_apple", 
+        xPos = -0.02, yPos = -0.04, zPos = 0.1, xRot = 34.0, yRot = 140.0, zRot = 224.0
+    }
+},
+
+
+
+
+
+
+
+---------------------------------------------
+--------- FOOD ---------
+["fruit_strawberry"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_strawberry", 
+        xPos = 0.17, yPos = 0.06, zPos = 0.01, xRot = -126.0, yRot = -79.0, zRot = 20.0
+    }
+},
+["fruit_plum"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_plum", 
+        xPos = 0.15, yPos = 0.06, zPos = 0.01, xRot = -126.0, yRot = -135.0, zRot = -71.0
+    }
+},
+["fruit_pineapple"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_pineapple", 
+        xPos = 0.16, yPos = 0.1, zPos = -0.02, xRot = -141.0, yRot = -190.0, zRot = -161.0
+    }
+},
+["fruit_pear"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_pear", 
+        xPos = 0.14, yPos = 0.07, zPos = 0.03, xRot = -236.0, yRot = -128.0, zRot = -161.0
+    }
+},
+["fruit_peach"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_peach", 
+        xPos = 0.15, yPos = 0.07, zPos = 0.00, xRot = -333.0, yRot = -128.0, zRot = -161.0
+    }
+},
+["fruit_orange"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_orange", 
+        xPos = 0.15, yPos = 0.07, zPos = 0.00, xRot = -258.0, yRot = -118.0, zRot = -126.0
+    }
+},
+["fruit_kiwi"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_kiwi", 
+        xPos = 0.15, yPos = 0.07, zPos = 0.00, xRot = -258.0, yRot = -118.0, zRot = -126.0
+    }
+},
+["fruit_grapefruit"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_grapefruit", 
+        xPos = 0.15, yPos = 0.07, zPos = 0.00, xRot = -258.0, yRot = -118.0, zRot = -126.0
+    }
+},
+["fruit_currant"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_currant", 
+        xPos = 0.17, yPos = 0.05, zPos = 0.03, xRot = -335.0, yRot = -136.0, zRot = -165.0
+    }
+},
+["fruit_cherry"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_cherry", 
+        xPos = 0.15, yPos = 0.11, zPos = -0.02, xRot = -76.0, yRot = -38.0, zRot = -179.0
+    }
+},
+["fruit_banana"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_banana", 
+        xPos = 0.15, yPos = 0.08, zPos = 0.00, xRot = -127.0, yRot = -81.0, zRot = 17.0
+    }
+},
+["fruit_apple"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_drink_juice_fruit_apple", 
+        xPos = 0.15, yPos = 0.08, zPos = 0.00, xRot = -127.0, yRot = -39.0, zRot = 87.0
+    }
+},		
