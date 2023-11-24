6f291db864d95e44a3fc6a2659d9b1e8dbb677f7
BzZz <adelheid.kaszubi@gmail.com>
2023-11-24 22:04:15 +0100

    Create bzzz_icecream.lua
    https://bzzz.tebex.io/package/5377344

---
+++ ox_inventory/bzzz_icecream.lua
@@ -0,0 +1,157 @@
+---------------------------------------bzzz_icecream----------------------------------
+
+
+['icecream_chr'] = { -- brain cold go brrrrr
+        label = 'Ice cream cherry',
+		degrade = 1440,-- 24 stunden
+        weight = 200,
+        stack = true,
+        close = true,
+        description = "cool ice cream cherry",
+        client = {
+            status = { hunger = 25000 },
+            anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+            prop = { model = 'bzzz_icecream_cherry',
+            bone = 18905,
+            pos = vec3(0.14, 0.03, 0.01),
+            rot = vec3(85.0, 70.0, -203.0) },
+            usetime = 7500,
+			notification = 'Fantastic ice cream'
+
+        },
+    },
+
+    ['icecream_chc'] = { -- brain cold go brrrrr
+        label = 'Ice cream chocolate',
+		degrade = 1440,-- 24 stunden
+        weight = 250,
+        stack = true,
+        close = true,
+        description = "cool ice cream chocolate",
+        client = {
+            status = { hunger = 25000 },
+            anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+            prop = { model = 'bzzz_icecream_chocolate',
+            bone = 18905,
+            pos = vec3(0.14, 0.03, 0.01),
+            rot = vec3(85.0, 70.0, -203.0) },
+            usetime = 7500,
+			notification = 'Fantastic ice cream'
+        },
+    },
+
+    ['icecream_lmn'] = { -- brain cold go brrrrr
+        label = 'Ice cream lemon',
+		degrade = 1440,-- 24 stunden
+        weight = 250,
+        stack = true,
+        close = true,
+        description = "cool ice cream lemon",
+        client = {
+            status = { hunger = 25000 },
+            anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+            prop = { model = 'bzzz_icecream_lemon',
+            bone = 18905,
+            pos = vec3(0.14, 0.03, 0.01),
+            rot = vec3(85.0, 70.0, -203.0) },
+            usetime = 7500,
+			notification = 'Fantastic ice cream'
+        },
+    },
+
+    ['icecream_pso'] = { -- brain cold go brrrrr
+        label = 'Ice cream pistachio',
+		degrade = 1440,-- 24 stunden
+        weight = 250,
+        stack = true,
+        close = true,
+        description = "cool ice cream pistachio",
+        client = {
+            status = { hunger = 25000 },
+            anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+            prop = { model = 'bzzz_icecream_pistachio',
+            bone = 18905,
+            pos = vec3(0.14, 0.03, 0.01),
+            rot = vec3(85.0, 70.0, -203.0) },
+            usetime = 7500,
+			notification = 'Fantastic ice cream'
+        },
+    },
+
+    ['icecream_rpy'] = { -- brain cold go brrrrr
+        label = 'Ice cream raspberry',
+		degrade = 1440,-- 24 stunden
+        weight = 250,
+        stack = true,
+        close = true,
+        description = "cool ice cream raspberry",
+        client = {
+            status = { hunger = 25000 },
+            anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+            prop = { model = 'bzzz_icecream_raspberry',
+            bone = 18905,
+            pos = vec3(0.14, 0.03, 0.01),
+            rot = vec3(85.0, 70.0, -203.0) },
+            usetime = 7500,
+			notification = 'Fantastic ice cream'
+        },
+    },
+
+    ['icecream_ckd'] = { -- brain cold go brrrrr
+        label = 'Ice cream stracciatella',
+		degrade = 1440,-- 24 stunden
+        weight = 250,
+        stack = true,
+        close = true,
+        description = "cool ice cream stracciatella",
+        client = {
+            status = { hunger = 25000 },
+            anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+            prop = { model = 'bzzz_icecream_stracciatella',
+            bone = 18905,
+            pos = vec3(0.14, 0.03, 0.01),
+            rot = vec3(85.0, 70.0, -203.0) },
+            usetime = 7500,
+			notification = 'Fantastic ice cream'
+        },
+    },
+
+    ['icecream_sby'] = { -- brain cold go brrrrr
+        label = 'Ice cream strawberry',
+		degrade = 1440,-- 24 stunden
+        weight = 250,
+        stack = true,
+        close = true,
+        description = "cool ice cream strawberry",
+        client = {
+            status = { hunger = 25000 },
+            anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+            prop = { model = 'bzzz_icecream_strawberry',
+            bone = 18905,
+            pos = vec3(0.14, 0.03, 0.01),
+            rot = vec3(85.0, 70.0, -203.0) },
+            usetime = 7500,
+			notification = 'Fantastic ice cream'
+        },
+    },
+
+    ['icecream_vna'] = { -- brain cold go brrrrr
+        label = 'Ice cream walnut',
+		degrade = 1440,-- 24 stunden
+        weight = 250,
+        stack = true,
+        close = true,
+        description = "cool ice cream walnut",
+        client = {
+            status = { hunger = 25000 },
+            anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+            prop = { model = 'bzzz_icecream_walnut',
+            bone = 18905,
+            pos = vec3(0.14, 0.03, 0.01),
+            rot = vec3(85.0, 70.0, -203.0) },
+            usetime = 7500,
+			notification = 'Fantastic ice cream'
+        },
+    },
+	
+	--------------------------------- E N D E ------------------------------------------------------
