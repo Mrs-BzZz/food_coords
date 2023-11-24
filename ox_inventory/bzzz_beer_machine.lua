113aefcfaece5fb6f93d538aa72a89abd83ffe36
BzZz <adelheid.kaszubi@gmail.com>
2023-11-24 21:59:47 +0100

    Create bzzz_beer_machine.lua
    https://bzzz.tebex.io/package/5779962

---
+++ ox_inventory/bzzz_beer_machine.lua
@@ -0,0 +1,96 @@
+---------------------------------------------bzzz_beer_machine----------------------------------------------	
+
+	['beer_original'] = {
+		label = 'Original Beer',
+		weight = 200,
+		stack = true,
+        close = true,
+		description = "Nice cold beer",
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_prop_beer_original`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
+			bone = 60309,
+			usetime = 3500,
+			notification = 'you have quenched your thirst'
+		}
+	},
+
+	['beer_apple'] = {
+		label = 'Apple Beer',
+		weight = 200,
+		stack = true,
+        close = true,
+		description = "Nice cold beer",
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_prop_beer_apple`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
+			bone = 60309,
+			usetime = 3500,
+			notification = 'you have quenched your thirst'
+		}
+	},
+
+	['beer_blueberry'] = {
+		label = 'Blueberry Beer',
+		weight = 200,
+		stack = true,
+        close = true,
+		description = "Nice cold beer",
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_prop_beer_blueberry`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
+			bone = 60309,
+			usetime = 3500,
+			notification = 'you have quenched your thirst'
+		}
+	},
+
+	['beer_kiwi'] = {
+		label = 'Kiwi Beer',
+		weight = 200,
+		stack = true,
+        close = true,
+		description = "Nice cold beer",
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_prop_beer_kiwi`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
+			bone = 60309,
+			usetime = 3500,
+			notification = 'you have quenched your thirst'
+		}
+	},
+
+	['beer_plum'] = {
+		label = 'Plum Beer',
+		weight = 200,
+		stack = true,
+        close = true,
+		description = "Nice cold beer",
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_prop_beer_plum`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
+			bone = 60309,
+			usetime = 3500,
+			notification = 'you have quenched your thirst'
+		}
+	},
+
+	['beer_raspberry'] = {
+		label = 'Raspberry Beer',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_prop_beer_raspberry`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
+			bone = 60309,
+			usetime = 3500,
+			notification = 'you have quenched your thirst'
+		}
+	},	
+	
+	--------------------------------- E N D E ------------------------------------------------------
