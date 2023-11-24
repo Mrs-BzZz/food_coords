c5062153d89d727703a3b02cb3513021cda4114c
BzZz <adelheid.kaszubi@gmail.com>
2023-11-24 21:55:57 +0100

    Create bzzz_bakery.lua
    https://bzzz.tebex.io/package/5509877

---
+++ bzzz_usableitems/bzzz_bakery.lua
@@ -0,0 +1,396 @@
+--- FOOD
+--- Copy and paste into bzzz_usableitems and then restart the server
+
+
+
+
+["toast_d"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_toast_d", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.06, xRot = 46.0, yRot = 152.0, zRot = 4.0
+    }
+},
+
+["toast_b"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_toast_b", 
+        xPos = 0.18, yPos = 0.04, zPos = 0.04, xRot = 28.0, yRot = 152.0, zRot = 6.0
+    }
+},
+
+["toast_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_toast_a", 
+        xPos = 0.18, yPos = 0.04, zPos = 0.04, xRot = 28.0, yRot = 152.0, zRot = 6.0
+    }
+},
+
+["pretzel_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_pretzel_a", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.07, xRot = 36.0, yRot = 113.0, zRot = -3.0
+    }
+},
+
+["muffin_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_muffin_a", 
+        xPos = 0.13, yPos = 0.05, zPos = 0.04, xRot = 57.0, yRot = 149.0, zRot = 183.0
+    }
+},
+
+["lemoncake_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_lemoncake_a", 
+        xPos = 0.17, yPos = 0.07, zPos = 0.06, xRot = -27.0, yRot = 137.0, zRot = 177.0
+    }
+},
+
+["kaiserroll_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_kaiserroll_a", 
+        xPos = 0.14, yPos = 0.08, zPos = 0.03, xRot = -93.0, yRot = 60.0, zRot = 233.0
+    }
+},
+
+["hdroll_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_hdroll_a", 
+        xPos = 0.14, yPos = 0.05, zPos = 0.03, xRot = -93.0, yRot = 60.0, zRot = 233.0
+    }
+},
+
+["hdbuns_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_hdbuns_a", 
+        xPos = 0.14, yPos = 0.05, zPos = 0.03, xRot = -93.0, yRot = 60.0, zRot = 233.0
+    }
+},
+
+["doughnut_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_doughnut_a", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.02, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["donut_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_donut_a", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.02, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["donut_b"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_donut_b", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.02, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["donut_c"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_donut_c", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.02, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["donut_d"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_donut_d", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.02, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["donut_e"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_donut_e", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.02, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["donut_f"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_donut_f", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.02, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["donut_g"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_donut_g", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.02, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["danish_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_danish_a", 
+        xPos = 0.16, yPos = 0.06, zPos = 0.02, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["croissant_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_croissant_a", 
+        xPos = 0.12, yPos = 0.03, zPos = 0.06, xRot = -96.0, yRot = 46.0, zRot = 233.0
+    }
+},
+
+["bread_d"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_bread_d", 
+        xPos = 0.16, yPos = 0.03, zPos = 0.06, xRot = -96.0, yRot = 53.0, zRot = 231.0
+    }
+},
+
+["bread_c"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_bread_c", 
+        xPos = 0.16, yPos = 0.03, zPos = 0.06, xRot = -96.0, yRot = 53.0, zRot = 231.0
+    }
+},
+
+["bread_b"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_bread_b", 
+        xPos = 0.15, yPos = 0.03, zPos = 0.06, xRot = -96.0, yRot = 33.0, zRot = 51.0
+    }
+},
+
+["biscuit_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_biscuit_a", 
+        xPos = 0.16, yPos = 0.08, zPos = 0.00, xRot = 152.0, yRot = -33.0, zRot = 10.0
+    }
+},
+
+["bananabread_c"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_bananabread_c", 
+        xPos = 0.15, yPos = 0.06, zPos = 0.02, xRot = 152.0, yRot = -33.0, zRot = 10.0
+    }
+},
+
+["bananabread_b"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_bananabread_b", 
+        xPos = 0.15, yPos = 0.06, zPos = 0.02, xRot = 152.0, yRot = -33.0, zRot = 10.0
+    }
+},
+
+["bananabread_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_bananabread_a", 
+        xPos = 0.15, yPos = 0.02, zPos = 0.09, xRot = -265.0, yRot = -123.0, zRot = -50.0
+    }
+},
+
+["baguette_c"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_baguette_c", 
+        xPos = 0.1, yPos = -0.01, zPos = 0.05, xRot = -265.0, yRot = -140.0, zRot = -61.0
+    }
+},
+
+["baguette_b"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_baguette_b", 
+        xPos = 0.13, yPos = 0.02, zPos = 0.05, xRot = -269.0, yRot = -124.0, zRot = 155.0
+    }
+},
+
+["baguette_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_baguette_a", 
+        xPos = 0.09, yPos = -0.08, zPos = 0.11, xRot = -269.0, yRot = -124.0, zRot = -55.0
+    }
+},
+
+
+["bagel"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 18905, 
+        propName = "bzzz_food_bakery_bagel_a", 
+        xPos = 0.16, yPos = 0.07, zPos = 0.02, xRot = -21.0, yRot = -57.0, zRot = 0.0
+    }
+},
