b43c95e7a92f4f446f07e3ddd275cef174c0bd0e
BzZz <adelheid.kaszubi@gmail.com>
2024-01-14 23:05:03 +0100

    Create bzzz_fruits.lua

---
+++ ox_inventory/bzzz_fruits.lua
@@ -0,0 +1,113 @@
+-- copy and paste into ox_inventory/data/items.lua
+
+		['avocado_b'] = {
+		label = 'Piece of avocado',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_avocado_b`, bone = 60309, pos = vec3(-0.02, 0.01, 0.0), rot = vec3(30.0, -41.0, 80.0) },
+			usetime = 2500,
+			notification = 'You ate a piece of avocado.'
+		}
+	},
+		['coconut_b'] = {
+		label = 'Piece of coconut',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_coconut_b`, bone = 60309, pos = vec3(-0.02, 0.01, 0.0), rot = vec3(30.0, -41.0, 80.0) },
+			usetime = 2500,
+			notification = 'You ate a piece of coconut.'
+		}
+	},
+		['honeydew_melon_b'] = {
+		label = 'Piece of honeydew melon',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_honeydew_melon_b`, bone = 60309, pos = vec3(-0.02, -0.02, 0.0), rot = vec3(-50.0, -20.0, 80.0) },
+			usetime = 2500,
+			notification = 'You ate a honeydew melon.'
+		}
+	},	
+		['lemon_b'] = {
+		label = 'Piece of lemon',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_lemon_b`, bone = 60309, pos = vec3(-0.02, 0.01, 0.0), rot = vec3(30.0, -31.0, 80.0) },
+			usetime = 2500,
+			notification = 'You ate a piece of lemon.'
+		}
+	},	
+		['mandarin_b'] = {
+		label = 'Piece of mandarin',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_mandarin_b`, bone = 60309, pos = vec3(-0.03, 0.0, 0.01), rot = vec3(-10.0, -11.0, 170.0) },
+			usetime = 2500,
+			notification = 'You ate a piece of mandarin.'
+		}
+	},	
+		['mango_b'] = {
+		label = 'Piece of mango',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_mango_b`, bone = 60309, pos = vec3(-0.03, -0.01, 0.0), rot = vec3(-10.0, -11.0, 170.0) },
+			usetime = 2500,
+			notification = 'You ate a piece of mango.'
+		}
+	},	
+		['pomegranate_b'] = {
+		label = 'Piece of pomegranate',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_pomegranate_b`, bone = 60309, pos = vec3(-0.03, -0.01, -0.02), rot = vec3(-10.0, -11.0, 170.0) },
+			usetime = 2500,
+			notification = 'You ate a piece of pomegranate.'
+		}
+	},	
+		['dragon_fruit_b'] = {
+		label = 'Piece of dragon fruit',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_dragon_fruit_b`, bone = 60309, pos = vec3(-0.02, 0.0, -0.01), rot = vec3(-46.0, 19.0, 170.0) },
+			usetime = 2500,
+			notification = 'You ate a piece of dragon fruit.'
+		}
+	},	
+		['papaya_b'] = {
+		label = 'Piece of papaya',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_papaya_b`, bone = 60309, pos = vec3(0.0, -0.01, -0.03), rot = vec3(-35.0, 0.0, 170.0) },
+			usetime = 2500,
+			notification = 'You ate a piece of papaya.'
+		}
+	},		
+		['litchi_b'] = {
+		label = 'Litchi',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_fruits_litchi_b`, bone = 60309, pos = vec3(-0.02, 0.0, 0.01), rot = vec3(-46.0, 19.0, 170.0) },
+			usetime = 2500,
+			notification = 'You ate a litchi.'
+		}
+	},	
+	
