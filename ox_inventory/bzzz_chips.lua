3ece84646405f1d643533e12971c3a542368d683
BzZz <adelheid.kaszubi@gmail.com>
2024-01-15 18:39:32 +0100

    Create bzzz_chips.lua

---
+++ ox_inventory/bzzz_chips.lua
@@ -0,0 +1,51 @@
+	['chips_a'] = {
+		label = 'Chips',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'bzzz_eating_chips', clip = 'eat_chip' },
+			prop = { model = `bzzz_prop_chips_a_open`, bone = 60309, pos = vec3(0.11, 0.05, 0.04), rot = vec3(97.2, -14.2, 150.6) },
+			propTwo = { model = `bzzz_prop_chip_a`, bone = 28422, pos = vec3(0.1, 0.05, -0.04), rot = vec3(-14.0, 70.0, 63.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+	
+	['chips_b'] = {
+		label = 'Chips',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'bzzz_eating_chips', clip = 'eat_chip' },
+			prop = { model = `bzzz_prop_chips_b_open`, bone = 60309, pos = vec3(0.11, 0.05, 0.04), rot = vec3(97.2, -14.2, 150.6) },
+			propTwo = { model = `bzzz_prop_chip_b`, bone = 28422, pos = vec3(0.1, 0.05, -0.04), rot = vec3(-14.0, 70.0, 63.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+	
+	['chips_c'] = {
+		label = 'Chips',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'bzzz_eating_chips', clip = 'eat_chip' },
+			prop = { model = `bzzz_prop_chips_c_open`, bone = 60309, pos = vec3(0.11, 0.05, 0.04), rot = vec3(97.2, -14.2, 150.6) },
+			propTwo = { model = `bzzz_prop_chip_b`, bone = 28422, pos = vec3(0.1, 0.05, -0.04), rot = vec3(-14.0, 70.0, 63.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+	
+	['chips_d'] = {
+		label = 'Chips',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'bzzz_eating_chips', clip = 'eat_chip' },
+			prop = { model = `bzzz_prop_chips_d_open`, bone = 60309, pos = vec3(0.11, 0.05, 0.04), rot = vec3(97.2, -14.2, 150.6) },
+			propTwo = { model = `bzzz_prop_chip_a`, bone = 28422, pos = vec3(0.1, 0.05, -0.04), rot = vec3(-14.0, 70.0, 63.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
