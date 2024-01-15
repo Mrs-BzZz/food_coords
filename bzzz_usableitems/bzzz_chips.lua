f02e3afcc97a2c99179647e3a936a0aff411a3fa
BzZz <adelheid.kaszubi@gmail.com>
2024-01-15 18:41:48 +0100

    Create bzzz_chips.lua

---
+++ bzzz_usableitems/bzzz_chips.lua
@@ -0,0 +1,79 @@
+['chips_a'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_eating_chips",
+                animationNameEat = "eat_chip",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_prop_chips_a_open", 
+                xPos = 0.14, yPos = 0.02, zPos = 0.04, xRot = -90.0, yRot = 24.0, zRot = -23.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_prop_chip_a", 
+                xPos = 0.14, yPos = 0.05, zPos = -0.02, xRot = 105.0, yRot = -38.0, zRot = 51.0
+            }
+        },  
+
+['chips_b'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_eating_chips",
+                animationNameEat = "eat_chip",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_prop_chips_b_open", 
+                xPos = 0.14, yPos = 0.02, zPos = 0.04, xRot = -90.0, yRot = 24.0, zRot = -23.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_prop_chip_b", 
+                xPos = 0.14, yPos = 0.05, zPos = -0.02, xRot = 105.0, yRot = -38.0, zRot = 51.0
+            }
+        },  
+		
+['chips_c'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_eating_chips",
+                animationNameEat = "eat_chip",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_prop_chips_c_open", 
+                xPos = 0.14, yPos = 0.02, zPos = 0.04, xRot = -90.0, yRot = 24.0, zRot = -23.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_prop_chip_b", 
+                xPos = 0.14, yPos = 0.05, zPos = -0.02, xRot = 105.0, yRot = -38.0, zRot = 51.0
+            }
+        },
+		
+['chips_d'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_eating_chips",
+                animationNameEat = "eat_chip",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_prop_chips_d_open", 
+                xPos = 0.14, yPos = 0.02, zPos = 0.04, xRot = -90.0, yRot = 24.0, zRot = -23.0
+            },
+            secondProp = {
+                boneIndex = 57005, 
+                propName = "bzzz_prop_chip_a", 
+                xPos = 0.14, yPos = 0.05, zPos = -0.02, xRot = 105.0, yRot = -38.0, zRot = 51.0
+            }
+        },
