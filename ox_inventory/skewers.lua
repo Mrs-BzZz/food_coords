eafc48e15bf29d612004b5594cf37abd1ab7df98
BzZz <adelheid.kaszubi@gmail.com>
2024-01-15 19:39:07 +0100

    Create skewers.lua

---
+++ ox_inventory/skewers.lua
@@ -0,0 +1,165 @@
+		['shashlik_a'] = {
+		label = 'Shashlik',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_shashlik_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['chicken-intestine_a'] = {
+		label = 'Chicken intestine',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_chicken-intestine_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['fishball_a'] = {
+		label = 'Fish ball',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_fishball_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['kikiam_a'] = {
+		label = 'Kikiam',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_kikiam_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['black-pudding_a'] = {
+		label = 'Black pudding',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_black-pudding_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['quail-eggs_a'] = {
+		label = 'Quail eggs',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_quail-eggs_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['hotdog_a'] = {
+		label = 'Hotdog',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_hotdog_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['banana_a'] = {
+		label = 'Banana',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_banana_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['pork-bbq_a'] = {
+		label = 'Pork BBQ',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_pork-bbq_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['chicken-feet_a'] = {
+		label = 'Chicken feet',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_chicken-feet_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['corn_a'] = {
+		label = 'Corn',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_corn_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['corndog_a'] = {
+		label = 'Corndog',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_corndog_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['fish_a'] = {
+		label = 'Fish',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_fish_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['cheese_a'] = {
+		label = 'Cheese',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_cheese_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['potato_a'] = {
+		label = 'Potato',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_skewerpack_potato_a`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-10.0, -90.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
