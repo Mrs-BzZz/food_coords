f845c838c97af35d1c2f870792da2e9a7a6b375c
BzZz <adelheid.kaszubi@gmail.com>
2024-01-17 22:55:50 +0100

    Create bzzz_popsicles.lua

---
+++ bzzz_usableitems/bzzz_popsicles.lua
@@ -0,0 +1,100 @@
+-- copy and paste into bzzz_usableitems/config.lua
+-- for Food
+
+		["barfs"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_popsicle_barfs_b", 
+                xPos = 0.0, yPos = 0.0, zPos = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0
+            },
+        },	
+		["chilldo"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_popsicle_chilldo_b", 
+                xPos = 0.0, yPos = 0.0, zPos = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0
+            },
+        },	
+		["chufty"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_popsicle_chufty_b", 
+                xPos = 0.0, yPos = 0.0, zPos = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0
+            },
+        },	
+		["chufty2"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_popsicle_chufty2_b", 
+                xPos = 0.0, yPos = 0.0, zPos = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0
+            },
+        },	
+		["freeze"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_popsicle_freeze_b", 
+                xPos = 0.0, yPos = 0.0, zPos = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0
+            },
+        },	
+		["milken"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_popsicle_milken_b", 
+                xPos = 0.0, yPos = 0.0, zPos = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0
+            },
+        },	
+		["orang"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_popsicle_orang_b", 
+                xPos = 0.0, yPos = 0.0, zPos = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0
+            },
+        },	
+		["starfish"] = {
+            basicInfo = {
+                amountToAdd = 200000,
+                animationDict = "mp_player_inteat@burger",
+                animationName = "mp_player_int_eat_burger"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_popsicle_starfish_b", 
+                xPos = 0.0, yPos = 0.0, zPos = 0.0, xRot = 0.0, yRot = 0.0, zRot = 0.0
+            },
+        },	
+
