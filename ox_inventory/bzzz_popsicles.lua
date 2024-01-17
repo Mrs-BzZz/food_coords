642c801c597514ae15fcdb28bdb03fedbafec79c
BzZz <adelheid.kaszubi@gmail.com>
2024-01-17 22:55:00 +0100

    Create bzzz_popsicles.lua

---
+++ ox_inventory/bzzz_popsicles.lua
@@ -0,0 +1,92 @@
+-- copy and paste into ox_inventory/data/items.lua
+
+		['barfs'] = {
+		label = 'Popsicle Barfs',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_popsicle_barfs_b`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['chilldo'] = {
+		label = 'Popsicle Childo',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_popsicle_chilldo_b`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},
+		['chufty'] = {
+		label = 'Chocolate Chufty',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_popsicle_chufty_b`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['chufty2'] = {
+		label = 'Creamy Chufty',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_popsicle_chufty2_b`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['freeze'] = {
+		label = 'Freeze sucka',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_popsicle_freeze_b`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['milken'] = {
+		label = 'Üder milken',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_popsicle_milken_b`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['orang'] = {
+		label = 'Orang-O-Tang',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_popsicle_orang_b`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+		['starfish'] = {
+		label = 'Chocolate starfish',
+		weight = 350,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_popsicle_starfish_b`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			notification = 'Bon appétit'
+		}
+	},	
+
+	
