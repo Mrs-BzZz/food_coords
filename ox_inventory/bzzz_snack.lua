c4bb912c32965f68b471afa127799014acefebb8
BzZz <adelheid.kaszubi@gmail.com>
2024-03-11 21:11:00 +0100

    Create bzzz_snack.lua

---
+++ ox_inventory/bzzz_snack.lua
@@ -0,0 +1,96 @@
+['bzzz_croissant'] = {
+		label = 'Croissant',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_new_snacks_croissant_a`, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, -50.0, 80.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},	
+['bzzz_donut_a'] = {
+		label = 'Pink donut',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_new_snacks_donut_a`, pos = vec3(0.0, 0.0, -0.02), rot = vec3(0.0, -20.0, 80.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},		
+['bzzz_donut_b'] = {
+		label = 'Chocolate donut',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_new_snacks_donut_b`, pos = vec3(0.0, 0.0, -0.02), rot = vec3(0.0, -20.0, 80.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+['bzzz_peanuts'] = {
+		label = 'Peanuts',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_new_snacks_peanuts_a`, pos = vec3(0.0, -0.02, -0.01), rot = vec3(0.0, -20.0, 80.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+['bzzz_pretzels'] = {
+		label = 'Pretzels',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_new_snacks_pretzels_a`, pos = vec3(0.03, -0.04, -0.01), rot = vec3(-80.0, 0.0, 70.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+['bzzz_pepsiloca_a'] = {
+		label = 'Pepsiloca',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_new_snacks_pepsiloca_a`, pos = vec3(0.01 ,0.00, 0.07), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+['bzzz_pepsiloca_b'] = {
+		label = 'Pepsiloca light',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_new_snacks_pepsiloca_b`, pos = vec3(0.01 ,0.00, 0.07), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+['bzzz_energy'] = {
+		label = 'Energy drink',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_new_snacks_energy_a`, pos = vec3(0.01 ,0.00, 0.05), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},	
