11f38dc87f644c0553ad6e83d8066c43142ce899
BzZz <adelheid.kaszubi@gmail.com>
2024-04-22 14:06:01 +0200

    Create bzzz_cotton_candy.lua

---
+++ bzzz_usableitems/bzzz_cotton_candy.lua
@@ -0,0 +1,43 @@
+-- copy and paste into the food section
+
+
+		["bzzz_candy_cotton_yellow"] = {
+            basicInfo = {
+                amountToAdd = 200000, 
+                animationDict = "mp_player_inteat@burger", animationName = "mp_player_int_eat_burger"},
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_candy_cotton_yellow", 
+                xPos = 0.15, yPos = 0.07, zPos = 0.00, xRot = -50.0, yRot = 150.0, zRot = 10.0
+            }
+        },
+		["bzzz_candy_cotton_red"] = {
+            basicInfo = {
+                amountToAdd = 200000, 
+                animationDict = "mp_player_inteat@burger", animationName = "mp_player_int_eat_burger"},
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_candy_cotton_red", 
+                xPos = 0.15, yPos = 0.07, zPos = 0.00, xRot = -50.0, yRot = 150.0, zRot = 10.0
+            }
+        },
+		["bzzz_candy_cotton_blue"] = {
+            basicInfo = {
+                amountToAdd = 200000, 
+                animationDict = "mp_player_inteat@burger", animationName = "mp_player_int_eat_burger"},
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_candy_cotton_blue", 
+                xPos = 0.15, yPos = 0.07, zPos = 0.00, xRot = -50.0, yRot = 150.0, zRot = 10.0
+            }
+        },
+		["bzzz_candy_cotton_pink"] = {
+            basicInfo = {
+                amountToAdd = 200000, 
+                animationDict = "mp_player_inteat@burger", animationName = "mp_player_int_eat_burger"},
+            firstProp = {
+                boneIndex = 18905, 
+                propName = "bzzz_candy_cotton_pink", 
+                xPos = 0.15, yPos = 0.07, zPos = 0.00, xRot = -50.0, yRot = 150.0, zRot = 10.0
+            }
+        },
