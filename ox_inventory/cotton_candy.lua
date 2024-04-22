d2157b16e6db7c7cb97f11bb6eea642dcacd99df
BzZz <adelheid.kaszubi@gmail.com>
2024-04-22 13:33:02 +0200

    Create cotton_candy.lua

---
+++ ox_inventory/cotton_candy.lua
@@ -0,0 +1,50 @@
+--copy and paste into the ox_inventory/data/items.lua
+
+['bzzz_candy_cotton_yellow'] = {
+		label = 'Yellow candy cotton',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_candy_cotton_yellow`, pos = vec3(-0.02, 0.01, -0.01), rot = vec3(0.0, -90.0, 0.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+['bzzz_candy_cotton_pink'] = {
+		label = 'Pink candy cotton',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_candy_cotton_pink`, pos = vec3(-0.02, 0.01, -0.01), rot = vec3(0.0, -90.0, 0.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+['bzzz_candy_cotton_red'] = {
+		label = 'Red candy cotton',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_candy_cotton_red`, pos = vec3(-0.02, 0.01, -0.01), rot = vec3(0.0, -90.0, 0.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+['bzzz_candy_cotton_blue'] = {
+		label = 'Blue candy cotton',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_candy_cotton_blue`, pos = vec3(-0.02, 0.01, -0.01), rot = vec3(0.0, -90.0, 0.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
