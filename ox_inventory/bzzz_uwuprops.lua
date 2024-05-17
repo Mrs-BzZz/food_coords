1f2c8094599b16f292d8287c8604df2a286a3209
BzZz <adelheid.kaszubi@gmail.com>
2024-05-17 19:29:39 +0200

    Create bzzz_uwuprops.lua

---
+++ ox_inventory/bzzz_uwuprops.lua
@@ -0,0 +1,220 @@
+	["bzzz_uwuprops_cake_a"] = {
+		label = 'UwU cake',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_cake_a`, pos = vec3(-0.02, 0.0, 0.0), rot = vec3(32.0, -18.0, 29.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_cappuccino_a"] = {
+		label = 'Uwu cappuccino',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_uwuprops_cappuccino_a`, pos = vec3(0.01, 0.01, 0.04), rot = vec3(0.0, -30.0, 30.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_coffee_a"] = {
+		label = 'Uwu coffee',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_uwuprops_coffee_a`, pos = vec3(0.01, 0.01, 0.04), rot = vec3(0.0, -30.0, 30.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_cookie_a"] = {
+		label = 'Uwu cookie',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_cookie_a`, pos = vec3(-0.02, 0.0, 0.0), rot = vec3(32.0, -18.0, 29.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_donut_a"] = {
+		label = 'Uwu donut',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_donut_a`, pos = vec3(-0.02, 0.01, -0.02), rot = vec3(32.0, -18.0, 29.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_drink_a"] = {
+		label = 'Uwu ice coffee',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_uwuprops_drink_a`, pos = vec3(0.04, 0.01, 0.01), rot = vec3(0.0, -20.0, 25.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_drink_b"] = {
+		label = 'Uwu ice coffee',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_uwuprops_drink_b`, pos = vec3(0.04, 0.01, 0.01), rot = vec3(0.0, -20.0, 25.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_drink_c"] = {
+		label = 'Uwu ice coffee',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_uwuprops_drink_c`, pos = vec3(0.04, 0.01, 0.01), rot = vec3(0.0, -20.0, 25.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_drink_d"] = {
+		label = 'Uwu ice coffee',
+		weight = 500,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_uwuprops_drink_d`, pos = vec3(0.04, 0.01, 0.01), rot = vec3(0.0, -20.0, 25.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_icecone_a"] = {
+		label = 'Uwu ice cream in a cornout',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_icecone_a`, pos = vec3(0.01, 0.0, 0.0), rot = vec3(-75.0, -4.0, 80.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+	
+	["bzzz_uwuprops_icecone_b"] = {
+		label = 'Uwu ice cream in a cornout',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_icecone_b`, pos = vec3(0.01, 0.0, 0.0), rot = vec3(-75.0, -4.0, 80.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+	
+		["bzzz_uwuprops_icecone_c"] = {
+		label = 'Uwu ice cream in a cornout',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_icecone_c`, pos = vec3(0.01, 0.0, 0.0), rot = vec3(-75.0, -4.0, 80.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+	
+		["bzzz_uwuprops_icecone_d"] = {
+		label = 'Uwu ice cream in a cornout',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_icecone_d`, pos = vec3(0.01, 0.0, 0.0), rot = vec3(-75.0, -4.0, 80.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_icecream_a"] = {
+		label = 'Uwu ice cream',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_icecream_a`, pos = vec3(-0.03, 0.0, -0.01), rot = vec3(-75.0, -4.0, 80.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_popsicle_a"] = {
+		label = 'Uwu popsicle',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_popsicle_a`, pos = vec3(-0.03, 0.0, -0.01), rot = vec3(15.0, -9.0, 85.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_wafer_a"] = {
+		label = 'Uwu wafer',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_wafer_a`, pos = vec3(0.0, -0.02, -0.02), rot = vec3(15.0, -9.0, 85.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
+
+	["bzzz_uwuprops_lollipop_a"] = {
+		label = 'Uwu lollipop',
+		weight = 500,
+		client = {
+			status = { hunger = 200000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_uwuprops_lollipop_a`, pos = vec3(0.0, -0.02, -0.02), rot = vec3(15.0, -9.0, 85.0) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'Bon appétit'
+		}
+	},
