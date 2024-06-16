fd51da493bc8b661a8e2ccf048fdeda4d3e119cd
BzZz <adelheid.kaszubi@gmail.com>
2024-06-16 21:04:55 +0200

    Create bzzz_bowl_food.lua

---
+++ bzzz_usableitems/bzzz_bowl_food.lua
@@ -0,0 +1,135 @@
+-- Copy and paste into bzzz_usableitems/config.lua - FoodPlates section (bottom of the file)
+
+['bowl_alfredo'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_bowl_eat",
+                animationNameEat = "bowl_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_bowlfood_chicken_alfredo_a", 
+                xPos = 0.07, yPos = 0.03, zPos = 0.07, xRot = -30.0, yRot = 0.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_bowlfood_chicken_alfredo_b", 
+                xPos = 0.04, yPos = 0.05, zPos = 0.0, xRot = -90.0, yRot = -27.0, zRot = -27.0
+            }
+        },
+['bowl_carbonara'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_bowl_eat",
+                animationNameEat = "bowl_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_bowlfood_chicken_carbonara_a", 
+                xPos = 0.07, yPos = 0.03, zPos = 0.07, xRot = -30.0, yRot = 0.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_bowlfood_chicken_carbonara_b", 
+                xPos = 0.04, yPos = 0.05, zPos = 0.0, xRot = -90.0, yRot = -27.0, zRot = -27.0
+            }
+        },
+['bowl_primavera'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_bowl_eat",
+                animationNameEat = "bowl_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_bowlfood_primavera_pasta_a", 
+                xPos = 0.07, yPos = 0.03, zPos = 0.07, xRot = -30.0, yRot = 0.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_bowlfood_primavera_pasta_b", 
+                xPos = 0.04, yPos = 0.05, zPos = 0.0, xRot = -90.0, yRot = -27.0, zRot = -27.0
+            }
+        },		
+['bowl_sausage'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_bowl_eat",
+                animationNameEat = "bowl_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_bowlfood_sausage_pasta_a", 
+                xPos = 0.07, yPos = 0.03, zPos = 0.07, xRot = -30.0, yRot = 0.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_bowlfood_sausage_pasta_b", 
+                xPos = 0.04, yPos = 0.05, zPos = 0.0, xRot = -90.0, yRot = -27.0, zRot = -27.0
+            }
+        },	
+['bowl_borscht'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_bowl_eat",
+                animationNameEat = "bowl_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_bowlfood_soup_borscht_a", 
+                xPos = 0.07, yPos = 0.01, zPos = 0.05, xRot = -40.0, yRot = -7.0, zRot = -27.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_bowlfood_soup_borscht_b", 
+                xPos = 0.08, yPos = 0.03, zPos = -0.02, xRot = -60.0, yRot = 93.0, zRot = 43.0
+            }
+        },		
+['bowl_miso'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_bowl_eat",
+                animationNameEat = "bowl_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_bowlfood_soup_miso_a", 
+                xPos = 0.07, yPos = 0.01, zPos = 0.05, xRot = -40.0, yRot = -7.0, zRot = -27.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_bowlfood_soup_miso_b", 
+                xPos = 0.08, yPos = 0.03, zPos = -0.02, xRot = -60.0, yRot = 93.0, zRot = 43.0
+            }
+        },		
+['bowl_pho'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_bowl_eat",
+                animationNameEat = "bowl_eat",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_bowlfood_soup_pho_a", 
+                xPos = 0.07, yPos = 0.01, zPos = 0.05, xRot = -40.0, yRot = -7.0, zRot = -27.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_bowlfood_soup_pho_b", 
+                xPos = 0.08, yPos = 0.03, zPos = -0.02, xRot = -60.0, yRot = 93.0, zRot = 43.0
+            }
+        },		
