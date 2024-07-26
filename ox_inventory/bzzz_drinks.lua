d6daf2866021919c98505cfb1fd842b93ac5b8cb
BzZz <adelheid.kaszubi@gmail.com>
2024-07-26 22:20:34 +0200

    Create bzzz_drinks.lua

---
+++ ox_inventory/bzzz_drinks.lua
@@ -0,0 +1,364 @@
+	
+['bzzz_drink_beer_a'] = {
+		label = 'Big Beer',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_beer_a`, pos = vec3(0.03, 0.02, -0.01), rot = vec3(5.0, -4.0, 28.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},
+['bzzz_drink_beer_b'] = {
+		label = 'Big Beer',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_beer_b`, pos = vec3(0.02, 0.01, -0.07), rot = vec3(5.0, -4.0, 28.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},
+['bzzz_drink_beer_c'] = {
+		label = 'Small Beer',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_beer_c`, pos = vec3(0.04, 0.02, -0.02), rot = vec3(5.0, -4.0, 28.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_beer_d'] = {
+		label = 'Pint of beer',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_beer_d`, pos = vec3(0.04, 0.02, -0.02), rot = vec3(5.0, -4.0, 28.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_bahamamama_a'] = {
+		label = 'Bahama Mama',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_bahamamama_a`, pos = vec3(0.02, 0.02, -0.03), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_bluelagoon_a'] = {
+		label = 'Blue Lagoon',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_bluelagoon_a`, pos = vec3(0.02, 0.02, -0.03), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_longislandicedtea_a'] = {
+		label = 'Long Island Iced Tea',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_longislandicedtea_a`, pos = vec3(0.02, 0.02, -0.03), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_sexonthebeach_a'] = {
+		label = 'Sex on the Beach',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_longislandicedtea_a`, pos = vec3(0.02, 0.02, -0.03), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_blackrussian_a'] = {
+		label = 'Black Russian',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_blackrussian_a`, pos = vec3(0.03, 0.02, 0.04), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_maitai_a'] = {
+		label = 'Mai Tai',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_maitai_a`, pos = vec3(0.03, 0.02, 0.04), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_negroni_a'] = {
+		label = 'Negroni',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_negroni_a`, pos = vec3(0.03, 0.02, 0.04), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_oldfashioned_a'] = {
+		label = 'Old Fashioned',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_oldfashioned_a`, pos = vec3(0.03, 0.02, 0.04), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_whiskeysour_a'] = {
+		label = 'Whiskey Sour',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_whiskeysour_a`, pos = vec3(0.03, 0.02, 0.04), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_whiterussian_a'] = {
+		label = 'White Russian',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_whiterussian_a`, pos = vec3(0.03, 0.02, 0.04), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_bloodymary_a'] = {
+		label = 'Bloody Mary',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_bloodymary_a`, pos = vec3(0.02, 0.02, -0.05), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_gintonic_a'] = {
+		label = 'Gin with Tonic',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_gintonic_a`, pos = vec3(0.02, 0.02, -0.05), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_rumcoke_a'] = {
+		label = 'Rum with Coke',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_rumcoke_a`, pos = vec3(0.02, 0.02, -0.05), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_screwdriver_a'] = {
+		label = 'Screwdriver',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_screwdriver_a`, pos = vec3(0.02, 0.02, -0.05), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_tomcollins_a'] = {
+		label = 'Tom Collins',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_tomcollins_a`, pos = vec3(0.02, 0.02, -0.05), rot = vec3(5.0, -1.0, -27.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_cosmopolitan_a'] = {
+		label = 'Cosmopolitan',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_cosmopolitan_a`, pos = vec3(0.04, 0.01, 0.0), rot = vec3(12.0, -8.0, -48.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_gimlet_a'] = {
+		label = 'Gimlet',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_gimlet_a`, pos = vec3(0.04, 0.01, 0.0), rot = vec3(12.0, -8.0, -48.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_margarita_a'] = {
+		label = 'Margarita',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_margarita_a`, pos = vec3(0.04, 0.01, 0.0), rot = vec3(12.0, -8.0, -48.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_martini_a'] = {
+		label = 'Martini',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_martini_a`, pos = vec3(0.04, 0.01, 0.0), rot = vec3(12.0, -8.0, -48.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_manhattan_a'] = {
+		label = 'Manhattan',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_manhattan_a`, pos = vec3(0.04, 0.01, 0.0), rot = vec3(12.0, -8.0, -48.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_shot_b52_a'] = {
+		label = 'Shot of B52',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_shot_b52_a`, pos = vec3(0.0, 0.01, 0.07), rot = vec3(12.0, -8.0, 18.0) },
+			usetime = 3000,
+			notification = 'Cheers!'
+		}
+	},
+['bzzz_drink_shot_bluekamikaze_a'] = {
+		label = 'Shot of Blue Kamikaze',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_shot_bluekamikaze_a`, pos = vec3(0.0, 0.01, 0.07), rot = vec3(12.0, -8.0, 18.0) },
+			usetime = 3000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_shot_kamikaze_a'] = {
+		label = 'Shot of Kamikaze',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_shot_kamikaze_a`, pos = vec3(0.0, 0.01, 0.07), rot = vec3(12.0, -8.0, 18.0) },
+			usetime = 3000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_shot_lemondrop_a'] = {
+		label = 'Shot of Lemon Drop',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_shot_lemondrop_a`, pos = vec3(0.0, 0.01, 0.07), rot = vec3(12.0, -8.0, 18.0) },
+			usetime = 3000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_shot_rum_a'] = {
+		label = 'Shot of Rum',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_shot_rum_a`, pos = vec3(0.0, 0.01, 0.07), rot = vec3(12.0, -8.0, 18.0) },
+			usetime = 3000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_shot_vodka_a'] = {
+		label = 'Shot of Vodka',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_shot_vodka_a`, pos = vec3(0.0, 0.01, 0.07), rot = vec3(12.0, -8.0, 18.0) },
+			usetime = 3000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_shot_washinapple_a'] = {
+		label = 'Shot of Washington Apple',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_shot_washinapple_a`, pos = vec3(0.0, 0.01, 0.07), rot = vec3(12.0, -8.0, 18.0) },
+			usetime = 3000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_moscowmule_a'] = {
+		label = 'Moscow Mule',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_moscowmule_a`, pos = vec3(0.03, 0.03, 0.02), rot = vec3(12.0, -8.0, 18.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},	
+['bzzz_drink_coconut_a'] = {
+		label = 'Coconut',
+		weight = 350,
+		client = {
+			status = { thirst = 200000 },
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `bzzz_drink_coconut_a`, pos = vec3(0.07, 0.05, 0.04), rot = vec3(28.0, -50.0, 14.0) },
+			usetime = 5000,
+			notification = 'Cheers!'
+		}
+	},
