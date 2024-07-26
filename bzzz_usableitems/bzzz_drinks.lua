a2d0b858a8483f9c0f27c297e0707f50e42ed78c
BzZz <adelheid.kaszubi@gmail.com>
2024-07-26 20:23:54 +0200

    Create bzzz_drinks.lua

---
+++ bzzz_usableitems/bzzz_drinks.lua
@@ -0,0 +1,385 @@
+["bzzz_drink_beer_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, -- Adds water to your status bar (require esx_status)
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", --  If you don't want the effect, delete this line / You can find the effects at https://wiki.rage.mp/index.php?title=Timecycle_Modifiers
+        effectDuration = 5000 -- The time is given in milliseconds
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_beer_a",
+        xPos = 0.1, yPos = -0.05, zPos = 0.06, xRot = -83.0, yRot = 190.0, zRot = 18.0}
+},
+
+["bzzz_drink_beer_b"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5",
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_beer_b",
+        xPos = 0.08, yPos = -0.12, zPos = 0.07, xRot = -83.0, yRot = 190.0, zRot = 18.0}
+},
+
+["bzzz_drink_beer_c"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 3000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_beer_c",
+        xPos = 0.06, yPos = -0.05, zPos = 0.05, xRot = -61.0, yRot = 85.0, zRot = -15.0}
+},
+
+["bzzz_drink_beer_d"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_beer_d",
+        xPos = 0.06, yPos = -0.05, zPos = 0.05, xRot = -61.0, yRot = 85.0, zRot = -15.0}
+},
+
+["bzzz_drink_bahamamama_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator9", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_bahamamama_a",
+        xPos = 0.06, yPos = -0.05, zPos = 0.05, xRot = 79.0, yRot = 35.0, zRot = 145.0}
+},
+
+["bzzz_drink_bluelagoon_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator9", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_bluelagoon_a",
+        xPos = 0.06, yPos = -0.05, zPos = 0.05, xRot = 79.0, yRot = 35.0, zRot = 145.0}
+},
+
+["bzzz_drink_longislandicedtea_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator9", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_longislandicedtea_a",
+        xPos = 0.06, yPos = -0.05, zPos = 0.05, xRot = 79.0, yRot = 35.0, zRot = 145.0}
+},
+
+["bzzz_drink_sexonthebeach_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator9", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_sexonthebeach_a",
+        xPos = 0.06, yPos = -0.05, zPos = 0.05, xRot = 79.0, yRot = 35.0, zRot = 145.0}
+},
+
+["bzzz_drink_blackrussian_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "BarryFadeOut", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_blackrussian_a",
+        xPos = 0.09, yPos = 0.0, zPos = 0.06, xRot = -75.0, yRot = 40.0, zRot = -50.0}
+},
+
+["bzzz_drink_maitai_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "BikerFilter", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_maitai_a",
+        xPos = 0.09, yPos = 0.0, zPos = 0.06, xRot = -75.0, yRot = 40.0, zRot = -50.0}
+},
+
+["bzzz_drink_negroni_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "BlackOut", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_negroni_a",
+        xPos = 0.09, yPos = 0.0, zPos = 0.06, xRot = -75.0, yRot = 40.0, zRot = -50.0}
+},
+
+["bzzz_drink_oldfashioned_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "Drug_deadman", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_oldfashioned_a",
+        xPos = 0.09, yPos = 0.0, zPos = 0.06, xRot = -75.0, yRot = 40.0, zRot = -50.0}
+},
+
+["bzzz_drink_whiskeysour_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "Drug_deadman_blend", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_whiskeysour_a",
+        xPos = 0.09, yPos = 0.0, zPos = 0.06, xRot = -75.0, yRot = 40.0, zRot = -50.0}
+},
+
+["bzzz_drink_bloodymary_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "Drunk", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_bloodymary_a",
+        xPos = 0.09, yPos = -0.08, zPos = 0.05, xRot = -83.0, yRot = 190.0, zRot = 18.0}
+},
+
+["bzzz_drink_gintonic_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "NG_filmic25", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_gintonic_a",
+        xPos = 0.09, yPos = -0.08, zPos = 0.05, xRot = -83.0, yRot = 190.0, zRot = 18.0}
+},
+
+["bzzz_drink_rumcoke_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_rumcoke_a",
+        xPos = 0.08, yPos = -0.09, zPos = 0.07, xRot = -83.0, yRot = 190.0, zRot = 28.0}
+},
+
+["bzzz_drink_screwdriver_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "NG_filmic02", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_screwdriver_a",
+        xPos = 0.09, yPos = -0.08, zPos = 0.05, xRot = -83.0, yRot = 190.0, zRot = 18.0}
+},
+
+["bzzz_drink_tomcollins_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "NG_filmic02", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_tomcollins_a",
+        xPos = 0.09, yPos = -0.08, zPos = 0.05, xRot = -83.0, yRot = 190.0, zRot = 18.0}
+},
+
+["bzzz_drink_cosmopolitan_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "NG_filmic02", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_cosmopolitan_a",
+        xPos = 0.08, yPos = -0.01, zPos = 0.07, xRot = 27.0, yRot = 118.0, zRot = 188.0}
+},
+
+["bzzz_drink_gimlet_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "Glasses_BlackOut", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_gimlet_a",
+        xPos = 0.08, yPos = -0.01, zPos = 0.07, xRot = 27.0, yRot = 118.0, zRot = 188.0}
+},
+
+["bzzz_drink_margarita_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "NG_filmic10", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_margarita_a",
+        xPos = 0.08, yPos = -0.01, zPos = 0.07, xRot = 27.0, yRot = 118.0, zRot = 188.0}
+},
+
+["bzzz_drink_martini_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_martini_a",
+        xPos = 0.08, yPos = -0.01, zPos = 0.07, xRot = 27.0, yRot = 118.0, zRot = 188.0}
+},
+
+["bzzz_drink_manhattan_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "PPFilter", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_manhattan_a",
+        xPos = 0.08, yPos = -0.01, zPos = 0.07, xRot = 27.0, yRot = 118.0, zRot = 188.0}
+},
+
+["bzzz_drink_shot_b52_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_shot_b52_a",
+        xPos = 0.14, yPos = 0.02, zPos = 0.03, xRot = -70.0, yRot = 140.0, zRot = 0.0}
+},
+
+["bzzz_drink_shot_bluekamikaze_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_shot_bluekamikaze_a",
+        xPos = 0.14, yPos = 0.02, zPos = 0.03, xRot = -70.0, yRot = 140.0, zRot = 0.0}
+},
+
+["bzzz_drink_shot_kamikaze_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "DRUG_gas_huffin", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_shot_kamikaze_a",
+        xPos = 0.14, yPos = 0.02, zPos = 0.03, xRot = -70.0, yRot = 140.0, zRot = 0.0}
+},
+
+["bzzz_drink_shot_lemondrop_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_shot_lemondrop_a",
+        xPos = 0.14, yPos = 0.02, zPos = 0.03, xRot = -70.0, yRot = 140.0, zRot = 0.0}
+},
+
+["bzzz_drink_shot_rum_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_shot_rum_a",
+        xPos = 0.14, yPos = 0.02, zPos = 0.03, xRot = -70.0, yRot = 140.0, zRot = 0.0}
+},
+
+["bzzz_drink_shot_vodka_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_shot_vodka_a",
+        xPos = 0.14, yPos = 0.02, zPos = 0.03, xRot = -70.0, yRot = 140.0, zRot = 0.0}
+},
+
+["bzzz_drink_shot_washinapple_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_shot_washinapple_a",
+        xPos = 0.14, yPos = 0.02, zPos = 0.03, xRot = -70.0, yRot = 140.0, zRot = 0.0}
+},
+
+["bzzz_drink_moscowmule_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+        effectName = "spectator5", 
+        effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_moscowmule_a",
+        xPos = 0.1, yPos = -0.02, zPos = 0.05, xRot = -70.0, yRot = 140.0, zRot = 0.0}
+},
+
+["bzzz_drink_coconut_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_intdrink", animationName = "loop_bottle",
+       -- effectName = "spectator5", 
+       -- effectDuration = 5000 
+    },
+        prop = {
+        boneIndex = 18905, propName = "bzzz_drink_coconut_a",
+        xPos = 0.1, yPos = 0.04, zPos = 0.11, xRot = -13.0, yRot = 140.0, zRot = 0.0}
+},
+
+
