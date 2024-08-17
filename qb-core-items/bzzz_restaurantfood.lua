3c2c9ede38340eea53b6a37fa7a70d47add67b65
BzZz <adelheid.kaszubi@gmail.com>
2024-08-17 23:40:07 +0200

    Create bzzz_restaurantfood.lua

---
+++ qb-core-items/bzzz_restaurantfood.lua
@@ -0,0 +1,157 @@
+-- Register items to qb-core/shared/items.lua
+-- restaurant food
+res_food_a = { 
+    name = 'res_food_a', 
+    label = 'Schnitzel with fries', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_b = { 
+    name = 'res_food_b', 
+    label = 'Schnitzel with croquettes', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_c = { 
+    name = 'res_food_c', 
+    label = 'Spaghetti', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_c.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_d = { 
+    name = 'res_food_d', 
+    label = 'Grilled fish', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_d.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_e = { 
+    name = 'res_food_e', 
+    label = 'Steak with potatoes', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_e.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_f = { 
+    name = 'res_food_f', 
+    label = 'Steak with fries', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_f.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_g = { 
+    name = 'res_food_g', 
+    label = 'Lobster', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_g.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_h = { 
+    name = 'res_food_h', 
+    label = 'Eggs with toast', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_h.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_i = { 
+    name = 'res_food_i', 
+    label = 'Sushi', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_i.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_j = { 
+    name = 'res_food_j', 
+    label = 'Shrimps with sauce', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_j.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_k = { 
+    name = 'res_food_k', 
+    label = 'Chicken with potatoes', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_k.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
+
+res_food_l = { 
+    name = 'res_food_l', 
+    label = 'Vegetarian salad', 
+    weight = 500, 
+    type = 'item', 
+    image = 'res_food_l.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bon appetit' 
+},
