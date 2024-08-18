9d9d3481e90553776630f37398fff50816ed6f92
BzZz <adelheid.kaszubi@gmail.com>
2024-08-18 19:02:01 +0200

    Create bzzz_fruit.lua

---
+++ qb-core-items/bzzz_fruit.lua
@@ -0,0 +1,124 @@
+-- Register items to qb-core/shared/items.lua
+-- fruit
+
+
+avocado_b = { 
+    name = 'avocado_b', 
+    label = 'Piece of avocado', 
+    weight = 200, 
+    type = 'item', 
+    image = 'avocado_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+coconut_b = { 
+    name = 'coconut_b', 
+    label = 'Piece of coconut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'coconut_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+honeydew_melon_b = { 
+    name = 'honeydew_melon_b', 
+    label = 'Piece of honeydew melon', 
+    weight = 200, 
+    type = 'item', 
+    image = 'honeydew_melon_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+lemon_b = { 
+    name = 'lemon_b', 
+    label = 'Piece of lemon', 
+    weight = 200, 
+    type = 'item', 
+    image = 'lemon_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+mandarin_b = { 
+    name = 'mandarin_b', 
+    label = 'Piece of mandarin', 
+    weight = 200, 
+    type = 'item', 
+    image = 'mandarin_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+mango_b = { 
+    name = 'mango_b', 
+    label = 'Piece of mango', 
+    weight = 200, 
+    type = 'item', 
+    image = 'mango_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+pomegranate_b = { 
+    name = 'pomegranate_b', 
+    label = 'Piece of pomegranate', 
+    weight = 200, 
+    type = 'item', 
+    image = 'pomegranate_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+dragon_fruit_b = { 
+    name = 'dragon_fruit_b', 
+    label = 'Piece of dragon fruit', 
+    weight = 200, 
+    type = 'item', 
+    image = 'dragon_fruit_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+papaya_b = { 
+    name = 'papaya_b', 
+    label = 'Piece of papaya', 
+    weight = 200, 
+    type = 'item', 
+    image = 'papaya_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+litchi_b = { 
+    name = 'litchi_b', 
+    label = 'Litchi', 
+    weight = 200, 
+    type = 'item', 
+    image = 'litchi_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
