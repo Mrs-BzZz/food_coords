4990afe57f5bfd0c2b908c2eeee58210fa74a623
BzZz <adelheid.kaszubi@gmail.com>
2024-08-18 17:00:27 +0200

    Create bzzz_snackprops.lua

---
+++ qb-core-items/bzzz_snackprops.lua
@@ -0,0 +1,99 @@
+-- Register items to qb-core/shared/items.lua
+-- snack props
+
+bzzz_croissant = { 
+    name = 'bzzz_croissant', 
+    label = 'Croissant', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_croissant.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+bzzz_donut_a = { 
+    name = 'bzzz_donut_a', 
+    label = 'Pink donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_donut_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+bzzz_donut_b = { 
+    name = 'bzzz_donut_b', 
+    label = 'Chocolate donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_donut_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+bzzz_peanuts = { 
+    name = 'bzzz_peanuts', 
+    label = 'Peanuts', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_peanuts.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+bzzz_pretzels = { 
+    name = 'bzzz_pretzels', 
+    label = 'Pretzels', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_pretzels.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+bzzz_pepsiloca_a = { 
+    name = 'bzzz_pepsiloca_a', 
+    label = 'Pepsiloca', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_pepsiloca_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Drink' 
+},
+bzzz_pepsiloca_b = { 
+    name = 'bzzz_pepsiloca_b', 
+    label = 'Pepsiloca light', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_pepsiloca_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Drink' 
+},
+bzzz_energy = { 
+    name = 'bzzz_energy', 
+    label = 'Energy drink', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_energy.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Drink' 
+},
