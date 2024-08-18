5acbbdd20db5a02e0e7f36c7a841bafe40fbe971
BzZz <adelheid.kaszubi@gmail.com>
2024-08-18 17:05:08 +0200

    Create bzzz_popsicles.lua

---
+++ qb-core-items/bzzz_popsicles.lua
@@ -0,0 +1,99 @@
+-- Register items to qb-core/shared/items.lua
+-- popsicles
+
+barfs = { 
+    name = 'barfs', 
+    label = 'Popsicle Barfs', 
+    weight = 200, 
+    type = 'item', 
+    image = 'barfs_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+chilldo = { 
+    name = 'chilldo', 
+    label = 'Popsicle Childo', 
+    weight = 200, 
+    type = 'item', 
+    image = 'chilldo_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+chufty = { 
+    name = 'chufty', 
+    label = 'Chocolate Chufty', 
+    weight = 200, 
+    type = 'item', 
+    image = 'chufty_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+chufty2 = { 
+    name = 'chufty2', 
+    label = 'Creamy Chufty', 
+    weight = 200, 
+    type = 'item', 
+    image = 'chufty2_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+freeze = { 
+    name = 'freeze', 
+    label = 'Freeze sucka', 
+    weight = 200, 
+    type = 'item', 
+    image = 'freeze_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+milken = { 
+    name = 'milken', 
+    label = 'Üder milken', 
+    weight = 200, 
+    type = 'item', 
+    image = 'milken_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+orang = { 
+    name = 'orang', 
+    label = 'Orang-O-Tang', 
+    weight = 200, 
+    type = 'item', 
+    image = 'orang_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
+starfish = { 
+    name = 'starfish', 
+    label = 'Chocolate starfish', 
+    weight = 200, 
+    type = 'item', 
+    image = 'starfish_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Food' 
+},
