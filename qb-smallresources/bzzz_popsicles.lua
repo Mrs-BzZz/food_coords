743658ad72d98ab7545a93794848571d1d6268ab
BzZz <adelheid.kaszubi@gmail.com>
2024-08-18 17:19:31 +0200

    Create bzzz_popsicles.lua

---
+++ qb-smallresources/bzzz_popsicles.lua
@@ -0,0 +1,199 @@
+-- 1) Register items to qb-core/shared/items.lua
+-- https://github.com/Mrs-BzZz/food_coords/blob/main/qb-core-items/bzzz_popsicles.lua
+
+
+
+-- 2) Add items to qb-smallresources/config.lua (Config.Consumables - custom)
+['barfs'] = {
+    progress = {
+        label = 'Using Item...',
+        time = 5000
+    },
+    animation = {
+        animDict = 'mp_player_inteat@burger',
+        anim = 'mp_player_int_eat_burger',
+        flags = 17,
+    },
+    prop = {
+        model = 'bzzz_popsicle_barfs_b',
+        bone = 60309,
+        coords = vector3(0.0, 0.0, 0.0), -- vector 3 format
+        rotation = vector3(0.0, 0.0, 0.0), -- vector 3 format
+    },
+    replenish = {
+        type = 'Hunger', -- replenish type 'Hunger'/'Thirst' / false
+        replenish = math.random(20, 40),
+        isAlcohol = false, -- if you want it to add alcohol count
+        event = false, -- 'eventname' if you want it to trigger an outside event on use useful for drugs
+        server = false -- if the event above is a server event
+    }
+},
+['chilldo'] = {
+    progress = {
+        label = 'Using Item...',
+        time = 5000
+    },
+    animation = {
+        animDict = 'mp_player_inteat@burger',
+        anim = 'mp_player_int_eat_burger',
+        flags = 17,
+    },
+    prop = {
+        model = 'bzzz_popsicle_chilldo_b',
+        bone = 60309,
+        coords = vector3(0.0, 0.0, 0.0), -- vector 3 format
+        rotation = vector3(0.0, 0.0, 0.0), -- vector 3 format
+    },
+    replenish = {
+        type = 'Hunger', -- replenish type 'Hunger'/'Thirst' / false
+        replenish = math.random(20, 40),
+        isAlcohol = false, -- if you want it to add alcohol count
+        event = false, -- 'eventname' if you want it to trigger an outside event on use useful for drugs
+        server = false -- if the event above is a server event
+    }
+},
+['chufty'] = {
+    progress = {
+        label = 'Using Item...',
+        time = 5000
+    },
+    animation = {
+        animDict = 'mp_player_inteat@burger',
+        anim = 'mp_player_int_eat_burger',
+        flags = 17,
+    },
+    prop = {
+        model = 'bzzz_popsicle_chufty_b',
+        bone = 60309,
+        coords = vector3(0.0, 0.0, 0.0), -- vector 3 format
+        rotation = vector3(0.0, 0.0, 0.0), -- vector 3 format
+    },
+    replenish = {
+        type = 'Hunger', -- replenish type 'Hunger'/'Thirst' / false
+        replenish = math.random(20, 40),
+        isAlcohol = false, -- if you want it to add alcohol count
+        event = false, -- 'eventname' if you want it to trigger an outside event on use useful for drugs
+        server = false -- if the event above is a server event
+    }
+},
+['chufty2'] = {
+    progress = {
+        label = 'Using Item...',
+        time = 5000
+    },
+    animation = {
+        animDict = 'mp_player_inteat@burger',
+        anim = 'mp_player_int_eat_burger',
+        flags = 17,
+    },
+    prop = {
+        model = 'bzzz_popsicle_chufty2_b',
+        bone = 60309,
+        coords = vector3(0.0, 0.0, 0.0), -- vector 3 format
+        rotation = vector3(0.0, 0.0, 0.0), -- vector 3 format
+    },
+    replenish = {
+        type = 'Hunger', -- replenish type 'Hunger'/'Thirst' / false
+        replenish = math.random(20, 40),
+        isAlcohol = false, -- if you want it to add alcohol count
+        event = false, -- 'eventname' if you want it to trigger an outside event on use useful for drugs
+        server = false -- if the event above is a server event
+    }
+},
+['freeze'] = {
+    progress = {
+        label = 'Using Item...',
+        time = 5000
+    },
+    animation = {
+        animDict = 'mp_player_inteat@burger',
+        anim = 'mp_player_int_eat_burger',
+        flags = 17,
+    },
+    prop = {
+        model = 'bzzz_popsicle_freeze_b',
+        bone = 60309,
+        coords = vector3(0.0, 0.0, 0.0), -- vector 3 format
+        rotation = vector3(0.0, 0.0, 0.0), -- vector 3 format
+    },
+    replenish = {
+        type = 'Hunger', -- replenish type 'Hunger'/'Thirst' / false
+        replenish = math.random(20, 40),
+        isAlcohol = false, -- if you want it to add alcohol count
+        event = false, -- 'eventname' if you want it to trigger an outside event on use useful for drugs
+        server = false -- if the event above is a server event
+    }
+},
+['milken'] = {
+    progress = {
+        label = 'Using Item...',
+        time = 5000
+    },
+    animation = {
+        animDict = 'mp_player_inteat@burger',
+        anim = 'mp_player_int_eat_burger',
+        flags = 17,
+    },
+    prop = {
+        model = 'bzzz_popsicle_milken_b',
+        bone = 60309,
+        coords = vector3(0.0, 0.0, 0.0), -- vector 3 format
+        rotation = vector3(0.0, 0.0, 0.0), -- vector 3 format
+    },
+    replenish = {
+        type = 'Hunger', -- replenish type 'Hunger'/'Thirst' / false
+        replenish = math.random(20, 40),
+        isAlcohol = false, -- if you want it to add alcohol count
+        event = false, -- 'eventname' if you want it to trigger an outside event on use useful for drugs
+        server = false -- if the event above is a server event
+    }
+},
+['orang'] = {
+    progress = {
+        label = 'Using Item...',
+        time = 5000
+    },
+    animation = {
+        animDict = 'mp_player_inteat@burger',
+        anim = 'mp_player_int_eat_burger',
+        flags = 17,
+    },
+    prop = {
+        model = 'bzzz_popsicle_orang_b',
+        bone = 60309,
+        coords = vector3(0.0, 0.0, 0.0), -- vector 3 format
+        rotation = vector3(0.0, 0.0, 0.0), -- vector 3 format
+    },
+    replenish = {
+        type = 'Hunger', -- replenish type 'Hunger'/'Thirst' / false
+        replenish = math.random(20, 40),
+        isAlcohol = false, -- if you want it to add alcohol count
+        event = false, -- 'eventname' if you want it to trigger an outside event on use useful for drugs
+        server = false -- if the event above is a server event
+    }
+},
+['starfish'] = {
+    progress = {
+        label = 'Using Item...',
+        time = 5000
+    },
+    animation = {
+        animDict = 'mp_player_inteat@burger',
+        anim = 'mp_player_int_eat_burger',
+        flags = 17,
+    },
+    prop = {
+        model = 'bzzz_popsicle_starfish_b',
+        bone = 60309,
+        coords = vector3(0.0, 0.0, 0.0), -- vector 3 format
+        rotation = vector3(0.0, 0.0, 0.0), -- vector 3 format
+    },
+    replenish = {
+        type = 'Hunger', -- replenish type 'Hunger'/'Thirst' / false
+        replenish = math.random(20, 40),
+        isAlcohol = false, -- if you want it to add alcohol count
+        event = false, -- 'eventname' if you want it to trigger an outside event on use useful for drugs
+        server = false -- if the event above is a server event
+    }
+},
+
