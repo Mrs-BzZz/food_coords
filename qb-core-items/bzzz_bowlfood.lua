4c0b8fa133adad4437c60da294367730cc8e7209
BzZz <adelheid.kaszubi@gmail.com>
2024-08-18 15:25:02 +0200

    Create bzzz_bowlfood.lua

---
+++ qb-core-items/bzzz_bowlfood.lua
@@ -0,0 +1,86 @@
+-- Register items to qb-core/shared/items.lua
+-- bowlfood
+bowl_carbonara = { 
+    name = 'bowl_carbonara', 
+    label = 'Chicken Carbonara', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bowl_carbonara.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bowl with food' 
+},
+bowl_miso = { 
+    name = 'bowl_miso', 
+    label = 'Soup Miso', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bowl_miso.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bowl with food' 
+},
+bowl_pho = { 
+    name = 'bowl_pho', 
+    label = 'Soup Pho Bo', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bowl_pho.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bowl with food' 
+},
+bowl_primavera = { 
+    name = 'bowl_primavera', 
+    label = 'Pasta primavera', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bowl_primavera.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bowl with food' 
+},
+bowl_sausage = { 
+    name = 'bowl_sausage', 
+    label = 'Pasta sausage', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bowl_sausage.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bowl with food' 
+},
+bowl_alfredo = { 
+    name = 'bowl_alfredo', 
+    label = 'Chicken Alfredo', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bowl_alfredo.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bowl with food' 
+},
+bowl_borscht = { 
+    name = 'bowl_borscht', 
+    label = 'Borscht', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bowl_borscht.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Bowl with food' 
+},
