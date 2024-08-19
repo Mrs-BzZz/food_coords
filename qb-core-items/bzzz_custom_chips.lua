a9eff15ca51dbc769cd37d7218c8f3bbbca7e695
BzZz <adelheid.kaszubi@gmail.com>
2024-08-19 18:25:12 +0200

    Create bzzz_custom_chips.lua

---
+++ qb-core-items/bzzz_custom_chips.lua
@@ -0,0 +1,51 @@
+-- Register items to qb-core/shared/items.lua
+-- custom chips
+
+chips_a = { 
+    name = 'chips_a', 
+    label = 'Pepper chips', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_chips_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+chips_b = { 
+    name = 'chips_b', 
+    label = 'Salty chips', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_chips_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+chips_c = { 
+    name = 'chips_c', 
+    label = 'Garlic chips', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_chips_c.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+chips_d = { 
+    name = 'chips_d', 
+    label = 'Chilli chips', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_chips_d.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
