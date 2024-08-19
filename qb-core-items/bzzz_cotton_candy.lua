4be2243328e89166276eb30db24c95d008b2aa24
BzZz <adelheid.kaszubi@gmail.com>
2024-08-19 18:24:30 +0200

    Create bzzz_cotton_candy.lua

---
+++ qb-core-items/bzzz_cotton_candy.lua
@@ -0,0 +1,53 @@
+-- Register items to qb-core/shared/items.lua
+-- cotton candy
+
+
+bzzz_candy_cotton_yellow = { 
+    name = 'bzzz_candy_cotton_yellow', 
+    label = 'Yellow candy cotton', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_candy_cotton_yellow.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+bzzz_candy_cotton_pink = { 
+    name = 'bzzz_candy_cotton_pink', 
+    label = 'Pink candy cotton', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_candy_cotton_pink.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+bzzz_candy_cotton_red = { 
+    name = 'bzzz_candy_cotton_red', 
+    label = 'Red candy cotton', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_candy_cotton_red.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+bzzz_candy_cotton_blue = { 
+    name = 'bzzz_candy_cotton_blue', 
+    label = 'Blue candy cotton', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_candy_cotton_blue.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
