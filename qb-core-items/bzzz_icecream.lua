7b730326ec033b49eec4b3a7667d78227da1c491
BzZz <adelheid.kaszubi@gmail.com>
2024-08-27 19:37:46 +0200

    Create bzzz_icecream.lua

---
+++ qb-core-items/bzzz_icecream.lua
@@ -0,0 +1,99 @@
+-- Register items to qb-core/shared/items.lua
+-- ice cream
+
+icecream_chr = { 
+    name = 'icecream_chr', 
+    label = 'Ice cream cherry', 
+    weight = 250, 
+    type = 'item', 
+    image = 'icecream_cherry.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Cool ice cream cherry' 
+},
+icecream_chc = { 
+    name = 'icecream_chc', 
+    label = 'Ice cream chocolate', 
+    weight = 250, 
+    type = 'item', 
+    image = 'icecream_chocolate.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Cool ice cream chocolate' 
+},
+icecream_lmn = { 
+    name = 'icecream_lmn', 
+    label = 'Ice cream lemon', 
+    weight = 250, 
+    type = 'item', 
+    image = 'icecream_lemon.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Cool ice cream lemon' 
+},
+icecream_pso = { 
+    name = 'icecream_pso', 
+    label = 'Ice cream pistachio', 
+    weight = 250, 
+    type = 'item', 
+    image = 'icecream_pistachio.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Cool ice cream pistachio' 
+},
+icecream_rpy = { 
+    name = 'icecream_rpy', 
+    label = 'Ice cream raspberry', 
+    weight = 250, 
+    type = 'item', 
+    image = 'icecream_raspberry.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Cool ice cream raspberry' 
+},
+icecream_ckd = { 
+    name = 'icecream_ckd', 
+    label = 'Ice cream stracciatella', 
+    weight = 250, 
+    type = 'item', 
+    image = 'icecream_stracciatella.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Cool ice cream stracciatella' 
+},
+icecream_sby = { 
+    name = 'icecream_sby', 
+    label = 'Ice cream strawberry', 
+    weight = 250, 
+    type = 'item', 
+    image = 'icecream_strawberry.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Cool ice cream strawberry' 
+},
+icecream_vna = { 
+    name = 'icecream_vna', 
+    label = 'Ice cream walnut', 
+    weight = 250, 
+    type = 'item', 
+    image = 'icecream_walnut.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Cool ice cream walnut' 
+},
