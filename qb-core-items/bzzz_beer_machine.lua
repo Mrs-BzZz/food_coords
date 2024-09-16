abf74df3f9516acf5e691c6de07e2a4d8e9bb413
BzZz <adelheid.kaszubi@gmail.com>
2024-09-16 15:12:55 +0200

    Create bzzz_beer_machine.lua

---
+++ qb-core-items/bzzz_beer_machine.lua
@@ -0,0 +1,75 @@
+-- Register items to qb-core/shared/items.lua
+-- beer
+
+beer_original = { 
+    name = 'beer_original', 
+    label = 'Original Beer', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_beer_original.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Nice cold beer' 
+},
+beer_apple = { 
+    name = 'beer_apple', 
+    label = 'Apple Beer', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_beer_apple.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Nice cold beer' 
+},
+beer_blueberry = { 
+    name = 'beer_blueberry', 
+    label = 'Blueberry Beer', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_beer_blueberry.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Nice cold beer' 
+},
+beer_kiwi = { 
+    name = 'beer_kiwi', 
+    label = 'Kiwi Beer', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_beer_kiwi.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Nice cold beer' 
+},
+beer_plum = { 
+    name = 'beer_plum', 
+    label = 'Plum Beer', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_beer_plum.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Nice cold beer' 
+},
+beer_raspberry = { 
+    name = 'beer_raspberry', 
+    label = 'Raspberry Beer', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_beer_raspberry.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Nice cold beer' 
+},
