e56af3ea5bc7084ce65c30261252eef708150ded
BzZz <adelheid.kaszubi@gmail.com>
2024-10-02 13:20:06 +0200

    Create bzzz_mixfood.lua

---
+++ qb-core-items/bzzz_mixfood.lua
@@ -0,0 +1,136 @@
+-- Register items to qb-core/shared/items.lua
+-- mixfood
+
+
+
+coffeecup_a = { 
+    name = 'coffeecup_a', 
+    label = 'Coffee Cup', 
+    weight = 200, 
+    type = 'item', 
+    image = 'coffeecup_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+
+bubbletea_a = {
+    name = 'bubbletea_a',
+    label = 'Bubble Tea',
+    weight = 250,
+    type = 'item',
+    image = 'bubbletea_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
+
+capisun_a = {
+    name = 'capisun_a',
+    label = 'Capisun',
+    weight = 250,
+    type = 'item',
+    image = 'capisun_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
+
+hotdog_a = {
+    name = 'hotdog_a',
+    label = 'Hotdog',
+    weight = 250,
+    type = 'item',
+    image = 'hotdog_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
+
+hotdog_b = {
+    name = 'hotdog_b',
+    label = 'Hotdog',
+    weight = 250,
+    type = 'item',
+    image = 'hotdog_b.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
+
+carneasada_a = {
+    name = 'carneasada_a',
+    label = 'Carne Asada',
+    weight = 250,
+    type = 'item',
+    image = 'carneasada_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
+
+nachos_a = {
+    name = 'nachos_a',
+    label = 'Nachos',
+    weight = 250,
+    type = 'item',
+    image = 'nachos_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
+
+kebab_a = {
+    name = 'kebab_a',
+    label = 'Kebab',
+    weight = 250,
+    type = 'item',
+    image = 'kebab_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
+
+burrito_a = {
+    name = 'burrito_a',
+    label = 'Burrito',
+    weight = 250,
+    type = 'item',
+    image = 'burrito_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
+
+churros_a = {
+    name = 'churros_a',
+    label = 'Churros',
+    weight = 250,
+    type = 'item',
+    image = 'churros_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
+
