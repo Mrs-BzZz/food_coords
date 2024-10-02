e7e265df8a6d7ae8688b8e4235def0b86217a67f
BzZz <adelheid.kaszubi@gmail.com>
2024-10-02 11:44:26 +0200

    Create bzzz_bakery.lua

---
+++ qb-core-items/bzzz_bakery.lua
@@ -0,0 +1,420 @@
+-- Register items to qb-core/shared/items.lua
+-- bakery
+
+toast_d = { 
+    name = 'toast_d', 
+    label = 'Sandwich', 
+    weight = 200, 
+    type = 'item', 
+    image = 'toast_d.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+toast_c = { 
+    name = 'toast_c', 
+    label = 'Burnt Toast', 
+    weight = 200, 
+    type = 'item', 
+    image = 'toast_c.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+toast_b = { 
+    name = 'toast_b', 
+    label = 'Baked Toast', 
+    weight = 200, 
+    type = 'item', 
+    image = 'toast_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+toast_a = { 
+    name = 'toast_a', 
+    label = 'Toast', 
+    weight = 200, 
+    type = 'item', 
+    image = 'toast_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+pretzel_a = { 
+    name = 'pretzel_a', 
+    label = 'Pretzel', 
+    weight = 200, 
+    type = 'item', 
+    image = 'pretzel_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+muffin_a = { 
+    name = 'muffin_a', 
+    label = 'Muffin', 
+    weight = 200, 
+    type = 'item', 
+    image = 'muffin_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Muffin'
+},
+
+lemoncake_a = { 
+    name = 'lemoncake_a', 
+    label = 'Lemon Cake', 
+    weight = 200, 
+    type = 'item', 
+    image = 'lemoncake_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+kaiserroll_a = { 
+    name = 'kaiserroll_a', 
+    label = 'Kaiser roll', 
+    weight = 200, 
+    type = 'item', 
+    image = 'kaiserroll_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+hdroll_a = { 
+    name = 'hdroll_a', 
+    label = 'Hotdog roll', 
+    weight = 200, 
+    type = 'item', 
+    image = 'hdroll_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+hdbuns_a = { 
+    name = 'hdbuns_a', 
+    label = 'Hotdog bun', 
+    weight = 200, 
+    type = 'item', 
+    image = 'hdbuns_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+doughnut_a = { 
+    name = 'doughnut_a', 
+    label = 'Sweet Doughnut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'doughnut_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+donut_a = { 
+    name = 'donut_a', 
+    label = 'Raspberry donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'donut_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+donut_b = { 
+    name = 'donut_b', 
+    label = 'Chocolate donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'donut_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+donut_c = { 
+    name = 'donut_c', 
+    label = 'Strawberry donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'donut_c.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+donut_d = { 
+    name = 'donut_d', 
+    label = 'Lavender donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'donut_d.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+donut_e = { 
+    name = 'donut_e', 
+    label = 'Mint donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'donut_e.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+donut_f = { 
+    name = 'donut_f', 
+    label = 'Banana donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'donut_f.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+donut_g = { 
+    name = 'donut_g', 
+    label = 'Milk donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'donut_g.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '
+},
+
+danish_a = { 
+    name = 'danish_a', 
+    label = 'Raisin swirls', 
+    weight = 200, 
+    type = 'item', 
+    image = 'danish_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+croissant_a = { 
+    name = 'croissant_a', 
+    label = 'Croissant', 
+    weight = 200, 
+    type = 'item', 
+    image = 'croissant_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+bread_d = { 
+    name = 'bread_d', 
+    label = 'Bread with salami', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bread_d.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+bread_c = { 
+    name = 'bread_c', 
+    label = 'Bread with butter', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bread_c.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+bread_b = { 
+    name = 'bread_b', 
+    label = 'Slice bread', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bread_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+bread_a = { 
+    name = 'bread_a', 
+    label = 'Bread', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bread_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+biscuit_a = { 
+    name = 'biscuit_a', 
+    label = 'Biscuit', 
+    weight = 200, 
+    type = 'item', 
+    image = 'biscuit_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+bananabread_c = { 
+    name = 'bananabread_c', 
+    label = 'Garnished banana bread', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bananabread_c.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+bananabread_b = { 
+    name = 'bananabread_b', 
+    label = 'Slice banana bread', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bananabread_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+bananabread_a = { 
+    name = 'bananabread_a', 
+    label = 'Banana bread', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bananabread_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+baguette_c = { 
+    name = 'baguette_c', 
+    label = 'Garnished baguette', 
+    weight = 200, 
+    type = 'item', 
+    image = 'baguette_c.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+baguette_b = { 
+    name = 'baguette_b', 
+    label = 'Small baguette', 
+    weight = 200, 
+    type = 'item', 
+    image = 'baguette_b.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+baguette_a = { 
+    name = 'baguette_a', 
+    label = 'Big baguette', 
+    weight = 200, 
+    type = 'item', 
+    image = 'baguette_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+bagel = { 
+    name = 'bagel', 
+    label = 'Bagel', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bagel_a.png', 
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' ' 
+},
+
+
