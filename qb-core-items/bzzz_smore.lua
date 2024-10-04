f9a788199188d750a0ae7db557a0227e8cafeacb
BzZz <adelheid.kaszubi@gmail.com>
2024-10-04 14:17:29 +0200

    Create bzzz_smore.lua

---
+++ qb-core-items/bzzz_smore.lua
@@ -0,0 +1,17 @@
+-- Register items to qb-core/shared/items.lua
+-- s'more
+
+
+
+desert = {
+    name = 'desert',
+    label = "S'more",
+    weight = 350,
+    type = 'item',
+    image = 'bzzz_food_dessert_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = ' ',
+},
