3ef11f4f46305db0d9d85bcf811fffcf9276d49a
BzZz <adelheid.kaszubi@gmail.com>
2024-12-04 20:20:00 +0100

    Create bzzz_popcorn.lua

---
+++ qb-core-items/bzzz_popcorn.lua
@@ -0,0 +1,81 @@
+-- Register items to qb-core/shared/items.lua
+-- popcorn
+
+
+bzzz_prop_popcorn_box_a = { 
+    name = 'bzzz_prop_popcorn_box_a', 
+    label = 'Small popcorn', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_popcorn_box_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+	
+bzzz_prop_popcorn_box_b = { 
+    name = 'bzzz_prop_popcorn_box_b', 
+    label = 'Medium popcorn', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_popcorn_box_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_prop_popcorn_box_c = { 
+    name = 'bzzz_prop_popcorn_box_c', 
+    label = 'Large popcorn', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_popcorn_box_c.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_prop_popcorn_drink_a = { 
+    name = 'bzzz_prop_popcorn_drink_a', 
+    label = 'Small Pop-Cola', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bzzz_prop_popcorn_drink_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_prop_popcorn_drink_b = { 
+    name = 'bzzz_prop_popcorn_drink_b', 
+    label = 'Medium Pop-Cola', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bzzz_prop_popcorn_drink_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_prop_popcorn_drink_c = { 
+    name = 'bzzz_prop_popcorn_drink_c', 
+    label = 'Large Pop-Cola', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bzzz_prop_popcorn_drink_c.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
