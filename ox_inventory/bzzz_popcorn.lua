18d0af4ee1d59170bd06b54b2031fa1e42d9b2ac
BzZz <adelheid.kaszubi@gmail.com>
2024-12-04 19:19:32 +0100

    Create bzzz_popcorn.lua

---
+++ ox_inventory/bzzz_popcorn.lua
@@ -0,0 +1,74 @@
+['bzzz_prop_popcorn_box_a'] = {
+        label = 'Small popcorn',
+        weight = 200,
+        stack = false,
+        close = true,
+        description = "Small popcorn",
+        client = {
+            status = { hunger = 200000 },
+            anim = { dict = 'bzzz_popcorn_animation', clip = 'bzzz_popcorn_animation' },
+
+            prop = { model = 'bzzz_prop_popcorn_box_a',
+            bone = 60309,
+            pos = vec3(0.1, 0.03, 0.09),
+            rot = vec3(-80.0, -14.0, 3.0) },
+
+            propTwo = { model = 'bzzz_prop_popcorn_piece',
+            bone = 28422,
+            pos = vec3(0.09, 0.06, -0.04),
+            rot = vec3(18.0, 45.0, 60.0) },
+
+            usetime = 17500,
+      			notification = 'Yum'
+        },
+    },	
+	
+['bzzz_prop_popcorn_box_b'] = {
+        label = 'Medium popcorn',
+        weight = 200,
+        stack = false,
+        close = true,
+        description = "Medium popcorn",
+        client = {
+            status = { hunger = 200000 },
+            anim = { dict = 'bzzz_popcorn_animation', clip = 'bzzz_popcorn_animation' },
+
+            prop = { model = 'bzzz_prop_popcorn_box_b',
+            bone = 60309,
+            pos = vec3(0.1, 0.03, 0.09),
+            rot = vec3(-80.0, -14.0, 3.0) },
+
+            propTwo = { model = 'bzzz_prop_popcorn_piece',
+            bone = 28422,
+            pos = vec3(0.09, 0.06, -0.04),
+            rot = vec3(18.0, 45.0, 60.0) },
+
+            usetime = 17500,
+      			notification = 'Yum'
+        },
+    },
+	
+['bzzz_prop_popcorn_box_c'] = {
+        label = 'Large popcorn',
+        weight = 200,
+        stack = false,
+        close = true,
+        description = "Large popcorn",
+        client = {
+            status = { hunger = 200000 },
+            anim = { dict = 'bzzz_popcorn_animation', clip = 'bzzz_popcorn_animation' },
+
+            prop = { model = 'bzzz_prop_popcorn_box_c',
+            bone = 60309,
+            pos = vec3(0.13, -0.01, 0.11),
+            rot = vec3(-80.0, -14.0, 3.0) },
+
+            propTwo = { model = 'bzzz_prop_popcorn_piece',
+            bone = 28422,
+            pos = vec3(0.09, 0.06, -0.04),
+            rot = vec3(18.0, 45.0, 60.0) },
+
+            usetime = 17500,
+		       	notification = 'Yum'
+        },
+    },	
