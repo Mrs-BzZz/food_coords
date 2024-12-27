862b360996b8808574d80054969fdf70d779c3d8
BzZz <adelheid.kaszubi@gmail.com>
2024-12-27 22:04:39 +0100

    Create bzzz_grillfood.lua

---
+++ qb-core-items/bzzz_grillfood.lua
@@ -0,0 +1,311 @@
+-- Register items to qb-core/shared/items.lua
+-- grill food
+
+bzzz_food_grill_zucchini_grill_a = { 
+    name = 'bzzz_food_grill_zucchini_grill_a', 
+    label = 'Grilled zucchini', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_food_grill_zucchini_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+bzzz_food_grill_tofu_grill_a = { 
+    name = 'bzzz_food_grill_tofu_grill_a', 
+    label = 'Grilled tofu', 
+    weight = 100, 
+    type = 'item', 
+    image = 'bzzz_food_grill_tofu_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_steak_grill_d = { 
+    name = 'bzzz_food_grill_steak_grill_d', 
+    label = 'Ossobuco', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bzzz_food_grill_steak_grill_d.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_steak_grill_c = { 
+    name = 'bzzz_food_grill_steak_grill_c', 
+    label = 'T-Bone Steak', 
+    weight = 400, 
+    type = 'item', 
+    image = 'bzzz_food_grill_steak_grill_c.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_steak_grill_b = { 
+    name = 'bzzz_food_grill_steak_grill_b', 
+    label = 'Tomahawk steak', 
+    weight = 250, 
+    type = 'item', 
+    image = 'bzzz_food_grill_steak_grill_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+bzzz_food_grill_steak_grill_a = { 
+    name = 'bzzz_food_grill_steak_grill_a', 
+    label = 'Grilled steak', 
+    weight = 250, 
+    type = 'item', 
+    image = 'bzzz_food_grill_steak_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_skewer_grill_a = { 
+    name = 'bzzz_food_grill_skewer_grill_a', 
+    label = 'Grilled meat', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_grill_skewer_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_sausage_grill_a = { 
+    name = 'bzzz_food_grill_sausage_grill_a', 
+    label = 'Grilled sausage', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_grill_sausage_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_salmon_grill_a = { 
+    name = 'bzzz_food_grill_salmon_grill_a', 
+    label = 'Grilled salmon', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_grill_salmon_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_ribs_grill_b = { 
+    name = 'bzzz_food_grill_ribs_grill_b', 
+    label = 'Grilled rib', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_grill_ribs_grill_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_potato_grill_a = { 
+    name = 'bzzz_food_grill_potato_grill_a', 
+    label = 'Grilled potato', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_grill_potato_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+bzzz_food_grill_pineapple_grill_a = { 
+    name = 'bzzz_food_grill_pineapple_grill_a', 
+    label = 'Grilled pineapple', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_grill_pineapple_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_naan_grill_a = { 
+    name = 'bzzz_food_grill_naan_grill_a', 
+    label = 'Grilled naan', 
+    weight = 350, 
+    type = 'item', 
+    image = 'bzzz_food_grill_naan_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_mushrooms_grill_a = { 
+    name = 'bzzz_food_grill_mushrooms_grill_a', 
+    label = 'Grilled mushrooms', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_food_grill_mushrooms_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_halloumi_grill_a = { 
+    name = 'bzzz_food_grill_halloumi_grill_a', 
+    label = 'Grilled halloumi', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_food_grill_halloumi_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_fish_grill_a = { 
+    name = 'bzzz_food_grill_fish_grill_a', 
+    label = 'Grilled fish', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_food_grill_fish_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_corn_grill_a = { 
+    name = 'bzzz_food_grill_corn_grill_a', 
+    label = 'Grilled corn', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_food_grill_corn_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_chicken_grill_d = { 
+    name = 'bzzz_food_grill_chicken_grill_d', 
+    label = 'Grilled chicken steak', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_food_grill_chicken_grill_d.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_chicken_grill_c = { 
+    name = 'bzzz_food_grill_chicken_grill_c', 
+    label = 'Grilled chicken wing', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_food_grill_chicken_grill_c.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_chicken_grill_b = { 
+    name = 'bzzz_food_grill_chicken_grill_b', 
+    label = 'Grilled chicken thigh', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_food_grill_chicken_grill_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_camembert_grill_a = { 
+    name = 'bzzz_food_grill_camembert_grill_a', 
+    label = 'Grilled camembert', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_food_grill_camembert_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_burger_grill_d = { 
+    name = 'bzzz_food_grill_burger_grill_d', 
+    label = 'Grilled burger', 
+    weight = 600, 
+    type = 'item', 
+    image = 'bzzz_food_grill_burger_grill_d.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_baguette_grill_c = { 
+    name = 'bzzz_food_grill_baguette_grill_c', 
+    label = 'Grilled chicken baguette', 
+    weight = 600, 
+    type = 'item', 
+    image = 'bzzz_food_grill_baguette_grill_c.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_grill_bacon_grill_a = { 
+    name = 'bzzz_food_grill_bacon_grill_a', 
+    label = 'Grilled bacon', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_food_grill_bacon_grill_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
