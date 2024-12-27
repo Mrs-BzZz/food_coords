4d2033c0efc73252133817c9676d92ce50840a7c
BzZz <adelheid.kaszubi@gmail.com>
2024-12-27 20:45:42 +0100

    Create bzzz_grillfood.lua

---
+++ ox_inventory/bzzz_grillfood.lua
@@ -0,0 +1,266 @@
+
+['bzzz_food_grill_zucchini_grill_a'] = {
+		label = 'Grilled zucchini',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_zucchini_grill_a`, pos = vec3(0.04, -0.02, -0.05), rot = vec3(30.0, -30.0, 65.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_tofu_grill_a'] = {
+		label = 'Grilled tofu',
+		weight = 100,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_tofu_grill_a`, pos = vec3(0.04, -0.02, -0.03), rot = vec3(15.0, -30.0, 65.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_steak_grill_d'] = {
+		label = 'Ossobuco',
+		weight = 500,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_steak_grill_d`, pos = vec3(0.03, -0.04, -0.02), rot = vec3(30.0, 0.0, -8.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_steak_grill_c'] = {
+		label = 'T-Bone Steak',
+		weight = 400,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_steak_grill_c`, pos = vec3(0.06, -0.04, -0.02), rot = vec3(24.0, 30.0, -62.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_steak_grill_b'] = {
+		label = 'Tomahawk steak',
+		weight = 250,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_steak_grill_b`, pos = vec3(0.03, -0.03, -0.02), rot = vec3(18.0, -35.0, 64.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_steak_grill_a'] = {
+		label = 'Grilled steak',
+		weight = 250,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_steak_grill_a`, pos = vec3(0.03, -0.03, -0.02), rot = vec3(18.0, -35.0, 64.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_skewer_grill_a'] = {
+		label = 'Grilled meat',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_skewer_grill_a`, pos = vec3(0.04, -0.02, -0.03), rot = vec3(15.0, -30.0, 65.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_sausage_grill_a'] = {
+		label = 'Grilled sausage',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_sausage_grill_a`, pos = vec3(0.04, -0.02, -0.03), rot = vec3(15.0, -30.0, 65.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_salmon_grill_a'] = {
+		label = 'Grilled salmon',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_salmon_grill_a`, pos = vec3(0.0, -0.02, -0.02), rot = vec3(25.0, -35.0, 64.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_ribs_grill_b'] = {
+		label = 'Grilled rib',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_ribs_grill_b`, pos = vec3(0.01, -0.01, -0.01), rot = vec3(-62.0, 5.0, 174.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_potato_grill_a'] = {
+		label = 'Grilled potato',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_potato_grill_a`, pos = vec3(-0.03, 0.0, -0.01), rot = vec3(2.0, -15.0, 106.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_pineapple_grill_a'] = {
+		label = 'Grilled pineapple',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_pineapple_grill_a`, pos = vec3(-0.03, 0.0, -0.01), rot = vec3(2.0, -15.0, 106.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_naan_grill_a'] = {
+		label = 'Grilled naan',
+		weight = 350,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_naan_grill_a`, pos = vec3(0.03, -0.06, -0.04), rot = vec3(32.0, 5.0, 25.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_mushrooms_grill_a'] = {
+		label = 'Grilled mushrooms',
+		weight = 300,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_mushrooms_grill_a`, pos = vec3(0.04, -0.02, -0.03), rot = vec3(15.0, -30.0, 65.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_halloumi_grill_a'] = {
+		label = 'Grilled halloumi',
+		weight = 300,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_halloumi_grill_a`, pos = vec3(-0.03, 0.0, 0.0), rot = vec3(22.0, -25.0, 85.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_fish_grill_a'] = {
+		label = 'Grilled fish',
+		weight = 300,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_fish_grill_a`, pos = vec3(0.06, 0.00, -0.04), rot = vec3(-20.0, -16.0, -83.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_corn_grill_a'] = {
+		label = 'Grilled corn',
+		weight = 300,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_corn_grill_a`, pos = vec3(0.04, -0.02, -0.03), rot = vec3(15.0, -30.0, 65.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},	
+['bzzz_food_grill_chicken_grill_d'] = {
+		label = 'Grilled chicken steak',
+		weight = 300,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_chicken_grill_d`, pos = vec3(-0.01, 0.0, 0.0), rot = vec3(-20.0, -16.0, -83.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_chicken_grill_c'] = {
+		label = 'Grilled chicken wing',
+		weight = 300,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_chicken_grill_c`, pos = vec3(-0.01, 0.0, 0.0), rot = vec3(-20.0, -16.0, -83.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_chicken_grill_b'] = {
+		label = 'Grilled chicken thigh',
+		weight = 300,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_chicken_grill_b`, pos = vec3(-0.01, 0.0, 0.0), rot = vec3(-20.0, -16.0, -83.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_camembert_grill_a'] = {
+		label = 'Grilled camembert',
+		weight = 300,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_camembert_grill_a`, pos = vec3(-0.01, 0.0, 0.0), rot = vec3(-20.0, -16.0, -83.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_burger_grill_d'] = {
+		label = 'Grilled burger',
+		weight = 600,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_burger_grill_d`, pos = vec3(0.0, -0.02, -0.03), rot = vec3(0.0, 0.0, -50.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_baguette_grill_c'] = {
+		label = 'Grilled chicken baguette',
+		weight = 600,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_baguette_grill_c`, pos = vec3(0.07, 0.01, -0.02), rot = vec3(0.15, 0.0, -60.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+['bzzz_food_grill_bacon_grill_a'] = {
+		label = 'Grilled bacon',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_food_grill_bacon_grill_a`, pos = vec3(0.02, -0.01, -0.01), rot = vec3(0.0, 0.0, 80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+	
