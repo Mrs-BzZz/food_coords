8ea50cdc6ba2068ccacd8a18503a34de2c2b8f07
BzZz <adelheid.kaszubi@gmail.com>
2025-01-06 18:12:50 +0100

    Create bzzz_sugary.lua

---
+++ ox_inventory/bzzz_sugary.lua
@@ -0,0 +1,360 @@
+['bzzz_sugary_alfajores_a'] = {
+		label = 'Alfajores',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_alfajores_a`, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_applepie_b'] = {
+		label = 'Apple pie',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_applepie_b`, pos = vec3(-0.03, 0.01, 0.0), rot = vec3(-30.0, 0.0, 160.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_baklava_a'] = {
+		label = 'Baklava cake',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_baklava_a`, pos = vec3(0.0, -0.01, -0.01), rot = vec3(-150.0, -20.0, 30.0) },
+			usetime = 3000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_bar_marte_a'] = {
+		label = 'Marte bar',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_bar_marte_a`, pos = vec3(0.0, 0.0, 0.0), rot = vec3(10.0, 0.0, 0.0) },
+			usetime = 3500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_bar_snackbar_a'] = {
+		label = 'Snackbar',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_bar_snackbar_a`, pos = vec3(0.0, 0.0, 0.0), rot = vec3(10.0, 0.0, 0.0) },
+			usetime = 3500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_berliner_a'] = {
+		label = 'Berliner',
+		weight = 80,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_berliner_a`, pos = vec3(0.0, 0.0, -0.01), rot = vec3(10.0, 0.0, 0.0) },
+			usetime = 3500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_brownies_a'] = {
+		label = 'Brownies',
+		weight = 150,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_brownies_a`, pos = vec3(-0.02, 0.0, -0.02), rot = vec3(10.0, 0.0, 0.0) },
+			usetime = 3500, 
+			notification = 'Bon appétit!'
+		}
+	},
+	
+['bzzz_sugary_cake_a'] = {
+		label = 'Chocolate cake',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_cake_a`, pos = vec3(-0.02, 0.0, -0.02), rot = vec3(10.0, 7.0, -42.0) },
+			usetime = 3500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_cake_b'] = {
+		label = 'Strawberry cake',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_cake_b`, pos = vec3(-0.02, 0.0, -0.02), rot = vec3(10.0, 7.0, -42.0) },
+			usetime = 3500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_candy_a'] = {
+		label = 'Rainbow candies',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_sugary_candy_a`, pos = vec3(-0.05, 0.02, 0.0), rot = vec3(0.0, 0.0, -90.0) },
+			usetime = 3500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_cheesecake_a'] = { 
+		label = 'Cheesecake',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_cheesecake_a`, pos = vec3(0.0, 0.02, -0.02), rot = vec3(16.0, 16.0, -18.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_chocolate_a'] = {
+		label = 'Milk chocolate',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_chocolate_a`, pos = vec3(0.04, -0.02, -0.01), rot = vec3(0.0, -24.0, 72.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_chocolate_b'] = {
+		label = 'White chocolate',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_chocolate_b`, pos = vec3(0.04, -0.02, -0.01), rot = vec3(0.0, -24.0, 72.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_chocothrone_a'] = {
+		label = 'Chocothrone',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_chocothrone_a`, pos = vec3(0.05, -0.01, 0.0), rot = vec3(1.0, -4.0, -8.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_cookie_a'] = {
+		label = 'Cookie',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_cookie_a`, pos = vec3(-0.01, -0.01, 0.0), rot = vec3(25.0, 2.0, -8.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_cookie_b'] = {
+		label = 'Cookie',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_cookie_b`, pos = vec3(-0.01, -0.01, 0.0), rot = vec3(25.0, 2.0, -8.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_lollipop_a'] = {
+		label = 'Lollipop',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_lollipop_a`, pos = vec3(0.0, -0.01, 0.0), rot = vec3(-81.0, -42.0, 76.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_lollipop_b'] = {
+		label = 'Lollipop',
+		weight = 200,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_lollipop_b`, pos = vec3(0.0, -0.01, 0.0), rot = vec3(-101.0, -42.0, 76.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_macaron_a'] = {
+		label = 'Macaron',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_macaron_a`, pos = vec3(-0.03, -0.01, 0.0), rot = vec3(-11.0, -180.0, 76.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_macaron_b'] = {
+		label = 'Macaron',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_macaron_b`, pos = vec3(-0.03, -0.01, 0.0), rot = vec3(-11.0, -180.0, 76.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_macaron_c'] = {
+		label = 'Macaron',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_macaron_c`, pos = vec3(-0.03, -0.01, 0.0), rot = vec3(-11.0, -180.0, 76.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_macaron_d'] = {
+		label = 'Macaron',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_macaron_d`, pos = vec3(-0.03, -0.01, 0.0), rot = vec3(-11.0, -180.0, 76.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_mochi_a'] = {
+		label = 'Green mochi',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_mochi_a`, pos = vec3(-0.01, 0.0, -0.02), rot = vec3(39.0, -10.0, 46.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_pancakes_a'] = {
+		label = 'Pancake',
+		weight = 100,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_pancakes_a`, pos = vec3(-0.03, -0.01, 0.0), rot = vec3(40.0, -64.0, 52.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_redvelvet_a'] = {
+		label = 'Red Velvet cake',
+		weight = 100,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_redvelvet_a`, pos = vec3(0.0, 0.02, -0.02), rot = vec3(16.0, 16.0, -18.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_tiramisu_a'] = {
+		label = 'Tiramisu',
+		weight = 100,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_tiramisu_a`, pos = vec3(-0.02, 0.0, -0.01), rot = vec3(20.0, -32.0, 82.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_treslechescake_a'] = {
+		label = 'Tres Leches cake',
+		weight = 100,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_treslechescake_a`, pos = vec3(-0.02, 0.0, -0.01), rot = vec3(20.0, -32.0, 82.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_truffles_a'] = {
+		label = 'Truffle',
+		weight = 20,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_truffles_a`, pos = vec3(-0.02, 0.0, -0.01), rot = vec3(70.0, -64.0, 52.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_wafer_a'] = {
+		label = 'Wafer',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_wafer_a`, pos = vec3(0.02, -0.04, -0.06), rot = vec3(20.0, -44.0, 72.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_sugary_wafer_b'] = {
+		label = 'Wafer',
+		weight = 50,
+		client = {
+			status = { thirst = 100000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_sugary_wafer_b`, pos = vec3(-0.02, -0.01, -0.03), rot = vec3(20.0, -44.0, 72.0) },
+			usetime = 4500, 
+			notification = 'Bon appétit!'
+		}
+	},
+
