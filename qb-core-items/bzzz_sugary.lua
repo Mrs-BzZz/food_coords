2a6986e4bb0d0ee41e662f90d206e163f5710006
BzZz <adelheid.kaszubi@gmail.com>
2025-01-06 18:19:44 +0100

    Create bzzz_sugary.lua

---
+++ qb-core-items/bzzz_sugary.lua
@@ -0,0 +1,392 @@
+-- Register items to qb-core/shared/items.lua
+-- sugary
+
+bzzz_sugary_alfajores_a = { 
+    name = 'bzzz_sugary_alfajores_a', 
+    label = 'Alfajores', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_alfajores_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_applepie_b = { 
+    name = 'bzzz_sugary_applepie_b', 
+    label = 'Apple pie', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_applepie_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_baklava_a = { 
+    name = 'bzzz_sugary_baklava_a', 
+    label = 'Baklava cake', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_baklava_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_bar_marte_a = { 
+    name = 'bzzz_sugary_bar_marte_a', 
+    label = 'Marte bar', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_bar_marte_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_bar_snackbar_a = { 
+    name = 'bzzz_sugary_bar_snackbar_a', 
+    label = 'Snackbar', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_bar_snackbar_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_berliner_a = { 
+    name = 'bzzz_sugary_berliner_a', 
+    label = 'Berliner', 
+    weight = 80, 
+    type = 'item', 
+    image = 'bzzz_sugary_berliner_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_brownies_a = { 
+    name = 'bzzz_sugary_brownies_a', 
+    label = 'Brownies', 
+    weight = 150, 
+    type = 'item', 
+    image = 'bzzz_sugary_brownies_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_cake_a = { 
+    name = 'bzzz_sugary_cake_a', 
+    label = 'Chocolate cake', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_cake_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_cake_b = { 
+    name = 'bzzz_sugary_cake_b', 
+    label = 'Strawberry cake', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_cake_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_candy_a = { 
+    name = 'bzzz_sugary_candy_a', 
+    label = 'Rainbow candies', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_candy_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_cheesecake_a = { 
+    name = 'bzzz_sugary_cheesecake_a', 
+    label = 'Cheesecake', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_cheesecake_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_chocolate_a = { 
+    name = 'bzzz_sugary_chocolate_a', 
+    label = 'Milk chocolate', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_chocolate_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_chocolate_b = { 
+    name = 'bzzz_sugary_chocolate_b', 
+    label = 'White chocolate', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_chocolate_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_chocothrone_a = { 
+    name = 'bzzz_sugary_chocothrone_a', 
+    label = 'Chocothrone', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_chocothrone_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_cookie_a = { 
+    name = 'bzzz_sugary_cookie_a', 
+    label = 'Cookie', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_cookie_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_cookie_b = { 
+    name = 'bzzz_sugary_cookie_b', 
+    label = 'Cookie', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_cookie_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_lollipop_a = { 
+    name = 'bzzz_sugary_lollipop_a', 
+    label = 'Lollipop', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_lollipop_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_lollipop_b = { 
+    name = 'bzzz_sugary_lollipop_b', 
+    label = 'Lollipop', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_sugary_lollipop_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_macaron_a = { 
+    name = 'bzzz_sugary_macaron_a', 
+    label = 'Macaron', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_macaron_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_macaron_b = { 
+    name = 'bzzz_sugary_macaron_b', 
+    label = 'Macaron', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_macaron_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_macaron_c = { 
+    name = 'bzzz_sugary_macaron_c', 
+    label = 'Macaron', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_macaron_c.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_macaron_d = { 
+    name = 'bzzz_sugary_macaron_d', 
+    label = 'Macaron', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_macaron_d.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_mochi_a = { 
+    name = 'bzzz_sugary_mochi_a', 
+    label = 'Green mochi', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_mochi_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_pancakes_a = { 
+    name = 'bzzz_sugary_pancakes_a', 
+    label = 'Pancake', 
+    weight = 100, 
+    type = 'item', 
+    image = 'bzzz_sugary_pancakes_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_redvelvet_a = { 
+    name = 'bzzz_sugary_redvelvet_a', 
+    label = 'Red Velvet cake', 
+    weight = 100, 
+    type = 'item', 
+    image = 'bzzz_sugary_redvelvet_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_tiramisu_a = { 
+    name = 'bzzz_sugary_tiramisu_a', 
+    label = 'Tiramisu', 
+    weight = 100, 
+    type = 'item', 
+    image = 'bzzz_sugary_tiramisu_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_treslechescake_a = { 
+    name = 'bzzz_sugary_treslechescake_a', 
+    label = 'Tres Leches cake', 
+    weight = 100, 
+    type = 'item', 
+    image = 'bzzz_sugary_treslechescake_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_truffles_a = { 
+    name = 'bzzz_sugary_truffles_a', 
+    label = 'Truffle', 
+    weight = 20, 
+    type = 'item', 
+    image = 'bzzz_sugary_truffles_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_wafer_a = { 
+    name = 'bzzz_sugary_wafer_a', 
+    label = 'Wafer', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_wafer_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_sugary_wafer_b = { 
+    name = 'bzzz_sugary_wafer_b', 
+    label = 'Wafer', 
+    weight = 50, 
+    type = 'item', 
+    image = 'bzzz_sugary_wafer_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
