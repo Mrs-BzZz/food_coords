c4ff14342491c8e36e1ad040fe3627af8d7305d3
BzZz <adelheid.kaszubi@gmail.com>
2025-01-26 18:43:51 +0100

    Create bzzz_dinomeal.lua

---
+++ qb-core-items/bzzz_dinomeal.lua
@@ -0,0 +1,171 @@
+-- Register items to qb-core/shared/items.lua
+-- dino meal
+
+bzzz_food_dino_burger_a = { 
+    name = 'bzzz_food_dino_burger_a', 
+    label = 'Dino burger', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_burger_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_chocolate_b = { 
+    name = 'bzzz_food_dino_chocolate_b', 
+    label = 'Dino chocolate', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_chocolate_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_cupcake_a = { 
+    name = 'bzzz_food_dino_cupcake_a', 
+    label = 'Dino cupcake', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_cupcake_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_donut_b = { 
+    name = 'bzzz_food_dino_donut_b', 
+    label = 'Dino donut', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_donut_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_icecream_a = { 
+    name = 'bzzz_food_dino_icecream_a', 
+    label = 'Dino icecream', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_icecream_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_pizza_b = { 
+    name = 'bzzz_food_dino_pizza_b', 
+    label = 'Dino pizza', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_pizza_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_steak_b = { 
+    name = 'bzzz_food_dino_steak_b', 
+    label = 'Dino steak', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_steak_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_drink_a = { 
+    name = 'bzzz_food_dino_drink_a', 
+    label = 'Dino drink', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_drink_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_drink_b = { 
+    name = 'bzzz_food_dino_drink_b', 
+    label = 'Dino sprunk', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_drink_b.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_drink_c = { 
+    name = 'bzzz_food_dino_drink_c', 
+    label = 'Dino cola', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_drink_c.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_drink_d = { 
+    name = 'bzzz_food_dino_drink_d', 
+    label = 'Dino orange', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_drink_d.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_fries_a = { 
+    name = 'bzzz_food_dino_fries_a', 
+    label = 'Dino fries', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_fries_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_food_dino_nuggets_a = { 
+    name = 'bzzz_food_dino_nuggets_a', 
+    label = 'Dino nuggets', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_food_dino_nuggets_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
