16fe631105ad44a43ef2a0625fce7621b3f70e02
BzZz <adelheid.kaszubi@gmail.com>
2025-01-26 20:24:44 +0100

    Create bzzz_dinomeal.lua

---
+++ bzzz_usableitems/bzzz_dinomeal.lua
@@ -0,0 +1,208 @@
+-- EN: Pay attention to which section you copy the code to. If you paste the code incorrectly, it won't work properly!
+-- FR: Faites attention à la section dans laquelle vous copiez le code. Si vous collez le code de manière incorrecte, il ne fonctionnera pas correctement !
+-- DE: Achten Sie darauf, in welchen Abschnitt Sie den Code kopieren. Wenn Sie den Code falsch einfügen, wird er nicht richtig funktionieren!
+
+
+--------- FOOD SECTION ------
+["bzzz_food_dino_burger_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+		animationName = "mp_player_int_eat_burger"
+		},
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_burger_a", 
+        xPos = -0.02, yPos = -0.01, zPos = 0.0, 
+		xRot = 30.0, yRot = 0.0, zRot = 0.0
+    }
+},		
+["bzzz_food_dino_chocolate_b"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_chocolate_b", 
+        xPos = -0.01, yPos = 0.02, zPos = -0.02, 
+        xRot = 10.0, yRot = -70.0, zRot = 0.0
+    }
+},
+["bzzz_food_dino_cupcaket_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_cupcake_a", 
+        xPos = -0.01, yPos = 0.02, zPos = -0.03, 
+        xRot = 33.0, yRot = -10.0, zRot = -8.0
+    }
+},
+
+["bzzz_food_dino_donut_b"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_donut_b", 
+        xPos = -0.03, yPos = -0.01, zPos = -0.02, 
+        xRot = 33.0, yRot = -10.0, zRot = -8.0
+    }
+},
+
+["bzzz_food_dino_icecream_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_icecream_a", 
+        xPos = 0.03, yPos = 0.0, zPos = -0.03, 
+        xRot = 0.0, yRot = -80.0, zRot = 0.0
+    }
+},
+
+["bzzz_food_dino_pizza_b"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_pizza_b", 
+        xPos = 0.05, yPos = -0.02, zPos = 0.0, 
+        xRot = -20.0, yRot = -20.0, zRot = 230.0
+    }
+},
+
+["bzzz_food_dino_steak_b"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_steak_b", 
+        xPos = -0.02, yPos = -0.01, zPos = -0.01, 
+        xRot = -20.0, yRot = -20.0, zRot = 230.0
+    }
+},
+
+
+
+
+
+
+
+--------- DRINK SECTION ------
+["bzzz_food_dino_drink_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_intdrink", 
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_drink_a", 
+        xPos = 0.03, yPos = 0.02, zPos = 0.07, 
+        xRot = 32.0, yRot = 6.0, zRot = -64.0
+    }
+},
+
+["bzzz_food_dino_drink_b"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_intdrink", 
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_drink_b", 
+        xPos = 0.03, yPos = 0.02, zPos = 0.07, 
+        xRot = 32.0, yRot = 6.0, zRot = -64.0
+    }
+},
+
+["bzzz_food_dino_drink_c"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_intdrink", 
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_drink_c", 
+        xPos = 0.03, yPos = 0.02, zPos = 0.07, 
+        xRot = 32.0, yRot = 6.0, zRot = -64.0
+    }
+},
+
+["bzzz_food_dino_drink_d"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_intdrink", 
+        animationName = "loop_bottle"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_dino_drink_d", 
+        xPos = 0.03, yPos = 0.02, zPos = 0.07, 
+        xRot = 32.0, yRot = 6.0, zRot = -64.0
+    }
+},
+
+
+
+
+
+--------- FOODPLATES SECTION ------
+['bzzz_food_dino_fries_a'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_food_dino_anim",
+                animationNameEat = "bzzz_food_dino_anim",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_food_dino_fries_a", 
+                xPos = 0.12, yPos = 0.0, zPos = 0.05, xRot = -99.0, yRot = -34.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_food_dino_fries_b", 
+                xPos = 0.08, yPos = 0.04, zPos = -0.03, xRot = -131.0, yRot = 15.0, zRot = -46.0
+            }
+        },
+['bzzz_food_dino_nuggets_a'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_food_dino_anim",
+                animationNameEat = "bzzz_food_dino_anim",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_food_dino_nuggets_a", 
+                xPos = 0.12, yPos = 0.0, zPos = 0.06, xRot = -99.0, yRot = -34.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_food_dino_nuggets_b", 
+                xPos = 0.09, yPos = 0.05, zPos = -0.03, xRot = -50.0, yRot = 15.0, zRot = -28.0
+            }
+        },
