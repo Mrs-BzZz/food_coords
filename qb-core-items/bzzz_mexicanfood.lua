cec370be10b7216c1bb0588c7b3f611205c888cb
BzZz <adelheid.kaszubi@gmail.com>
2025-02-26 04:39:22 +0100

    Create bzzz_mexicanfood.lua

---
+++ qb-core-items/bzzz_mexicanfood.lua
@@ -0,0 +1,210 @@
+-- Register items to qb-core/shared/items.lua
+-- mexican
+
+bzzz_mexicanfood_burrito_a = { 
+    name = 'bzzz_mexicanfood_burrito_a', 
+    label = 'Burrito', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_burrito_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Mexican burrito'  
+},
+
+bzzz_mexicanfood_caldoderes_a = { 
+    name = 'bzzz_mexicanfood_caldoderes_a', 
+    label = 'Caldo de Res', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_caldoderes_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Mexican beef and vegetable soup'  
+},
+
+bzzz_mexicanfood_camotes_a = { 
+    name = 'bzzz_mexicanfood_camotes_a', 
+    label = 'Camotes', 
+    weight = 400, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_camotes_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Mexican sweet potatoes'  
+},
+
+bzzz_mexicanfood_ceviche_a = { 
+    name = 'bzzz_mexicanfood_ceviche_a', 
+    label = 'Ceviche', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_ceviche_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Dish made of fresh raw fish'  
+},
+
+bzzz_mexicanfood_chilaquiles_a = { 
+    name = 'bzzz_mexicanfood_chilaquiles_a', 
+    label = 'Chilaquiles', 
+    weight = 400, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_chilaquiles_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Traditional Mexican food'  
+},
+
+bzzz_mexicanfood_elote_a = { 
+    name = 'bzzz_mexicanfood_elote_a', 
+    label = 'Elote', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_elote_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Corn on the cob charred on the grill'  
+},
+
+bzzz_mexicanfood_enchiladas_a = { 
+    name = 'bzzz_mexicanfood_enchiladas_a', 
+    label = 'Enchilada', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_enchiladas_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Corn tortillas'  
+},
+
+bzzz_mexicanfood_fajitas_a = { 
+    name = 'bzzz_mexicanfood_fajitas_a', 
+    label = 'Fajita', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_fajitas_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Meat mixture in tortilla'  
+},
+
+bzzz_mexicanfood_flan_a = { 
+    name = 'bzzz_mexicanfood_flan_a', 
+    label = 'Flan', 
+    weight = 100, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_flan_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Caramel dessert'  
+},
+
+bzzz_mexicanfood_gorditas_a = { 
+    name = 'bzzz_mexicanfood_gorditas_a', 
+    label = 'Gordita', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_gorditas_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Corn cakes made with masa dough'  
+},
+
+bzzz_mexicanfood_guacamole_a = { 
+    name = 'bzzz_mexicanfood_guacamole_a', 
+    label = 'Guacamole', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_guacamole_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Avocado sauce'  
+},
+
+bzzz_mexicanfood_menudo_a = { 
+    name = 'bzzz_mexicanfood_menudo_a', 
+    label = 'Menudo', 
+    weight = 500, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_menudo_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = "Mexican soup, made with cow's stomach in broth with a red chili pepper base"  
+},
+
+bzzz_mexicanfood_quesadilla_a = { 
+    name = 'bzzz_mexicanfood_quesadilla_a', 
+    label = 'Quesadilla', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_quesadilla_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Mexican tortilla with cheese'  
+},
+
+bzzz_mexicanfood_sopes_a = { 
+    name = 'bzzz_mexicanfood_sopes_a', 
+    label = 'Sope', 
+    weight = 100, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_sopes_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = "Traditional Mexican dish consisting of a fried masa base with savory toppings"  
+},
+
+bzzz_mexicanfood_taco_a = { 
+    name = 'bzzz_mexicanfood_taco_a', 
+    label = 'Taco', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_taco_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Tortilla pancake with filling'  
+},
+
+bzzz_mexicanfood_tortas_a = { 
+    name = 'bzzz_mexicanfood_tortas_a', 
+    label = 'Torta', 
+    weight = 300, 
+    type = 'item', 
+    image = 'bzzz_mexicanfood_tortas_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = 'Mexican sandwich'  
+},
