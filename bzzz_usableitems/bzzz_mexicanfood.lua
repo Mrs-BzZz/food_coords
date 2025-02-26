860756c0669b41567ab98dd4af7dee489c53f37c
BzZz <adelheid.kaszubi@gmail.com>
2025-02-26 05:49:08 +0100

    Create bzzz_mexicanfood.lua

---
+++ bzzz_usableitems/bzzz_mexicanfood.lua
@@ -0,0 +1,272 @@
+--------- FOOD SECTION ------
+["bzzz_mexicanfood_burrito_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+		animationName = "mp_player_int_eat_burger"
+		},
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_burrito_a", 
+        xPos = 0.06, yPos = -0.02, zPos = -0.03, 
+		xRot = -127.0, yRot = -50.0, zRot = 70.0
+    }
+},		
+
+["bzzz_mexicanfood_elote_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_elote_a", 
+        xPos = -0.03, yPos = 0.0, zPos = -0.01, 
+        xRot = 2.0, yRot = 6.0, zRot = 86.0
+    }
+},
+
+["bzzz_mexicanfood_enchiladas_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_enchiladas_a", 
+        xPos = -0.03, yPos = 0.0, zPos = -0.01, 
+        xRot = 2.0, yRot = 6.0, zRot = 86.0
+    }
+},
+
+["bzzz_mexicanfood_fajitas_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_fajitas_a", 
+        xPos = -0.03, yPos = 0.0, zPos = -0.01, 
+        xRot = 2.0, yRot = 6.0, zRot = 86.0
+    }
+},
+
+["bzzz_mexicanfood_gorditas_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_gorditas_a", 
+        xPos = -0.01, yPos = 0.01, zPos = -0.01, 
+        xRot = -40.0, yRot = -44.0, zRot = 76.0
+    }
+},
+
+["bzzz_mexicanfood_quesadilla_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_quesadilla_a", 
+        xPos = -0.01, yPos = -0.03, zPos = -0.05, 
+        xRot = -30.0, yRot = -150.0, zRot = 0.0
+    }
+},
+
+["bzzz_mexicanfood_sopes_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_sopes_a", 
+        xPos = -0.02, yPos = -0.02, zPos = -0.02, 
+        xRot = 62.0, yRot = -47.0, zRot = 46.0
+    }
+},
+
+["bzzz_mexicanfood_taco_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_taco_a", 
+        xPos = 0.00, yPos = -0.01, zPos = -0.02, 
+        xRot = 60.0, yRot = -50.0, zRot = 70.0
+    }
+},
+
+["bzzz_mexicanfood_tortas_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_tortas_a", 
+        xPos = 0.00, yPos = -0.01, zPos = -0.02, 
+        xRot = 40.0, yRot = -50.0, zRot = 70.0
+    }
+},
+
+
+
+
+
+
+
+
+
+
+	--------- FOODPLATES SECTION ------
+['bzzz_mexicanfood_caldoderes_a'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz@mfood@anim",
+                animationNameEat = "bzzz_mexican",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_mexicanfood_caldoderes_a", 
+                xPos = 0.08, yPos = 0.01, zPos = 0.07, xRot = -30.0, yRot = 0.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_mexicanfood_pozole_b", 
+                xPos = 0.09, yPos = 0.03, zPos = -0.05, xRot = -70.0, yRot = 136.0, zRot = 36.0
+            }
+        },
+		
+["bzzz_mexicanfood_camotes_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDictEat = "bzzz@mfood@anim",
+        animationNameEat = "bzzz_mexican",
+        animationDictChewing = "facials@gen_male@base",
+        animationNameChewing = "eating_1"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_camotes_a", 
+        xPos = 0.08, yPos = -0.03, zPos = 0.08, 
+        xRot = -30.0, yRot = 10.0, zRot = 0.0
+    },
+    secondProp = {
+        boneIndex = 28422, 
+        propName = "bzzz_mexicanfood_camotes_b", 
+        xPos = 0.08, yPos = 0.04, zPos = -0.05, 
+        xRot = -90.0, yRot = -20.0, zRot = -10.0
+    }
+},
+
+["bzzz_mexicanfood_ceviche_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDictEat = "bzzz@mfood@anim",
+        animationNameEat = "bzzz_mexican",
+        animationDictChewing = "facials@gen_male@base",
+        animationNameChewing = "eating_1"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_ceviche_a", 
+        xPos = 0.08, yPos = 0.01, zPos = 0.07, 
+        xRot = -30.0, yRot = 0.0, zRot = 0.0
+    },
+    secondProp = {
+        boneIndex = 28422, 
+        propName = "bzzz_mexicanfood_ceviche_b", 
+        xPos = 0.08, yPos = 0.04, zPos = -0.05, 
+        xRot = -90.0, yRot = -20.0, zRot = -10.0
+    }
+},
+
+["bzzz_mexicanfood_chilaquiles_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDictEat = "bzzz@mfood@anim",
+        animationNameEat = "bzzz_mexican",
+        animationDictChewing = "facials@gen_male@base",
+        animationNameChewing = "eating_1"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_chilaquiles_a", 
+        xPos = 0.08, yPos = -0.03, zPos = 0.08, 
+        xRot = -30.0, yRot = 10.0, zRot = 0.0
+    },
+    secondProp = {
+        boneIndex = 28422, 
+        propName = "bzzz_mexicanfood_chilaquiles_b", 
+        xPos = 0.08, yPos = 0.04, zPos = -0.05, 
+        xRot = -90.0, yRot = -20.0, zRot = -10.0
+    }
+},
+
+["bzzz_mexicanfood_guacamole_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDictEat = "bzzz@mfood@anim",
+        animationNameEat = "bzzz_mexican",
+        animationDictChewing = "facials@gen_male@base",
+        animationNameChewing = "eating_1"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_guacamole_a", 
+        xPos = 0.08, yPos = 0.01, zPos = 0.05, 
+        xRot = -30.0, yRot = 10.0, zRot = 0.0
+    },
+    secondProp = {
+        boneIndex = 28422, 
+        propName = "bzzz_mexicanfood_guacamole_b", 
+        xPos = 0.09, yPos = 0.07, zPos = -0.03, 
+        xRot = -78.0, yRot = -44.0, zRot = 76.0
+    }
+},
+
+["bzzz_mexicanfood_menudo_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDictEat = "bzzz@mfood@anim",
+        animationNameEat = "bzzz_mexican",
+        animationDictChewing = "facials@gen_male@base",
+        animationNameChewing = "eating_1"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mexicanfood_menudo_a", 
+        xPos = 0.08, yPos = 0.01, zPos = 0.07, 
+        xRot = -30.0, yRot = 0.0, zRot = 0.0
+    },
+    secondProp = {
+        boneIndex = 28422, 
+        propName = "bzzz_mexicanfood_pozole_b", 
+        xPos = 0.09, yPos = 0.03, zPos = -0.05, xRot = -70.0, yRot = 136.0, zRot = 36.0
+    }
+},
+	
+
+
+
+
+
+
