af83ff5f594fb88267c3ba7c62d48e100aa704e0
BzZz <adelheid.kaszubi@gmail.com>
2025-04-28 19:48:12 +0200

    Create bzzz_mushroomsfood.lua

---
+++ qb-core-items/bzzz_mushroomsfood.lua
@@ -0,0 +1,93 @@
+-- Register items to qb-core/shared/items.lua
+-- mushrooms
+
+bzzz_mushroomsfood_omelette_a = { 
+    name = 'bzzz_mushroomsfood_omelette_a', 
+    label = 'Mushroom omelette', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mushroomsfood_omelette_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_mushroomsfood_rolls_a = { 
+    name = 'bzzz_mushroomsfood_rolls_a', 
+    label = 'Mushroom Beef Rolls', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mushroomsfood_rolls_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_mushroomsfood_pho_a = { 
+    name = 'bzzz_mushroomsfood_pho_a', 
+    label = 'Mushroom Pho', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mushroomsfood_pho_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_mushroomsfood_risotto_a = { 
+    name = 'bzzz_mushroomsfood_risotto_a', 
+    label = 'Porcini Mushroom Risotto', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mushroomsfood_risotto_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_mushroomsfood_soup_a = { 
+    name = 'bzzz_mushroomsfood_soup_a', 
+    label = 'Mushroom Soup', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_mushroomsfood_soup_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_mushroomsfood_skewer_a = { 
+    name = 'bzzz_mushroomsfood_skewer_a', 
+    label = 'Grilled Mushroom Skewer', 
+    weight = 350, 
+    type = 'item', 
+    image = 'bzzz_mushroomsfood_skewer_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_mushroomsfood_taco_a = { 
+    name = 'bzzz_mushroomsfood_taco_a', 
+    label = 'Mushroom Taco', 
+    weight = 350, 
+    type = 'item', 
+    image = 'bzzz_mushroomsfood_taco_a.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
