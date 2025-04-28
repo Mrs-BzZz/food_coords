a7179e782ece1097cdf29a1d9228d53193c3b82e
BzZz <adelheid.kaszubi@gmail.com>
2025-04-28 21:14:48 +0200

    Create bzzz_mushroomsfood.lua

---
+++ bzzz_usableitems/bzzz_mushroomsfood.lua
@@ -0,0 +1,142 @@
+--------- FOOD ---------
+["bzzz_mushroomsfood_skewer_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mushroomsfood_skewer_a", 
+        xPos = 0.0, yPos = 0.0, zPos = 0.0, xRot = -90.0, yRot = -90.0, zRot = 0.0
+    }
+},
+["bzzz_mushroomsfood_taco_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_mushroomsfood_taco_a", 
+        xPos = 0.0, yPos = 0.0, zPos = -0.02, xRot = 0.0, yRot = -2.0, zRot = 2.0
+    }
+},
+
+------------------------------------------------------------------------------------------------
+
+
+
+-- Copy and paste into bzzz_usableitems/config.lua - FoodPlates section (bottom of the file)
+
+['bzzz_mushroomsfood_omelette_a'] = {
+            basicInfo = {
+                amountToAdd = 200000, -- Adds food to your status bar (require esx_status)
+                animationDictEat = "bzzz_mushrooms_food",
+                animationNameEat = "bzzz_mushrooms_food",
+                animationDictChewing = "facials@gen_male@base",
+                animationNameChewing = "eating_1"
+            },
+            firstProp = {
+                boneIndex = 60309, 
+                propName = "bzzz_mushroomsfood_omelette_a", 
+                xPos = 0.08, yPos = 0.01, zPos = 0.05, xRot = -30.0, yRot = 0.0, zRot = 0.0
+            },
+            secondProp = {
+                boneIndex = 28422, 
+                propName = "bzzz_mushroomsfood_omelette_b", 
+                xPos = 0.09, yPos = 0.04, zPos = -0.05, xRot = -90.0, yRot = 0.0, zRot = -30.0
+            }
+        }, 
+		
+['bzzz_mushroomsfood_rolls_a'] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDictEat = "bzzz_mushrooms_food",
+        animationNameEat = "bzzz_mushrooms_food",
+        animationDictChewing = "facials@gen_male@base",
+        animationNameChewing = "eating_1"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_mushroomsfood_rolls_a",
+        xPos = 0.08, yPos = 0.01, zPos = 0.05,
+        xRot = -30.0, yRot = 0.0, zRot = 0.0
+    },
+    secondProp = {
+        boneIndex = 28422,
+        propName = "bzzz_mushroomsfood_rolls_b",
+        xPos = 0.09, yPos = 0.04, zPos = -0.05,
+        xRot = -90.0, yRot = 0.0, zRot = -30.0
+    }
+},
+
+['bzzz_mushroomsfood_pho_a'] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDictEat = "bzzz_mushrooms_food",
+        animationNameEat = "bzzz_mushrooms_food",
+        animationDictChewing = "facials@gen_male@base",
+        animationNameChewing = "eating_1"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_mushroomsfood_pho_a",
+        xPos = 0.07, yPos = 0.03, zPos = 0.07,
+        xRot = -30.0, yRot = 0.0, zRot = 0.0
+    },
+    secondProp = {
+        boneIndex = 28422,
+        propName = "bzzz_mushroomsfood_pho_b",
+        xPos = 0.08, yPos = 0.04, zPos = -0.05,
+        xRot = -90.0, yRot = -20.0, zRot = -10.0
+    }
+},
+
+['bzzz_mushroomsfood_risotto_a'] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDictEat = "bzzz_mushrooms_food",
+        animationNameEat = "bzzz_mushrooms_food",
+        animationDictChewing = "facials@gen_male@base",
+        animationNameChewing = "eating_1"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_mushroomsfood_risotto_a",
+        xPos = 0.07, yPos = 0.03, zPos = 0.07,
+        xRot = -30.0, yRot = 0.0, zRot = 0.0
+    },
+    secondProp = {
+        boneIndex = 28422,
+        propName = "bzzz_mushroomsfood_risotto_b",
+        xPos = 0.09, yPos = 0.04, zPos = -0.06,
+        xRot = -110.0, yRot = 150.0, zRot = -30.0
+    }
+},
+
+['bzzz_mushroomsfood_soup_a'] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDictEat = "bzzz_mushrooms_food",
+        animationNameEat = "bzzz_mushrooms_food",
+        animationDictChewing = "facials@gen_male@base",
+        animationNameChewing = "eating_1"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_mushroomsfood_soup_a",
+        xPos = 0.07, yPos = 0.03, zPos = 0.07,
+        xRot = -30.0, yRot = 0.0, zRot = 0.0
+    },
+    secondProp = {
+        boneIndex = 28422,
+        propName = "bzzz_mushroomsfood_soup_b",
+        xPos = 0.09, yPos = 0.04, zPos = -0.06,
+        xRot = -110.0, yRot = 150.0, zRot = -30.0
+    }
+},
+
+
+
