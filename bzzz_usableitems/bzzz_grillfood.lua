28524d55115a3abad8f90df3355a1aec2f1fbd63
BzZz <adelheid.kaszubi@gmail.com>
2025-08-13 02:16:48 +0200

    Create bzzz_grillfood.lua

---
+++ bzzz_usableitems/bzzz_grillfood.lua
@@ -0,0 +1,316 @@
+--------- FOOD SECTION ------
+["bzzz_food_grill_zucchini_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000, 
+        animationDict = "mp_player_inteat@burger", 
+		animationName = "mp_player_int_eat_burger"
+		},
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_food_grill_zucchini_grill_a", 
+        xPos = 0.05, yPos = 0.0, zPos = 0.0, 
+		xRot = 31.0, yRot = -33.0, zRot = 82.0
+    }
+},		
+
+["bzzz_food_grill_tofu_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_tofu_grill_a",
+        xPos = 0.05, yPos = 0.0, zPos = 0.0,
+        xRot = 31.0, yRot = -33.0, zRot = 82.0
+    }
+},
+["bzzz_food_grill_steak_grill_d"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_steak_grill_d",
+        xPos = 0.03, yPos = -0.04, zPos = -0.02,
+        xRot = 30.0, yRot = 0.0, zRot = -8.0
+    }
+},
+["bzzz_food_grill_steak_grill_c"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_steak_grill_c",
+        xPos = 0.06, yPos = -0.04, zPos = -0.02,
+        xRot = 24.0, yRot = 30.0, zRot = -62.0
+    }
+},
+["bzzz_food_grill_steak_grill_b"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_steak_grill_b",
+        xPos = 0.05, yPos = 0.0, zPos = 0.0,
+        xRot = 31.0, yRot = -33.0, zRot = 82.0
+    }
+},
+["bzzz_food_grill_steak_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_steak_grill_a",
+        xPos = 0.05, yPos = 0.0, zPos = 0.0,
+        xRot = 31.0, yRot = -33.0, zRot = 82.0
+    }
+},
+["bzzz_food_grill_skewer_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_skewer_grill_a",
+        xPos = 0.05, yPos = 0.0, zPos = 0.0,
+        xRot = 31.0, yRot = -33.0, zRot = 82.0
+    }
+},
+["bzzz_food_grill_sausage_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_sausage_grill_a",
+        xPos = 0.05, yPos = 0.0, zPos = 0.0,
+        xRot = 31.0, yRot = -33.0, zRot = 82.0
+    }
+},
+["bzzz_food_grill_salmon_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_salmon_grill_a",
+        xPos = 0.0, yPos = 0.0, zPos = -0.01,
+        xRot = 10.0, yRot = -2.0, zRot = 82.0
+    }
+},
+["bzzz_food_grill_ribs_grill_b"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_ribs_grill_b",
+        xPos = 0.01, yPos = -0.01, zPos = -0.01,
+        xRot = -62.0, yRot = 5.0, zRot = 174.0
+    }
+},
+["bzzz_food_grill_potato_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger", 
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_potato_grill_a",
+        xPos = -0.03, yPos = 0.0, zPos = -0.01,
+        xRot = 2.0, yRot = -15.0, zRot = 106.0
+    }
+},
+["bzzz_food_grill_pineapple_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_pineapple_grill_a",
+        xPos = -0.03, yPos = 0.0, zPos = -0.01,
+        xRot = 2.0, yRot = -15.0, zRot = 106.0
+    }
+},
+["bzzz_food_grill_naan_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_naan_grill_a",
+        xPos = 0.03, yPos = -0.06, zPos = -0.04,
+        xRot = 32.0, yRot = 5.0, zRot = 25.0
+    }
+},
+["bzzz_food_grill_mushrooms_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_mushrooms_grill_a",
+        xPos = 0.05, yPos = 0.0, zPos = 0.0,
+        xRot = 31.0, yRot = -33.0, zRot = 82.0
+    }
+},
+["bzzz_food_grill_halloumi_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_halloumi_grill_a",
+        xPos = -0.03, yPos = 0.0, zPos = 0.0,
+        xRot = 22.0, yRot = -25.0, zRot = 85.0
+    }
+},
+["bzzz_food_grill_fish_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_fish_grill_a",
+        xPos = 0.06, yPos = 0.0, zPos = -0.01,
+        xRot = -20.0, yRot = -16.0, zRot = -83.0
+    }
+},
+["bzzz_food_grill_corn_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_corn_grill_a",
+        xPos = 0.05, yPos = 0.0, zPos = 0.0,
+        xRot = 31.0, yRot = -33.0, zRot = 82.0
+    }
+},
+["bzzz_food_grill_chicken_grill_d"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_chicken_grill_d",
+        xPos = -0.01, yPos = 0.0, zPos = 0.0,
+        xRot = -20.0, yRot = -16.0, zRot = -83.0
+    }
+},
+["bzzz_food_grill_chicken_grill_c"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_chicken_grill_c",
+        xPos = -0.01, yPos = 0.0, zPos = 0.0,
+        xRot = -20.0, yRot = -16.0, zRot = -83.0
+    }
+},
+["bzzz_food_grill_chicken_grill_b"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_chicken_grill_b",
+        xPos = -0.01, yPos = 0.0, zPos = 0.0,
+        xRot = -20.0, yRot = -16.0, zRot = -83.0
+    }
+},
+["bzzz_food_grill_camembert_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_camembert_grill_a",
+        xPos = -0.01, yPos = 0.0, zPos = 0.0,
+        xRot = -20.0, yRot = -16.0, zRot = -83.0
+    }
+},
+["bzzz_food_grill_burger_grill_d"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_burger_grill_d",
+        xPos = 0.0, yPos = -0.02, zPos = -0.03,
+        xRot = 0.0, yRot = 0.0, zRot = -50.0
+    }
+},
+["bzzz_food_grill_baguette_grill_c"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_baguette_grill_c",
+        xPos = 0.07, yPos = 0.01, zPos = -0.02,
+        xRot = 0.15, yRot = 0.0, zRot = -60.0
+    }
+},
+["bzzz_food_grill_bacon_grill_a"] = {
+    basicInfo = {
+        amountToAdd = 200000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309,
+        propName = "bzzz_food_grill_bacon_grill_a",
+        xPos = 0.02, yPos = -0.01, zPos = -0.01,
+        xRot = 0.0, yRot = 0.0, zRot = 80.0
+    }
+},
+
+
