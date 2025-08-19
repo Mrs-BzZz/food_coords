c892f21105e0948408dc4cb8728bc797d12c33e1
BzZz <adelheid.kaszubi@gmail.com>
2025-08-20 01:57:57 +0200

    Create bzzz_dinomeal.lua

---
+++ qs-inventory/bzzz_dinomeal.lua
@@ -0,0 +1,352 @@
+['bzzz_food_dino_burger_a']      = {
+    ['name'] = 'bzzz_food_dino_burger_a',
+    ['label'] = 'Dino burger',
+    ['weight'] = 500,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_burger_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino burger',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_burger_a',
+    ['client'] = {
+        status = { hunger = 8000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_dino_burger_a', pos = vec3(-0.02, -0.01, 0.0), rot = vec3(30.0, 0.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_chocolate_b']   = {
+    ['name'] = 'bzzz_food_dino_chocolate_b',
+    ['label'] = 'Dino chocolate',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_chocolate_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino chocolate',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_chocolate_b',
+    ['client'] = {
+        status = { hunger = 4500 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_dino_chocolate_b', pos = vec3(-0.01, 0.02, -0.02), rot = vec3(10.0, -70.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_cupcake_a']     = {
+    ['name'] = 'bzzz_food_dino_cupcake_a',
+    ['label'] = 'Dino cupcake',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_cupcake_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino cupcake',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_cupcake_a',
+    ['client'] = {
+        status = { hunger = 2500 },
+        usetime = 2000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_dino_cupcake_a', pos = vec3(-0.01, 0.02, -0.03), rot = vec3(33.0, -10.0, -8.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_donut_b']       = {
+    ['name'] = 'bzzz_food_dino_donut_b',
+    ['label'] = 'Dino donut',
+    ['weight'] = 300,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_donut_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino donut',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_donut_b',
+    ['client'] = {
+        status = { hunger = 3500 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_dino_donut_b', pos = vec3(-0.03, -0.01, -0.02), rot = vec3(33.0, -10.0, -8.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_icecream_a']    = {
+    ['name'] = 'bzzz_food_dino_icecream_a',
+    ['label'] = 'Dino icecream',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_icecream_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino icecream',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_icecream_a',
+    ['client'] = {
+        status = { hunger = 2000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_dino_icecream_a', pos = vec3(0.03, 0.0, -0.03), rot = vec3(0.0, -80.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_pizza_b']       = {
+    ['name'] = 'bzzz_food_dino_pizza_b',
+    ['label'] = 'Dino pizza',
+    ['weight'] = 500,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_pizza_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino pizza',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_pizza_b',
+    ['client'] = {
+        status = { hunger = 8500 },
+        usetime = 4500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_dino_pizza_b', pos = vec3(0.0, -0.01, -0.02), rot = vec3(20.0, 40.0, -90.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_steak_b']       = {
+    ['name'] = 'bzzz_food_dino_steak_b',
+    ['label'] = 'Dino steak',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_steak_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino steak',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_steak_b',
+    ['client'] = {
+        status = { hunger = 3500 },
+        usetime = 1500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_dino_steak_b', pos = vec3(-0.02, -0.01, -0.01), rot = vec3(0.0, 0.0, -100.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_drink_a']       = {
+    ['name'] = 'bzzz_food_dino_drink_a',
+    ['label'] = 'Dino drink',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_drink_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino drink',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_drink_a',
+    ['client'] = {
+        status = { thirst = 50000 },
+        usetime = 4500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_dino_drink_a', pos = vec3(0.03, 0.02, 0.07), rot = vec3(32.0, 6.0, -64.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_drink_b']       = {
+    ['name'] = 'bzzz_food_dino_drink_b',
+    ['label'] = 'Dino sprunk',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_drink_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino sprunk',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_drink_b',
+    ['client'] = {
+        status = { thirst = 50000 },
+        usetime = 4500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_dino_drink_b', pos = vec3(0.03, 0.02, 0.07), rot = vec3(32.0, 6.0, -64.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_drink_c']       = {
+    ['name'] = 'bzzz_food_dino_drink_c',
+    ['label'] = 'Dino cola',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_drink_c.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino cola',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_drink_c',
+    ['client'] = {
+        status = { thirst = 50000 },
+        usetime = 4500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_dino_drink_c', pos = vec3(0.03, 0.02, 0.07), rot = vec3(32.0, 6.0, -64.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_drink_d']       = {
+    ['name'] = 'bzzz_food_dino_drink_d',
+    ['label'] = 'Dino orange',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_drink_d.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino orange',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_drink_d',
+    ['client'] = {
+        status = { thirst = 50000 },
+        usetime = 4500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_dino_drink_d', pos = vec3(0.03, 0.02, 0.07), rot = vec3(32.0, 6.0, -64.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_fries_a']       = {
+    ['name'] = 'bzzz_food_dino_fries_a',
+    ['label'] = 'Dino fries',
+    ['weight'] = 500,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_fries_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino fries',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_fries_a',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 10000,
+        anim = { dict = 'bzzz_food_dino_anim', clip = 'bzzz_food_dino_anim' },
+        prop = {
+            { model = 'bzzz_food_dino_fries_a', pos = vec3(0.12, 0.0, 0.05), rot = vec3(-99.0, -34.0, 0.0), bone = 60309 },
+            { model = 'bzzz_food_dino_fries_b', pos = vec3(0.08, 0.04, -0.03), rot = vec3(-131.0, 15.0, -46.0), bone = 28422 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_food_dino_nuggets_a']     = {
+    ['name'] = 'bzzz_food_dino_nuggets_a',
+    ['label'] = 'Dino nuggets',
+    ['weight'] = 500,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_dino_nuggets_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Dino nuggets',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_dino_nuggets_a',
+    ['client'] = {
+        status = { hunger = 16000 },
+        usetime = 10000,
+        anim = { dict = 'bzzz_food_dino_anim', clip = 'bzzz_food_dino_anim' },
+        prop = {
+            { model = 'bzzz_food_dino_nuggets_a', pos = vec3(0.12, 0.0, 0.06), rot = vec3(-99.0, -34.0, 0.0), bone = 60309 },
+            { model = 'bzzz_food_dino_nuggets_b', pos = vec3(0.09, 0.05, -0.03), rot = vec3(-50.0, 15.0, -28.0), bone = 28422 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
