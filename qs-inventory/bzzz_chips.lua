6922899b6244454de36cde458143bce6037c0715
BzZz <adelheid.kaszubi@gmail.com>
2025-08-20 00:25:45 +0200

    Create bzzz_chips.lua

---
+++ qs-inventory/bzzz_chips.lua
@@ -0,0 +1,183 @@
+['chips_a']                 = {
+    ['name'] = 'chips_a',
+    ['label'] = 'Chips',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'chips_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Chips',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_chips_a_open',
+    ['client'] = {
+        status = { hunger = 10000 },
+        usetime = 2500,
+        anim = {
+            dict = 'bzzz_eating_chips',
+            clip = 'eat_chip'
+        },
+        prop = {
+            {
+                model = 'bzzz_prop_chips_a_open',
+                pos = vec3(0.11, 0.05, 0.04),
+                rot = vec3(97.2, -14.2, 150.6),
+                bone = 60309
+            },
+            {
+                model = 'bzzz_prop_chip_a',
+                pos = vec3(0.1, 0.05, -0.04),
+                rot = vec3(-14.0, 70.0, 63.0),
+                bone = 28422
+            }
+        },
+        disable = {
+            move = false,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        removeAfterUse = true
+    }
+},
+
+['chips_b']                 = {
+    ['name'] = 'chips_b',
+    ['label'] = 'Chips',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'chips_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Chips',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_chips_b_open',
+    ['client'] = {
+        status = { hunger = 10000 },
+        usetime = 2500,
+        anim = {
+            dict = 'bzzz_eating_chips',
+            clip = 'eat_chip'
+        },
+        prop = {
+            {
+                model = 'bzzz_prop_chips_b_open',
+                pos = vec3(0.11, 0.05, 0.04),
+                rot = vec3(97.2, -14.2, 150.6),
+                bone = 60309
+            },
+            {
+                model = 'bzzz_prop_chip_b',
+                pos = vec3(0.1, 0.05, -0.04),
+                rot = vec3(-14.0, 70.0, 63.0),
+                bone = 28422
+            }
+        },
+        disable = {
+            move = false,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        removeAfterUse = true
+    }
+},
+
+['chips_c']                 = {
+    ['name'] = 'chips_c',
+    ['label'] = 'Chips',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'chips_c.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Chips',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_chips_c_open',
+    ['client'] = {
+        status = { hunger = 10000 },
+        usetime = 2500,
+        anim = {
+            dict = 'bzzz_eating_chips',
+            clip = 'eat_chip'
+        },
+        prop = {
+            {
+                model = 'bzzz_prop_chips_c_open',
+                pos = vec3(0.11, 0.05, 0.04),
+                rot = vec3(97.2, -14.2, 150.6),
+                bone = 60309
+            },
+            {
+                model = 'bzzz_prop_chip_b',
+                pos = vec3(0.1, 0.05, -0.04),
+                rot = vec3(-14.0, 70.0, 63.0),
+                bone = 28422
+            }
+        },
+        disable = {
+            move = false,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        removeAfterUse = true
+    }
+},
+
+['chips_d']                 = {
+    ['name'] = 'chips_d',
+    ['label'] = 'Chips',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'chips_d.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Chips',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_chips_d_open',
+    ['client'] = {
+        status = { hunger = 10000 },
+        usetime = 2500,
+        anim = {
+            dict = 'bzzz_eating_chips',
+            clip = 'eat_chip'
+        },
+        prop = {
+            {
+                model = 'bzzz_prop_chips_d_open',
+                pos = vec3(0.11, 0.05, 0.04),
+                rot = vec3(97.2, -14.2, 150.6),
+                bone = 60309
+            },
+            {
+                model = 'bzzz_prop_chip_a',
+                pos = vec3(0.1, 0.05, -0.04),
+                rot = vec3(-14.0, 70.0, 63.0),
+                bone = 28422
+            }
+        },
+        disable = {
+            move = false,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        removeAfterUse = true
+    }
+},
