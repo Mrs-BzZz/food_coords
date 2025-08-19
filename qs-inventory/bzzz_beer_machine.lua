f66442b81a6faba1f9d393582f6561b9f7649d5d
BzZz <adelheid.kaszubi@gmail.com>
2025-08-20 01:23:25 +0200

    Create bzzz_beer_machine.lua

---
+++ qs-inventory/bzzz_beer_machine.lua
@@ -0,0 +1,162 @@
+['beer_original']           = {
+    ['name'] = 'beer_original',
+    ['label'] = 'Original Beer',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_beer_original.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Nice cold beer',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_beer_original',
+    ['client'] = {
+        status = { thirst = 7000 },
+        usetime = 3500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_prop_beer_original', pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['beer_apple']              = {
+    ['name'] = 'beer_apple',
+    ['label'] = 'Apple Beer',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_beer_apple.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Nice cold beer',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_beer_apple',
+    ['client'] = {
+        status = { thirst = 7000 },
+        usetime = 3500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_prop_beer_apple', pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['beer_blueberry']          = {
+    ['name'] = 'beer_blueberry',
+    ['label'] = 'Blueberry Beer',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_beer_blueberry.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Nice cold beer',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_beer_blueberry',
+    ['client'] = {
+        status = { thirst = 7000 },
+        usetime = 3500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_prop_beer_blueberry', pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['beer_kiwi']               = {
+    ['name'] = 'beer_kiwi',
+    ['label'] = 'Kiwi Beer',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_beer_kiwi.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Nice cold beer',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_beer_kiwi',
+    ['client'] = {
+        status = { thirst = 7000 },
+        usetime = 3500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_prop_beer_kiwi', pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['beer_plum']               = {
+    ['name'] = 'beer_plum',
+    ['label'] = 'Plum Beer',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_beer_plum.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Nice cold beer',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_beer_plum',
+    ['client'] = {
+        status = { thirst = 7000 },
+        usetime = 3500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_prop_beer_plum', pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['beer_raspberry']          = {
+    ['name'] = 'beer_raspberry',
+    ['label'] = 'Raspberry Beer',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_beer_raspberry.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Nice cold beer',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_beer_raspberry',
+    ['client'] = {
+        status = { thirst = 7000 },
+        usetime = 3500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_prop_beer_raspberry', pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+	
