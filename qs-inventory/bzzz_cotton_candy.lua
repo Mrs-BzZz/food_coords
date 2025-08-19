7aed9b2fadf0b7f8593a48fae91084f5b9636d40
BzZz <adelheid.kaszubi@gmail.com>
2025-08-20 01:27:21 +0200

    Create bzzz_cotton_candy.lua

---
+++ qs-inventory/bzzz_cotton_candy.lua
@@ -0,0 +1,108 @@
+['bzzz_candy_cotton_yellow'] = {
+    ['name'] = 'bzzz_candy_cotton_yellow',
+    ['label'] = 'Yellow Candy Cotton',
+    ['weight'] = 100,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_candy_cotton_yellow.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Yellow candy cotton',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_candy_cotton_yellow',
+    ['client'] = {
+        status = { thirst = 5000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_candy_cotton_yellow', pos = vec3(-0.02, 0.01, -0.01), rot = vec3(0.0, -90.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_candy_cotton_pink'] = {
+    ['name'] = 'bzzz_candy_cotton_pink',
+    ['label'] = 'Pink Candy Cotton',
+    ['weight'] = 100,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_candy_cotton_pink.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Pink candy cotton',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_candy_cotton_pink',
+    ['client'] = {
+        status = { thirst = 5000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_candy_cotton_pink', pos = vec3(-0.02, 0.01, -0.01), rot = vec3(0.0, -90.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_candy_cotton_red'] = {
+    ['name'] = 'bzzz_candy_cotton_red',
+    ['label'] = 'Red Candy Cotton',
+    ['weight'] = 100,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_candy_cotton_red.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Red candy cotton',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_candy_cotton_red',
+    ['client'] = {
+        status = { thirst = 5000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_candy_cotton_red', pos = vec3(-0.02, 0.01, -0.01), rot = vec3(0.0, -90.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_candy_cotton_blue'] = {
+    ['name'] = 'bzzz_candy_cotton_blue',
+    ['label'] = 'Blue Candy Cotton',
+    ['weight'] = 100,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_candy_cotton_blue.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Blue candy cotton',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_candy_cotton_blue',
+    ['client'] = {
+        status = { thirst = 5000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_candy_cotton_blue', pos = vec3(-0.02, 0.01, -0.01), rot = vec3(0.0, -90.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+	
