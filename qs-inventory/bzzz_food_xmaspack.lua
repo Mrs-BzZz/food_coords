40d44441c62ee0bf15a7cae174cff920d4dcf3b0
BzZz <adelheid.kaszubi@gmail.com>
2025-08-20 02:10:58 +0200

    Create bzzz_food_xmaspack.lua

---
+++ qs-inventory/bzzz_food_xmaspack.lua
@@ -0,0 +1,269 @@
+['gingerbread'] = {
+    ['name'] = 'gingerbread',
+    ['label'] = 'Gingerbread',
+    ['weight'] = 100,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_gingerbread_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_gingerbread_a',
+    ['client'] = {
+        status = { hunger = 5000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_xmas_gingerbread_a', pos = vec3(0.02, 0.00, -0.01), rot = vec3(362.0, -8.0, 432.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['xlollipop_a'] = {
+    ['name'] = 'xlollipop_a',
+    ['label'] = 'Xmas Lollipop Red',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_lollipop_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_lollipop_a',
+    ['client'] = {
+        status = { hunger = 1000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_xmas_lollipop_a', pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['xlollipop_b'] = {
+    ['name'] = 'xlollipop_b',
+    ['label'] = 'Xmas Lollipop Green',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_lollipop_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_lollipop_b',
+    ['client'] = {
+        status = { hunger = 1000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_xmas_lollipop_b', pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['xlollipop_c'] = {
+    ['name'] = 'xlollipop_c',
+    ['label'] = 'Xmas Lollipop Purple',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_lollipop_c.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_lollipop_c',
+    ['client'] = {
+        status = { hunger = 1000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_xmas_lollipop_c', pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['xlollipop_d'] = {
+    ['name'] = 'xlollipop_d',
+    ['label'] = 'Xmas Lollipop Yellow',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_lollipop_d.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_lollipop_d',
+    ['client'] = {
+        status = { hunger = 1000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_xmas_lollipop_d', pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['xlollipop_e'] = {
+    ['name'] = 'xlollipop_e',
+    ['label'] = 'Xmas Lollipop Blue',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_lollipop_e.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_lollipop_e',
+    ['client'] = {
+        status = { hunger = 1000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_xmas_lollipop_e', pos = vec3(0.02, -0.05, -0.02), rot = vec3(-73.0, -5.0, 50.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['macaroon'] = {
+    ['name'] = 'macaroon',
+    ['label'] = 'Macaroon',
+    ['weight'] = 150,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_macaroon_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_macaroon_a',
+    ['client'] = {
+        status = { hunger = 3000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_xmas_macaroon_a', pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 0.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['mug_a'] = {
+    ['name'] = 'mug_a',
+    ['label'] = 'Coffee Cup',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_mug_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Coffee Cup',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_mug_a',
+    ['client'] = {
+        status = { thirst = 4000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_xmas_mug_a', pos = vec3(-0.01, 0.02, 0.0), rot = vec3(0.0, 0.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['mug_b'] = {
+    ['name'] = 'mug_b',
+    ['label'] = 'Tea Cup',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_mug_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Tea Cup',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_mug_b',
+    ['client'] = {
+        status = { thirst = 4000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_xmas_mug_b', pos = vec3(0.0, 0.01, 0.0), rot = vec3(0.0, 0.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['mulled_wine'] = {
+    ['name'] = 'mulled_wine',
+    ['label'] = 'Mulled Wine',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_food_xmas_mug_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Mulled Wine',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_xmas_mug_b',
+    ['client'] = {
+        status = { thirst = 4000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_xmas_mug_b', pos = vec3(0.0, 0.01, 0.0), rot = vec3(0.0, 0.0, 0.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
