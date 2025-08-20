0292b4043dbae69f64f524fa4b69e360b2ca1294
BzZz <adelheid.kaszubi@gmail.com>
2025-08-21 01:57:38 +0200

    Create bzzz_fruits.lua

---
+++ qs-inventory/bzzz_fruits.lua
@@ -0,0 +1,270 @@
+['avocado_b'] = {
+    ['name'] = 'avocado_b',
+    ['label'] = 'Piece of avocado',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'avocado_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Piece of avocado',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_avocado_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_avocado_b', pos = vec3(-0.02, 0.01, 0.0), rot = vec3(30.0, -41.0, 80.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['coconut_b'] = {
+    ['name'] = 'coconut_b',
+    ['label'] = 'Piece of coconut',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'coconut_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Piece of coconut',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_coconut_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_coconut_b', pos = vec3(-0.02, 0.01, 0.0), rot = vec3(30.0, -41.0, 80.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['honeydew_melon_b'] = {
+    ['name'] = 'honeydew_melon_b',
+    ['label'] = 'Piece of honeydew melon',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'honeydew_melon_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Piece of honeydew melon',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_honeydew_melon_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_honeydew_melon_b', pos = vec3(-0.02, -0.02, 0.0), rot = vec3(-50.0, -20.0, 80.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['lemon_b'] = {
+    ['name'] = 'lemon_b',
+    ['label'] = 'Piece of lemon',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'lemon_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Piece of lemon',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_lemon_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_lemon_b', pos = vec3(-0.02, 0.01, 0.0), rot = vec3(30.0, -31.0, 80.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['mandarin_b'] = {
+    ['name'] = 'mandarin_b',
+    ['label'] = 'Piece of mandarin',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'mandarin_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Piece of mandarin',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_mandarin_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_mandarin_b', pos = vec3(-0.03, 0.0, 0.01), rot = vec3(-10.0, -11.0, 170.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['mango_b'] = {
+    ['name'] = 'mango_b',
+    ['label'] = 'Piece of mango',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'mango_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Piece of mango',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_mango_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_mango_b', pos = vec3(-0.03, -0.01, 0.0), rot = vec3(-10.0, -11.0, 170.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['pomegranate_b'] = {
+    ['name'] = 'pomegranate_b',
+    ['label'] = 'Piece of pomegranate',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'pomegranate_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Piece of pomegranate',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_pomegranate_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_pomegranate_b', pos = vec3(-0.03, -0.01, -0.02), rot = vec3(-10.0, -11.0, 170.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['dragon_fruit_b'] = {
+    ['name'] = 'dragon_fruit_b',
+    ['label'] = 'Piece of dragon fruit',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'dragon_fruit_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Piece of dragon fruit',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_dragon_fruit_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_dragon_fruit_b', pos = vec3(-0.02, 0.0, -0.01), rot = vec3(-46.0, 19.0, 170.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['papaya_b'] = {
+    ['name'] = 'papaya_b',
+    ['label'] = 'Piece of papaya',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'papaya_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Piece of papaya',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_papaya_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_papaya_b', pos = vec3(0.0, -0.01, -0.03), rot = vec3(-35.0, 0.0, 170.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['litchi_b'] = {
+    ['name'] = 'litchi_b',
+    ['label'] = 'Litchi',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'litchi_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Litchi',
+    ['created'] = nil,
+    ['decay'] = 0.20,
+    ['delete'] = false,
+    ['object'] = 'bzzz_fruits_litchi_b',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 2500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_fruits_litchi_b', pos = vec3(-0.02, 0.0, 0.01), rot = vec3(-46.0, 19.0, 170.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+	
