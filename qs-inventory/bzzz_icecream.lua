bfbb7793a4347bd838857cfcfce489f168d785e2
BzZz <adelheid.kaszubi@gmail.com>
2025-08-21 02:25:41 +0200

    Create bzzz_icecream.lua

---
+++ qs-inventory/bzzz_icecream.lua
@@ -0,0 +1,216 @@
+['icecream_chr'] = {
+    ['name'] = 'icecream_chr',
+    ['label'] = 'Ice cream cherry',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'icecream_chr.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Cool ice cream cherry',
+    ['created'] = nil,
+    ['decay'] = 0.30,
+    ['delete'] = false,
+    ['object'] = 'bzzz_icecream_cherry',
+    ['client'] = {
+        status = { hunger = 1500 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+        prop = {
+            { model = 'bzzz_icecream_cherry', bone = 18905, pos = vec3(0.10, 0.03, 0.03), rot = vec3(105.0, 70.0, -203.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['icecream_chc'] = {
+    ['name'] = 'icecream_chc',
+    ['label'] = 'Ice cream chocolate',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'icecream_chc.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Cool ice cream chocolate',
+    ['created'] = nil,
+    ['decay'] = 0.30,
+    ['delete'] = false,
+    ['object'] = 'bzzz_icecream_chocolate',
+    ['client'] = {
+        status = { hunger = 1500 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+        prop = {
+            { model = 'bzzz_icecream_chocolate', bone = 18905, pos = vec3(0.10, 0.03, 0.03), rot = vec3(105.0, 70.0, -203.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['icecream_lmn'] = {
+    ['name'] = 'icecream_lmn',
+    ['label'] = 'Ice cream lemon',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'icecream_lmn.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Cool ice cream lemon',
+    ['created'] = nil,
+    ['decay'] = 0.30,
+    ['delete'] = false,
+    ['object'] = 'bzzz_icecream_lemon',
+    ['client'] = {
+        status = { hunger = 1500 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+        prop = {
+            { model = 'bzzz_icecream_lemon', bone = 18905, pos = vec3(0.10, 0.03, 0.03), rot = vec3(105.0, 70.0, -203.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['icecream_pso'] = {
+    ['name'] = 'icecream_pso',
+    ['label'] = 'Ice cream pistachio',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'icecream_pso.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Cool ice cream pistachio',
+    ['created'] = nil,
+    ['decay'] = 0.30,
+    ['delete'] = false,
+    ['object'] = 'bzzz_icecream_pistachio',
+    ['client'] = {
+        status = { hunger = 1500 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+        prop = {
+            { model = 'bzzz_icecream_pistachio', bone = 18905, pos = vec3(0.10, 0.03, 0.03), rot = vec3(105.0, 70.0, -203.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['icecream_rpy'] = {
+    ['name'] = 'icecream_rpy',
+    ['label'] = 'Ice cream raspberry',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'icecream_rpy.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Cool ice cream raspberry',
+    ['created'] = nil,
+    ['decay'] = 0.30,
+    ['delete'] = false,
+    ['object'] = 'bzzz_icecream_raspberry',
+    ['client'] = {
+        status = { hunger = 1500 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+        prop = {
+            { model = 'bzzz_icecream_raspberry', bone = 18905, pos = vec3(0.10, 0.03, 0.03), rot = vec3(105.0, 70.0, -203.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['icecream_ckd'] = {
+    ['name'] = 'icecream_ckd',
+    ['label'] = 'Ice cream stracciatella',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'icecream_ckd.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Cool ice cream stracciatella',
+    ['created'] = nil,
+    ['decay'] = 0.30,
+    ['delete'] = false,
+    ['object'] = 'bzzz_icecream_stracciatella',
+    ['client'] = {
+        status = { hunger = 1500 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+        prop = {
+            { model = 'bzzz_icecream_stracciatella', bone = 18905, pos = vec3(0.10, 0.03, 0.03), rot = vec3(105.0, 70.0, -203.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['icecream_sby'] = {
+    ['name'] = 'icecream_sby',
+    ['label'] = 'Ice cream strawberry',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'icecream_sby.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Cool ice cream strawberry',
+    ['created'] = nil,
+    ['decay'] = 0.30,
+    ['delete'] = false,
+    ['object'] = 'bzzz_icecream_strawberry',
+    ['client'] = {
+        status = { hunger = 1500 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+        prop = {
+            { model = 'bzzz_icecream_strawberry', bone = 18905, pos = vec3(0.10, 0.03, 0.03), rot = vec3(105.0, 70.0, -203.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['icecream_vna'] = {
+    ['name'] = 'icecream_vna',
+    ['label'] = 'Ice cream walnut',
+    ['weight'] = 50,
+    ['type'] = 'item',
+    ['image'] = 'icecream_vna.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Cool ice cream walnut',
+    ['created'] = nil,
+    ['decay'] = 0.30,
+    ['delete'] = false,
+    ['object'] = 'bzzz_icecream_walnut',
+    ['client'] = {
+        status = { hunger = 1500 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+        prop = {
+            { model = 'bzzz_icecream_walnut', bone = 18905, pos = vec3(0.10, 0.03, 0.03), rot = vec3(105.0, 70.0, -203.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+	
