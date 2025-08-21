388fdc3d7c42e13401306dafca3e1217d740e639
BzZz <adelheid.kaszubi@gmail.com>
2025-08-21 20:34:52 +0200

    Create bzzz_uwuprops.lua

---
+++ qs-inventory/bzzz_uwuprops.lua
@@ -0,0 +1,459 @@
+['bzzz_uwuprops_cake_a'] = {
+    ['name'] = 'bzzz_uwuprops_cake_a',
+    ['label'] = 'UwU cake',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_cake_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'UwU cake',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_cake_a',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_cake_a', pos = vec3(-0.02, 0.0, 0.0), rot = vec3(32.0, -18.0, 29.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_cappuccino_a'] = {
+    ['name'] = 'bzzz_uwuprops_cappuccino_a',
+    ['label'] = 'Uwu cappuccino',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_cappuccino_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu cappuccino',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_cappuccino_a',
+    ['client'] = {
+        status = { thirst = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_uwuprops_cappuccino_a', pos = vec3(0.01, 0.01, 0.04), rot = vec3(0.0, -30.0, 30.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_coffee_a'] = {
+    ['name'] = 'bzzz_uwuprops_coffee_a',
+    ['label'] = 'Uwu coffee',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_coffee_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu coffee',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_coffee_a',
+    ['client'] = {
+        status = { thirst = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_uwuprops_coffee_a', pos = vec3(0.01, 0.01, 0.04), rot = vec3(0.0, -30.0, 30.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_cookie_a'] = {
+    ['name'] = 'bzzz_uwuprops_cookie_a',
+    ['label'] = 'Uwu cookie',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_cookie_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu cookie',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_cookie_a',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_cookie_a', pos = vec3(-0.02, 0.0, 0.0), rot = vec3(32.0, -18.0, 29.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_donut_a'] = {
+    ['name'] = 'bzzz_uwuprops_donut_a',
+    ['label'] = 'Uwu donut',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_donut_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu donut',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_donut_a',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_donut_a', pos = vec3(-0.02, 0.01, -0.02), rot = vec3(32.0, -18.0, 29.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_drink_a'] = {
+    ['name'] = 'bzzz_uwuprops_drink_a',
+    ['label'] = 'Uwu ice coffee',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_drink_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu ice coffee',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_drink_a',
+    ['client'] = {
+        status = { thirst = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_uwuprops_drink_a', pos = vec3(0.04, 0.01, 0.01), rot = vec3(0.0, -20.0, 25.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_drink_b'] = {
+    ['name'] = 'bzzz_uwuprops_drink_b',
+    ['label'] = 'Uwu ice coffee',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_drink_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu ice coffee',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_drink_b',
+    ['client'] = {
+        status = { thirst = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_uwuprops_drink_b', pos = vec3(0.04, 0.01, 0.01), rot = vec3(0.0, -20.0, 25.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_drink_c'] = {
+    ['name'] = 'bzzz_uwuprops_drink_c',
+    ['label'] = 'Uwu ice coffee',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_drink_c.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu ice coffee',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_drink_c',
+    ['client'] = {
+        status = { thirst = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_uwuprops_drink_c', pos = vec3(0.04, 0.01, 0.01), rot = vec3(0.0, -20.0, 25.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_drink_d'] = {
+    ['name'] = 'bzzz_uwuprops_drink_d',
+    ['label'] = 'Uwu ice coffee',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_drink_d.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu ice coffee',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_drink_d',
+    ['client'] = {
+        status = { thirst = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_uwuprops_drink_d', pos = vec3(0.04, 0.01, 0.01), rot = vec3(0.0, -20.0, 25.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_icecone_a'] = {
+    ['name'] = 'bzzz_uwuprops_icecone_a',
+    ['label'] = 'Uwu ice cream in a cornout',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_icecone_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu ice cream in a cornout',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_icecone_a',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_icecone_a', pos = vec3(0.01, 0.0, 0.0), rot = vec3(-75.0, -4.0, 80.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_icecone_b'] = {
+    ['name'] = 'bzzz_uwuprops_icecone_b',
+    ['label'] = 'Uwu ice cream in a cornout',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_icecone_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu ice cream in a cornout',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_icecone_b',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_icecone_b', pos = vec3(0.01, 0.0, 0.0), rot = vec3(-75.0, -4.0, 80.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_icecone_c'] = {
+    ['name'] = 'bzzz_uwuprops_icecone_c',
+    ['label'] = 'Uwu ice cream in a cornout',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_icecone_c.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu ice cream in a cornout',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_icecone_c',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_icecone_c', pos = vec3(0.01, 0.0, 0.0), rot = vec3(-75.0, -4.0, 80.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_icecone_d'] = {
+    ['name'] = 'bzzz_uwuprops_icecone_d',
+    ['label'] = 'Uwu ice cream in a cornout',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_icecone_d.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu ice cream in a cornout',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_icecone_d',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_icecone_d', pos = vec3(0.01, 0.0, 0.0), rot = vec3(-75.0, -4.0, 80.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_icecream_a'] = {
+    ['name'] = 'bzzz_uwuprops_icecream_a',
+    ['label'] = 'Uwu ice cream',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_icecream_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu ice cream',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_icecream_a',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_icecream_a', pos = vec3(-0.03, 0.0, -0.01), rot = vec3(-75.0, -4.0, 80.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_popsicle_a'] = {
+    ['name'] = 'bzzz_uwuprops_popsicle_a',
+    ['label'] = 'Uwu popsicle',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_popsicle_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu popsicle',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_popsicle_a',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_popsicle_a', pos = vec3(-0.03, 0.0, -0.01), rot = vec3(15.0, -9.0, 85.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_wafer_a'] = {
+    ['name'] = 'bzzz_uwuprops_wafer_a',
+    ['label'] = 'Uwu wafer',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_wafer_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu wafer',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_wafer_a',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_wafer_a', pos = vec3(0.0, -0.02, -0.02), rot = vec3(15.0, -9.0, 85.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_uwuprops_lollipop_a'] = {
+    ['name'] = 'bzzz_uwuprops_lollipop_a',
+    ['label'] = 'Uwu lollipop',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_uwuprops_lollipop_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Uwu lollipop',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_uwuprops_lollipop_a',
+    ['client'] = {
+        status = { hunger = 6000 },
+        usetime = 3000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_uwuprops_lollipop_a', pos = vec3(0.0, -0.02, -0.02), rot = vec3(15.0, -9.0, 85.0), bone = 60309 }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
