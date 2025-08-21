940001f7ba607d71c6b7ca1c8f3d26f13c4db731
BzZz <adelheid.kaszubi@gmail.com>
2025-08-21 03:06:37 +0200

    Create bzzz_mixfood.lua

---
+++ qs-inventory/bzzz_mixfood.lua
@@ -0,0 +1,270 @@
+['coffeecup_a'] = {
+    ['name'] = 'coffeecup_a',
+    ['label'] = 'Coffee Cup',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'coffeecup_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_cooffeecup01_a',
+    ['client'] = {
+        status = { thirst = 7000 },
+        usetime = 3500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_cooffeecup01_a', bone = 60309, pos = vec3(0.00, 0.02, 0.03), rot = vec3(0.00, 0.0, 0.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bubbletea_a'] = {
+    ['name'] = 'bubbletea_a',
+    ['label'] = 'Bubbletea',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'bubbletea_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_bubbletea_a',
+    ['client'] = {
+        status = { thirst = 7000 },
+        usetime = 23500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_bubbletea_a', bone = 60309, pos = vec3(0.03, 0.01, 0.05), rot = vec3(0.00, 0.0, 0.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['capisun_a'] = {
+    ['name'] = 'capisun_a',
+    ['label'] = 'Capisun',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'capisun_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_capisun_a',
+    ['client'] = {
+        status = { thirst = 7000 },
+        usetime = 23500,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            { model = 'bzzz_food_capisun_a', bone = 60309, pos = vec3(-0.03, 0.01, -0.08), rot = vec3(4.00, 4.0, 120.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['hotdog_a'] = {
+    ['name'] = 'hotdog_a',
+    ['label'] = 'Hotdog with mustard',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'hotdog_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_hotdog_a',
+    ['client'] = {
+        status = { hunger = 7000 },
+        usetime = 5500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_hotdog_a', bone = 60309, pos = vec3(0.02, 0.00, -0.01), rot = vec3(362.00, -8.00, 432.00) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['hotdog_b'] = {
+    ['name'] = 'hotdog_b',
+    ['label'] = 'Hotdog with ketchup',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'hotdog_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_hotdog_b',
+    ['client'] = {
+        status = { hunger = 7000 },
+        usetime = 5500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_hotdog_b', bone = 60309, pos = vec3(0.02, 0.00, -0.01), rot = vec3(362.00, -8.00, 432.00) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['carneasada_a'] = {
+    ['name'] = 'carneasada_a',
+    ['label'] = 'Carneasada',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'carneasada_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_carneasada_a',
+    ['client'] = {
+        status = { hunger = 7000 },
+        usetime = 5500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_carneasada_a', bone = 60309, pos = vec3(0.02, 0.00, -0.01), rot = vec3(362.00, -8.00, 432.00) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['nachos_a'] = {
+    ['name'] = 'nachos_a',
+    ['label'] = 'Nachos',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'nachos_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_nachos_a',
+    ['client'] = {
+        status = { hunger = 7000 },
+        usetime = 25500,
+        anim = { dict = 'anim@scripted@island@special_peds@pavel@hs4_pavel_ig5_caviar_p1', clip = 'base_idle' },
+        prop = {
+            { model = 'bzzz_food_nachos_a', bone = 60309, pos = vec3(-0.04, 0.07, -0.01), rot = vec3(0.00, 0.00, 0.00) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['kebab_a'] = {
+    ['name'] = 'kebab_a',
+    ['label'] = 'Kebab',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'kebab_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_kebab_a',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_kebab_a', bone = 60309, pos = vec3(0.00, 0.00, 0.00), rot = vec3(108.00, 22.00, -80.00) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['burrito_a'] = {
+    ['name'] = 'burrito_a',
+    ['label'] = 'Burrito',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'burrito_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_burrito_a',
+    ['client'] = {
+        status = { hunger = 20000 },
+        usetime = 7500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_food_burrito_a', bone = 60309, pos = vec3(0.03, 0.01, -0.03), rot = vec3(8.00, 96.00, 0.00) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['churros_a'] = {
+    ['name'] = 'churros_a',
+    ['label'] = 'Churros',
+    ['weight'] = 250,
+    ['type'] = 'item',
+    ['image'] = 'churros_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Something sweet',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_food_churros_a',
+    ['client'] = {
+        status = { hunger = 7000 },
+        usetime = 5500,
+        anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+        prop = {
+            { model = 'bzzz_food_churros_a', bone = 60309, pos = vec3(-0.10, 0.01, -0.03), rot = vec3(-69.00, 17.00, -84.00) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
