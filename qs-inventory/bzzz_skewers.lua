666f76530fb1000c6a6244afd3d9ab794161d01b
BzZz <adelheid.kaszubi@gmail.com>
2025-08-21 18:38:07 +0200

    Create bzzz_skewers.lua

---
+++ qs-inventory/bzzz_skewers.lua
@@ -0,0 +1,495 @@
+['shashlik_a'] = {
+    ['name'] = 'shashlik_a',
+    ['label'] = 'Shashlik',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'shashlik_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Shashlik',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_shashlik_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_shashlik_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['chicken-intestine_a'] = {
+    ['name'] = 'chicken-intestine_a',
+    ['label'] = 'Chicken intestine',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'chicken-intestine_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Chicken intestine',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_chicken-intestine_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_chicken-intestine_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['fishball_a'] = {
+    ['name'] = 'fishball_a',
+    ['label'] = 'Fish ball',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'fishball_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Fish ball',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_fishball_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_fishball_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['kikiam_a'] = {
+    ['name'] = 'kikiam_a',
+    ['label'] = 'Kikiam',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'kikiam_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Kikiam',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_kikiam_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_kikiam_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['black-pudding_a'] = {
+    ['name'] = 'black-pudding_a',
+    ['label'] = 'Black pudding',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'black-pudding_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Black pudding',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_black-pudding_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_black-pudding_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['quail-eggs_a'] = {
+    ['name'] = 'quail-eggs_a',
+    ['label'] = 'Quail eggs',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'quail-eggs_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Quail eggs',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_quail-eggs_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_quail-eggs_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['skewer_hotdog_a'] = {
+    ['name'] = 'skewer_hotdog_a',
+    ['label'] = 'Hotdog',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'skewer_hotdog_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Hotdog',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_hotdog_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_hotdog_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['banana_a'] = {
+    ['name'] = 'banana_a',
+    ['label'] = 'Banana',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'banana_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Banana',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_banana_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_banana_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['pork-bbq_a'] = {
+    ['name'] = 'pork-bbq_a',
+    ['label'] = 'Pork BBQ',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'pork-bbq_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Pork BBQ',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_pork-bbq_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_pork-bbq_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['chicken-feet_a'] = {
+    ['name'] = 'chicken-feet_a',
+    ['label'] = 'Chicken feet',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'chicken-feet_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Chicken feet',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_chicken-feet_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_chicken-feet_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['corn_a'] = {
+    ['name'] = 'corn_a',
+    ['label'] = 'Corn',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'corn_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Corn',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_corn_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_corn_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['corndog_a'] = {
+    ['name'] = 'corndog_a',
+    ['label'] = 'Corndog',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'corndog_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Corndog',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_corndog_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_corndog_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['fish_a'] = {
+    ['name'] = 'fish_a',
+    ['label'] = 'Fish',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'fish_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Fish',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_fish_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_fish_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['cheese_a'] = {
+    ['name'] = 'cheese_a',
+    ['label'] = 'Cheese',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'cheese_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Cheese',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_cheese_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_cheese_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['potato_a'] = {
+    ['name'] = 'potato_a',
+    ['label'] = 'Potato',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'potato_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Potato',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'bzzz_skewerpack_potato_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_skewerpack_potato_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(-10.0, -90.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
