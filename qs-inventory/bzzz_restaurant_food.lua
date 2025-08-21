be54f5a39c961436072a14011b089832233d6253
BzZz <adelheid.kaszubi@gmail.com>
2025-08-21 23:53:00 +0200

    Create bzzz_restaurant_food.lua

---
+++ qs-inventory/bzzz_restaurant_food.lua
@@ -0,0 +1,348 @@
+['res_food_a'] = {
+    ['name'] = 'res_food_a',
+    ['label'] = 'Schnitzel with fries',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Schnitzel with fries',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_a',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_a', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_a', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-100.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_b'] = { 
+    ['name'] = 'res_food_b',
+    ['label'] = 'Schnitzel with croquettes',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Schnitzel with croquettes',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_b',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_b', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_b', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_c'] = { 
+    ['name'] = 'res_food_c',
+    ['label'] = 'Spaghetti',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_c.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Spaghetti',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_c',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_c', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_c', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_d'] = {
+    ['name'] = 'res_food_d',
+    ['label'] = 'Grilled fish',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_d.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Grilled fish',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_d',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_d', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_d', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_e'] = { 
+    ['name'] = 'res_food_e',
+    ['label'] = 'Steak with potatoes',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_e.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Steak with potatoes',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_e',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_e', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_e', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_f'] = {
+    ['name'] = 'res_food_f',
+    ['label'] = 'Steak with fries',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_f.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Steak with fries',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_f',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_f', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_a', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_g'] = { 
+    ['name'] = 'res_food_g',
+    ['label'] = 'Lobster',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_g.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Lobster',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_g',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_g', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_d', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_h'] = { 
+    ['name'] = 'res_food_h',
+    ['label'] = 'Eggs with toast',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_h.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Eggs with toast',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_h',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_h', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_f', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_i'] = { 
+    ['name'] = 'res_food_i',
+    ['label'] = 'Sushi',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_i.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Sushi',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_i',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_i', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_h', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-100.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_j'] = {
+    ['name'] = 'res_food_j',
+    ['label'] = 'Shrimps with sauce',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_j.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Shrimps with sauce',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_j',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_j', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_material_shrimp_a', bone = 28422, pos = vec3(0.08, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_k'] = { 
+    ['name'] = 'res_food_k',
+    ['label'] = 'Chicken with potatoes',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_k.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Chicken with potatoes',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_k',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_k', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_e', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
+['res_food_l'] = { 
+    ['name'] = 'res_food_l',
+    ['label'] = 'Vegetarian salad',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'res_food_l.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Vegetarian salad',
+    ['created'] = nil,
+    ['decay'] = 0.05,
+    ['delete'] = false,
+    ['object'] = 'res_food_l',
+    ['client'] = {
+        status = { hunger = 70000 },
+        usetime = 15000,
+        anim = { dict = 'bzzz@restaurant@eat', clip = 'bzzz_restaurant_eat' },
+        prop = {
+            { model = 'bzzz_restaurant_food_l', bone = 60309, pos = vec3(0.08, -0.04, 0.08), rot = vec3(-30.0, 10.0, 0.0) },
+            { model = 'bzzz_restaurant_fork_g', bone = 28422, pos = vec3(0.07, 0.05, -0.03), rot = vec3(-90.0, 24.0, -86.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'You have satisfied your hunger'
+    }
+},
+
