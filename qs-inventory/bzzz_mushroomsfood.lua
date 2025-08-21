78acb2e1a7d624b18324b57740f1f9cd4ddcf34d
BzZz <adelheid.kaszubi@gmail.com>
2025-08-21 03:14:53 +0200

    Create bzzz_mushroomsfood.lua

---
+++ qs-inventory/bzzz_mushroomsfood.lua
@@ -0,0 +1,194 @@
+['bzzz_mushroomsfood_omelette_a'] = {
+    ['name'] = 'bzzz_mushroomsfood_omelette_a',
+    ['label'] = 'Mushroom omelette',
+    ['weight'] = 500,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_mushroomsfood_omelette_a.png',
+    ['unique'] = true,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Mushroom omelette',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_mushroomsfood_omelette_a',
+    ['client'] = {
+        status = { hunger = 25000 },
+        usetime = 9500,
+        anim = { dict = 'bzzz_mushrooms_food', clip = 'bzzz_mushrooms_food' },
+        prop = {
+            { model = 'bzzz_mushroomsfood_omelette_a', bone = 60309, pos = vec3(0.08, 0.01, 0.05), rot = vec3(-30.0, 0.0, 0.0) },
+            { model = 'bzzz_mushroomsfood_omelette_b', bone = 28422, pos = vec3(0.08, 0.04, -0.05), rot = vec3(-90.0, 0.0, -30.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_mushroomsfood_rolls_a'] = {
+    ['name'] = 'bzzz_mushroomsfood_rolls_a',
+    ['label'] = 'Mushroom Beef Rolls',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_mushroomsfood_rolls_a.png',
+    ['unique'] = true,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Mushroom Beef Rolls',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_mushroomsfood_rolls_a',
+    ['client'] = {
+        status = { hunger = 15000 },
+        usetime = 7000,
+        anim = { dict = 'bzzz_mushrooms_food', clip = 'bzzz_mushrooms_food' },
+        prop = {
+            { model = 'bzzz_mushroomsfood_rolls_a', bone = 60309, pos = vec3(0.08, 0.01, 0.05), rot = vec3(-30.0, 0.0, 0.0) },
+            { model = 'bzzz_mushroomsfood_rolls_b', bone = 28422, pos = vec3(0.08, 0.04, -0.05), rot = vec3(-90.0, 0.0, -30.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_mushroomsfood_pho_a'] = {
+    ['name'] = 'bzzz_mushroomsfood_pho_a',
+    ['label'] = 'Mushroom Pho',
+    ['weight'] = 400,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_mushroomsfood_pho_a.png',
+    ['unique'] = true,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Mushroom Pho',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_mushroomsfood_pho_a',
+    ['client'] = {
+        status = { hunger = 12000 },
+        usetime = 8500,
+        anim = { dict = 'bzzz_mushrooms_food', clip = 'bzzz_mushrooms_food' },
+        prop = {
+            { model = 'bzzz_mushroomsfood_pho_a', bone = 60309, pos = vec3(0.07, 0.03, 0.07), rot = vec3(-30.0, 0.0, 0.0) },
+            { model = 'bzzz_mushroomsfood_pho_b', bone = 28422, pos = vec3(0.08, 0.04, -0.05), rot = vec3(-90.0, -20.0, -10.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_mushroomsfood_risotto_a'] = {
+    ['name'] = 'bzzz_mushroomsfood_risotto_a',
+    ['label'] = 'Porcini Mushroom Risotto',
+    ['weight'] = 500,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_mushroomsfood_risotto_a.png',
+    ['unique'] = true,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Porcini Mushroom Risotto',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_mushroomsfood_risotto_a',
+    ['client'] = {
+        status = { hunger = 18000 },
+        usetime = 10500,
+        anim = { dict = 'bzzz_mushrooms_food', clip = 'bzzz_mushrooms_food' },
+        prop = {
+            { model = 'bzzz_mushroomsfood_risotto_a', bone = 60309, pos = vec3(0.07, 0.03, 0.07), rot = vec3(-30.0, 0.0, 0.0) },
+            { model = 'bzzz_mushroomsfood_risotto_b', bone = 28422, pos = vec3(0.08, 0.03, -0.06), rot = vec3(-110.0, 150.0, -30.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_mushroomsfood_soup_a'] = {
+    ['name'] = 'bzzz_mushroomsfood_soup_a',
+    ['label'] = 'Mushroom Soup',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_mushroomsfood_soup_a.png',
+    ['unique'] = true,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Mushroom Soup',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_mushroomsfood_soup_a',
+    ['client'] = {
+        status = { hunger = 13000 },
+        usetime = 10500,
+        anim = { dict = 'bzzz_mushrooms_food', clip = 'bzzz_mushrooms_food' },
+        prop = {
+            { model = 'bzzz_mushroomsfood_soup_a', bone = 60309, pos = vec3(0.07, 0.03, 0.07), rot = vec3(-30.0, 0.0, 0.0) },
+            { model = 'bzzz_mushroomsfood_soup_b', bone = 28422, pos = vec3(0.08, 0.03, -0.06), rot = vec3(-110.0, 150.0, -30.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_mushroomsfood_skewer_a'] = {
+    ['name'] = 'bzzz_mushroomsfood_skewer_a',
+    ['label'] = 'Grilled Mushroom Skewer',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_mushroomsfood_skewer_a.png',
+    ['unique'] = true,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Grilled Mushroom Skewer',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_mushroomsfood_skewer_a',
+    ['client'] = {
+        status = { hunger = 7000 },
+        usetime = 5500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_mushroomsfood_skewer_a', bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-90.0, -90.0, 0.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_mushroomsfood_taco_a'] = {
+    ['name'] = 'bzzz_mushroomsfood_taco_a',
+    ['label'] = 'Mushroom Taco',
+    ['weight'] = 350,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_mushroomsfood_taco_a.png',
+    ['unique'] = true,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Mushroom Taco',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_mushroomsfood_taco_a',
+    ['client'] = {
+        status = { hunger = 7000 },
+        usetime = 5500,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            { model = 'bzzz_mushroomsfood_taco_a', bone = 60309, pos = vec3(0.0, 0.0, 0.01), rot = vec3(0.0, 5.0, 5.0) }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true
+    }
+},
+	
