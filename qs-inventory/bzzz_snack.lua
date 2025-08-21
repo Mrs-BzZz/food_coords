83d99ed5fe167638cbfb80a40cfb7f435481ca49
BzZz <adelheid.kaszubi@gmail.com>
2025-08-21 18:44:28 +0200

    Create bzzz_snack.lua

---
+++ qs-inventory/bzzz_snack.lua
@@ -0,0 +1,264 @@
+['bzzz_croissant'] = {
+    ['name'] = 'bzzz_croissant',
+    ['label'] = 'Croissant',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_croissant.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Croissant',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_new_snacks_croissant_a',
+    ['client'] = {
+        status = { hunger = 5000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_new_snacks_croissant_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, 0.0),
+                rot = vec3(0.0, -50.0, 80.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['bzzz_donut_a'] = {
+    ['name'] = 'bzzz_donut_a',
+    ['label'] = 'Pink donut',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_donut_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Pink donut',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_new_snacks_donut_a',
+    ['client'] = {
+        status = { hunger = 5000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_new_snacks_donut_a',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, -0.02),
+                rot = vec3(0.0, -20.0, 80.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['bzzz_donut_b'] = {
+    ['name'] = 'bzzz_donut_b',
+    ['label'] = 'Chocolate donut',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_donut_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Chocolate donut',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_new_snacks_donut_b',
+    ['client'] = {
+        status = { hunger = 5000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_new_snacks_donut_b',
+                bone = 60309,
+                pos = vec3(0.0, 0.0, -0.02),
+                rot = vec3(0.0, -20.0, 80.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['bzzz_peanuts'] = {
+    ['name'] = 'bzzz_peanuts',
+    ['label'] = 'Peanuts',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_peanuts.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Peanuts',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_new_snacks_peanuts_a',
+    ['client'] = {
+        status = { hunger = 5000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_new_snacks_peanuts_a',
+                bone = 60309,
+                pos = vec3(0.0, -0.02, -0.01),
+                rot = vec3(0.0, -20.0, 80.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['bzzz_pretzels'] = {
+    ['name'] = 'bzzz_pretzels',
+    ['label'] = 'Pretzels',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_pretzels.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Pretzels',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_new_snacks_pretzels_a',
+    ['client'] = {
+        status = { hunger = 5000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+        prop = {
+            {
+                model = 'bzzz_new_snacks_pretzels_a',
+                bone = 60309,
+                pos = vec3(0.03, -0.04, -0.01),
+                rot = vec3(-80.0, 0.0, 70.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['bzzz_pepsiloca_a'] = {
+    ['name'] = 'bzzz_pepsiloca_a',
+    ['label'] = 'Pepsiloca',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_pepsiloca_a.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Pepsiloca',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_new_snacks_pepsiloca_a',
+    ['client'] = {
+        status = { hunger = 5000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            {
+                model = 'bzzz_new_snacks_pepsiloca_a',
+                bone = 60309,
+                pos = vec3(0.01 ,0.00, 0.07),
+                rot = vec3(0.0, 0.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['bzzz_pepsiloca_b'] = {
+    ['name'] = 'bzzz_pepsiloca_b',
+    ['label'] = 'Pepsiloca light',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_pepsiloca_b.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Pepsiloca light',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_new_snacks_pepsiloca_b',
+    ['client'] = {
+        status = { hunger = 5000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            {
+                model = 'bzzz_new_snacks_pepsiloca_b',
+                bone = 60309,
+                pos = vec3(0.01 ,0.00, 0.07),
+                rot = vec3(0.0, 0.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
+['bzzz_energy'] = {
+    ['name'] = 'bzzz_energy',
+    ['label'] = 'Energy drink',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_energy.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Energy drink',
+    ['created'] = nil,
+    ['decay'] = 0.02,
+    ['delete'] = false,
+    ['object'] = 'bzzz_new_snacks_energy_a',
+    ['client'] = {
+        status = { hunger = 5000 },
+        usetime = 5000,
+        anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+        prop = {
+            {
+                model = 'bzzz_new_snacks_energy_a',
+                bone = 60309,
+                pos = vec3(0.01 ,0.00, 0.05),
+                rot = vec3(0.0, 0.0, 0.0)
+            }
+        },
+        disable = { move = false, car = true, mouse = false, combat = true },
+        removeAfterUse = true,
+        notification = 'Bon appétit'
+    }
+},
+
