0b36fb21a77b7755fb925935edede572e9631c0f
BzZz <adelheid.kaszubi@gmail.com>
2026-01-21 21:07:28 +0100

    Add files via upload

---
+++ tgian_food_jobs/bzzz_beer_machine.lua
@@ -0,0 +1,107 @@
+-- Code prepared for you by: lumisaphy
+-- Any problems with the script/code must be resolved by Tgiann support (we do not have this script and have no way to help you)
+
+
+config.ConsumableItems = {
+    {
+        progressbarLabel = t("drinking"),
+        type = "drink",
+        item = "bzzz_prop_beer_original",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_intdrink",
+            animationName = "loop_bottle",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_beer_original",
+            PropBone = 60309,
+            PropPlacement = { 0.01, 0.01, 0.06, 5.0, 5.0, -180.5 },
+        }
+    },
+
+    {
+        progressbarLabel = t("drinking"),
+        type = "drink",
+        item = "bzzz_prop_beer_apple",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_intdrink",
+            animationName = "loop_bottle",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_beer_apple",
+            PropBone = 60309,
+            PropPlacement = { 0.01, 0.01, 0.06, 5.0, 5.0, -180.5 },
+        }
+    },
+
+    {
+        progressbarLabel = t("drinking"),
+        type = "drink",
+        item = "bzzz_prop_beer_blueberry",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_intdrink",
+            animationName = "loop_bottle",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_beer_blueberry",
+            PropBone = 60309,
+            PropPlacement = { 0.01, 0.01, 0.06, 5.0, 5.0, -180.5 },
+        }
+    },
+
+    {
+        progressbarLabel = t("drinking"),
+        type = "drink",
+        item = "bzzz_prop_beer_kiwi",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_intdrink",
+            animationName = "loop_bottle",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_beer_kiwi",
+            PropBone = 60309,
+            PropPlacement = { 0.01, 0.01, 0.06, 5.0, 5.0, -180.5 },
+        }
+    },
+
+    {
+        progressbarLabel = t("drinking"),
+        type = "drink",
+        item = "bzzz_prop_beer_plum",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_intdrink",
+            animationName = "loop_bottle",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_beer_plum",
+            PropBone = 60309,
+            PropPlacement = { 0.01, 0.01, 0.06, 5.0, 5.0, -180.5 },
+        }
+    },
+
+    {
+        progressbarLabel = t("drinking"),
+        type = "drink",
+        item = "bzzz_prop_beer_raspberry",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_intdrink",
+            animationName = "loop_bottle",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_beer_raspberry",
+            PropBone = 60309,
+            PropPlacement = { 0.01, 0.01, 0.06, 5.0, 5.0, -180.5 },
+        }
+    },
+}
