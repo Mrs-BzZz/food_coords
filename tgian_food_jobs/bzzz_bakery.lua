35ad2f1a3166622e6ead92fc6716ecd018ff29d3
BzZz <adelheid.kaszubi@gmail.com>
2026-01-21 21:06:48 +0100

    Create bzzz_bakery.lua

---
+++ tgian_food_jobs/bzzz_bakery.lua
@@ -0,0 +1,546 @@
+-- Code prepared for you by: lumisaphy
+-- Any problems with the script/code must be resolved by Tgiann support (we do not have this script and have no way to help you)
+
+config.consumableItems = {
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_toast_d",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_toast_d",
+            PropBone = 60309,
+            PropPlacement = { -0.01, 0.01, -0.05, 54.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_toast_c",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_toast_c",
+            PropBone = 60309,
+            PropPlacement = { 0.02, -0.01, -0.04, 48.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_toast_b",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_toast_b",
+            PropBone = 60309,
+            PropPlacement = { 0.02, -0.01, -0.04, 48.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_toast_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_toast_a",
+            PropBone = 60309,
+            PropPlacement = { 0.02, -0.01, -0.04, 48.0, 0.0, 0.0 },
+        }
+    },
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_pretzel_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_pretzel_a",
+            PropBone = 60309,
+            PropPlacement = { 0.03, -0.05, -0.04, 26.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_muffin_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_muffin_a",
+            PropBone = 60309,
+            PropPlacement = { 0.04, -0.01, -0.03, -12.0, -84.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_lemoncake_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_lemoncake_a",
+            PropBone = 60309,
+            PropPlacement = { 0.01, -0.04, -0.03, 104.0, 50.0, -18.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_kaiserroll_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_kaiserroll_a",
+            PropBone = 60309,
+            PropPlacement = { -0.01, 0.01, -0.05, 54.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_hdroll_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_hdroll_a",
+            PropBone = 60309,
+            PropPlacement = { 0.00, 0.00, -0.05, 56.0, 12.0, -14.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_hdbuns_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_hdbuns_a",
+            PropBone = 60309,
+            PropPlacement = { 0.00, 0.00, 0.00, 0.0, 0.0, 0.0 },
+        }
+    },
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_doughnut_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_doughnut_a",
+            PropBone = 60309,
+            PropPlacement = { 0.00, 0.01, -0.02, 28.0, 20.0, -34.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_donut_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_donut_a",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.02, -0.04, 46.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_donut_b",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_donut_b",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.02, -0.04, 46.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_donut_c",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_donut_c",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.02, -0.04, 46.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_donut_d",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_donut_d",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.02, -0.04, 46.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_donut_e",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_donut_e",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.02, -0.04, 46.0, 0.0, 0.0 },
+        }
+    },
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_donut_f",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_donut_f",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.02, -0.04, 46.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_donut_g",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_donut_g",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.02, -0.04, 46.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_danish_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_danish_a",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.02, -0.04, 38.0, -8.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_croissant_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_croissant_a",
+            PropBone = 60309,
+            PropPlacement = { 0.03, 0.01, -0.05, 54.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_bread_d",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_bread_d",
+            PropBone = 60309,
+            PropPlacement = { 0.02, -0.01, -0.03, 38.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_bread_c",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_bread_c",
+            PropBone = 60309,
+            PropPlacement = { 0.02, -0.01, -0.03, 38.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_bread_b",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_bread_b",
+            PropBone = 60309,
+            PropPlacement = { 0.02, -0.01, -0.03, 38.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_bread_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_bread_a",
+            PropBone = 60309,
+            PropPlacement = { 0.09, -0.01, -0.10, 78.0, -64.0, 12.0 },
+        }
+    },
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_biscuit_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_biscuit_a",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.01, -0.02, 22.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_bananabread_c",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_bananabread_c",
+            PropBone = 60309,
+            PropPlacement = { 0.00, 0.00, 0.00, 32.0, -32.0, 50.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_bananabread_b",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_bananabread_b",
+            PropBone = 60309,
+            PropPlacement = { 0.00, 0.00, 0.00, 32.0, -32.0, 50.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_bananabread_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_bananabread_a",
+            PropBone = 60309,
+            PropPlacement = { 0.03, 0.00, -0.06, 44.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_baguette_c",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_baguette_c",
+            PropBone = 60309,
+            PropPlacement = { 0.09, 0.01, -0.03, 40.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_baguette_b",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_baguette_b",
+            PropBone = 60309,
+            PropPlacement = { 0.09, 0.01, -0.03, 40.0, 0.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_baguette_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_baguette_a",
+            PropBone = 60309,
+            PropPlacement = { 0.19, 0.00, -0.03, -4.0, 8.0, 0.0 },
+        }
+    },
+
+    {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_food_bakery_bagel_a",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_food_bakery_bagel_a",
+            PropBone = 60309,
+            PropPlacement = { -0.01, -0.02, -0.04, 46.0, 0.0, 0.0 },
+        }
+    },
+}
+
