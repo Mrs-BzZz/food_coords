c5f9deea630331987a0f842beaf8626f581d7f2f
BzZz <adelheid.kaszubi@gmail.com>
2026-05-31 22:19:18 +0200

    Create bzzz_pickles.lua

---
+++ bzzz_usableitems/bzzz_pickles.lua
@@ -0,0 +1,65 @@
+-- FOOD
+-- SQL: https://github.com/Mrs-BzZz/food_coords/blob/main/SQL/bzzz_pickles.sql
+-- Copy and paste into bzzz_usableitems and then restart the server
+
+
+
+["bzzz_prop_pickle_a2"] = {
+    basicInfo = {
+        amountToAdd = 50000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_prop_pickle_a2", 
+        xPos = 0.02, yPos = 0.01, zPos = -0.03, xRot = 10.0, yRot = 0.0, zRot = 90.0}
+},
+
+["bzzz_prop_pickle_b2"] = {
+    basicInfo = {
+        amountToAdd = 50000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_prop_pickle_b2", 
+        xPos = 0.02, yPos = 0.01, zPos = -0.03, xRot = 10.0, yRot = 0.0, zRot = 90.0}
+},
+
+["bzzz_prop_pickle_c2"] = {
+    basicInfo = {
+        amountToAdd = 50000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_prop_pickle_c2", 
+        xPos = 0.02, yPos = 0.01, zPos = -0.03, xRot = 10.0, yRot = 0.0, zRot = 90.0}
+},
+
+["bzzz_prop_pickle_d2"] = {
+    basicInfo = {
+        amountToAdd = 50000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_prop_pickle_d2", 
+        xPos = 0.02, yPos = 0.01, zPos = -0.03, xRot = 10.0, yRot = 0.0, zRot = 90.0}
+},
+
+["bzzz_prop_pickle_e2"] = {
+    basicInfo = {
+        amountToAdd = 50000,
+        animationDict = "mp_player_inteat@burger",
+        animationName = "mp_player_int_eat_burger"
+    },
+    firstProp = {
+        boneIndex = 60309, 
+        propName = "bzzz_prop_pickle_e2", 
+        xPos = 0.02, yPos = 0.01, zPos = -0.03, xRot = 10.0, yRot = 0.0, zRot = 90.0}
+},
