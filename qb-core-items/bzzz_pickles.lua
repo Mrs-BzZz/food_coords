7eb152c30f4b21725273a03ce8e3d9d6bcabf4e3
BzZz <adelheid.kaszubi@gmail.com>
2026-05-31 21:52:00 +0200

    Create bzzz_pickles.lua

---
+++ qb-core-items/bzzz_pickles.lua
@@ -0,0 +1,70 @@
+-- Register items to qb-core/shared/items.lua
+-- pickles
+
+bzzz_prop_pickle_a2 = { 
+    name = 'bzzz_prop_pickle_a2', 
+    label = 'Classic Dill Pickle', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_pickle_a2.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+bzzz_prop_pickle_b2 = { 
+    name = 'bzzz_prop_pickle_b2', 
+    label = 'Garlic Crunch', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_pickle_b2.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+
+bzzz_prop_pickle_c2 = { 
+    name = 'bzzz_prop_pickle_c2', 
+    label = 'Hot Fire Pickle', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_pickle_c2.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+
+bzzz_prop_pickle_d2 = { 
+    name = 'bzzz_prop_pickle_d2', 
+    label = 'Smoky BBQ Pickle', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_pickle_d2.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
+
+
+bzzz_prop_pickle_e2 = { 
+    name = 'bzzz_prop_pickle_e2', 
+    label = 'Sweet Honey Pickle', 
+    weight = 200, 
+    type = 'item', 
+    image = 'bzzz_prop_pickle_e2.png',  
+    unique = false, 
+    useable = true, 
+    shouldClose = true, 
+    combinable = nil, 
+    description = ' '  
+},
