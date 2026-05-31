65f5bdc0714f728717774edec172c9fa0818e4fa
BzZz <adelheid.kaszubi@gmail.com>
2026-05-31 16:26:57 +0200

    Create items.lua
    MRE

---
+++ bzzz_mre/ox_inventory/items.lua
@@ -0,0 +1,169 @@
+-- food items for ox_inventory
+
+['bzzz_mre_bar'] = {
+		label = 'MRE Energy bar',
+		weight = 50,
+		client = {
+			status = { hunger = 20000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_mre_bar`, bone = 60309, pos = vec3(-0.01, -0.01, -0.01), rot = vec3(14.0, 12.0, -90.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_cookie'] = {
+		label = 'MRE Cookie',
+		weight = 100,
+		client = {
+			status = { hunger = 50000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_mre_cookie`, bone = 60309, pos = vec3(-0.01, -0.01, -0.01), rot = vec3(-28.0, -1.24, -154.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_crackers'] = {
+		label = 'MRE Crackers',
+		weight = 100,
+		client = {
+			status = { hunger = 50000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_mre_crackers`, bone = 60309, pos = vec3(-0.01, -0.01, -0.01), rot = vec3(-28.0, -1.24, -154.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_a'] = {
+		label = 'Spaghetti with meat sauce',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_a`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_b'] = {
+		label = 'Chicken Tikka Masala',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_b`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_c'] = {
+		label = 'Beef Stroganoff with Noodles',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_c`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_d'] = {
+		label = 'Lentil Stew with Vegetables',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_d`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_e'] = {
+		label = 'BBQ pulled pork with beans',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_e`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_f'] = {
+		label = 'Chili Macaroni',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_f`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_g'] = {
+		label = 'Teriyaki chicken with rice',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_g`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_h'] = {
+		label = 'Beef Enchilada in red sauce',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_h`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_i'] = {
+		label = 'Cheese Tortellini in tomato sauce',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_i`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_food_j'] = {
+		label = 'Meatballs in Marinara sauce',
+		weight = 100,
+		client = {
+			status = { hunger = 100000 },
+			anim = { dict = 'mp_player_inteat@pnq', clip = 'loop' },
+			prop = { model = `bzzz_mre_food_j`, bone = 60309, pos = vec3(-0.02, 0.03, -0.01), rot = vec3(5.0, 6.0, -80.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_mre_tortillas'] = {
+		label = 'MRE Tortillas',
+		weight = 100,
+		client = {
+			status = { hunger = 60000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_mre_tortillas`, bone = 60309, pos = vec3(0.0, 0.0, 0.0), rot = vec3(0.0, 30.0, -90.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
