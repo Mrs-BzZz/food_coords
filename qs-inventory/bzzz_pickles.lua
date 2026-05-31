9b12ed0039f78e78737098dd968f5cf3aac5d95f
BzZz <adelheid.kaszubi@gmail.com>
2026-05-31 22:03:55 +0200

    Create bzzz_pickles.lua

---
+++ qs-inventory/bzzz_pickles.lua
@@ -0,0 +1,199 @@
+['bzzz_prop_pickle_a2']                 = {
+    ['name'] = 'bzzz_prop_pickle_a2',
+    ['label'] = 'Classic Dill Pickle',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_pickle_a2.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Classic Dill Pickle',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_pickle_a2',
+    ['client'] = {
+        status = { hunger = 2000 },
+        usetime = 7500,
+        anim = {
+            dict = 'mp_player_inteat@burger',
+            clip = 'mp_player_int_eat_burger'
+        },
+        prop = {
+            {
+                model = 'bzzz_prop_pickle_a2',
+                pos = vec3(0.02, 0.01, -0.03),
+                rot = vec3(10.0, 0.0, 90.0),
+                bone = 60309
+            }
+        },
+        disable = {
+            move = false,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_prop_pickle_b2']                 = {
+    ['name'] = 'bzzz_prop_pickle_b2',
+    ['label'] = 'Garlic Crunch',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_pickle_b2.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Garlic Crunch',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_pickle_b2',
+    ['client'] = {
+        status = { hunger = 2000 },
+        usetime = 7500,
+        anim = {
+            dict = 'mp_player_inteat@burger',
+            clip = 'mp_player_int_eat_burger'
+        },
+        prop = {
+            {
+                model = 'bzzz_prop_pickle_b2',
+                pos = vec3(0.02, 0.01, -0.03),
+                rot = vec3(10.0, 0.0, 90.0),
+                bone = 60309
+            }
+        },
+        disable = {
+            move = false,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_prop_pickle_c2']                 = {
+    ['name'] = 'bzzz_prop_pickle_c2',
+    ['label'] = 'Hot Fire Pickle',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_pickle_c2.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Hot Fire Pickle',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_pickle_c2',
+    ['client'] = {
+        status = { hunger = 2000 },
+        usetime = 7500,
+        anim = {
+            dict = 'mp_player_inteat@burger',
+            clip = 'mp_player_int_eat_burger'
+        },
+        prop = {
+            {
+                model = 'bzzz_prop_pickle_c2',
+                pos = vec3(0.02, 0.01, -0.03),
+                rot = vec3(10.0, 0.0, 90.0),
+                bone = 60309
+            }
+        },
+        disable = {
+            move = false,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_prop_pickle_d2']                 = {
+    ['name'] = 'bzzz_prop_pickle_d2',
+    ['label'] = 'Smoky BBQ Pickle',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_pickle_d2.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Smoky BBQ Pickle',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_pickle_d2',
+    ['client'] = {
+        status = { hunger = 2000 },
+        usetime = 7500,
+        anim = {
+            dict = 'mp_player_inteat@burger',
+            clip = 'mp_player_int_eat_burger'
+        },
+        prop = {
+            {
+                model = 'bzzz_prop_pickle_d2',
+                pos = vec3(0.02, 0.01, -0.03),
+                rot = vec3(10.0, 0.0, 90.0),
+                bone = 60309
+            }
+        },
+        disable = {
+            move = false,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        removeAfterUse = true
+    }
+},
+
+['bzzz_prop_pickle_e2']                 = {
+    ['name'] = 'bzzz_prop_pickle_e2',
+    ['label'] = 'Sweet Honey Pickle',
+    ['weight'] = 200,
+    ['type'] = 'item',
+    ['image'] = 'bzzz_prop_pickle_e2.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Sweet Honey Pickle',
+    ['created'] = nil,
+    ['decay'] = 0.10,
+    ['delete'] = false,
+    ['object'] = 'bzzz_prop_pickle_e2',
+    ['client'] = {
+        status = { hunger = 2000 },
+        usetime = 7500,
+        anim = {
+            dict = 'mp_player_inteat@burger',
+            clip = 'mp_player_int_eat_burger'
+        },
+        prop = {
+            {
+                model = 'bzzz_prop_pickle_e2',
+                pos = vec3(0.02, 0.01, -0.03),
+                rot = vec3(10.0, 0.0, 90.0),
+                bone = 60309
+            }
+        },
+        disable = {
+            move = false,
+            car = true,
+            mouse = false,
+            combat = true,
+        },
+        removeAfterUse = true
+    }
+},
