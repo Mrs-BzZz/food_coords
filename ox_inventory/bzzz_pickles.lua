b8603082fcf8ee5d80ea6d990290fcf428096ef6
BzZz <adelheid.kaszubi@gmail.com>
2026-05-31 21:47:56 +0200

    Create bzzz_pickles.lua

---
+++ ox_inventory/bzzz_pickles.lua
@@ -0,0 +1,59 @@
+['bzzz_prop_pickle_a2'] = {
+		label = 'Classic Dill Pickle',
+		weight = 200,
+		client = {
+			status = { hunger = 30000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_prop_pickle_a2`, bone = 60309, pos = vec3(0.02, 0.01, -0.03), rot = vec3(10.0, 0.0, 90.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_prop_pickle_b2'] = {
+		label = 'Garlic Crunch',
+		weight = 200,
+		client = {
+			status = { hunger = 30000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_prop_pickle_b2`, bone = 60309, pos = vec3(0.02, 0.01, -0.03), rot = vec3(10.0, 0.0, 90.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_prop_pickle_c2'] = {
+		label = 'Hot Fire Pickle',
+		weight = 200,
+		client = {
+			status = { hunger = 30000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_prop_pickle_c2`, bone = 60309, pos = vec3(0.02, 0.01, -0.03), rot = vec3(10.0, 0.0, 90.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_prop_pickle_d2'] = {
+		label = 'Smoky BBQ Pickle',
+		weight = 200,
+		client = {
+			status = { hunger = 30000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_prop_pickle_d2`, bone = 60309, pos = vec3(0.02, 0.01, -0.03), rot = vec3(10.0, 0.0, 90.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
+
+['bzzz_prop_pickle_e2'] = {
+		label = 'Sweet Honey Pickle',
+		weight = 200,
+		client = {
+			status = { hunger = 30000 },
+			anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger' },
+			prop = { model = `bzzz_prop_pickle_e2`, bone = 60309, pos = vec3(0.02, 0.01, -0.03), rot = vec3(10.0, 0.0, 90.0) },
+			usetime = 5000, 
+			notification = 'Bon appétit!'
+		}
+	},
