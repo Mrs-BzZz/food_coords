b7113ba004da0842a4d5307541b430cb8688a204
BzZz <adelheid.kaszubi@gmail.com>
2026-05-31 22:05:39 +0200

    Create bzzz_pickles.lua

---
+++ tgian_food_jobs/bzzz_pickles.lua
@@ -0,0 +1,84 @@
+   {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_prop_pickle_a2",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_pickle_a2",
+            PropBone = 60309,
+            PropPlacement = {0.02, 0.01, -0.03, 10.0, 0.0, 90.0 },
+        }
+    },
+	
+	   {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_prop_pickle_b2",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_pickle_b2",
+            PropBone = 60309,
+            PropPlacement = {0.02, 0.01, -0.03, 10.0, 0.0, 90.0 },
+        }
+    },
+	
+	   {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_prop_pickle_c2",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_pickle_c2",
+            PropBone = 60309,
+            PropPlacement = {0.02, 0.01, -0.03, 10.0, 0.0, 90.0 },
+        }
+    },
+	
+	   {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_prop_pickle_d2",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_pickle_d2",
+            PropBone = 60309,
+            PropPlacement = {0.02, 0.01, -0.03, 10.0, 0.0, 90.0 },
+        }
+    },
+	
+	   {
+        progressbarLabel = t("eating"),
+        type = "food",
+        item = "bzzz_prop_pickle_e2",
+        time = 30000,
+        addValue = 100,
+        animation = {
+            animDictionary = "mp_player_inteat@burger",
+            animationName = "mp_player_int_eat_burger",
+        },
+        propOptions = {
+            Prop = "bzzz_prop_pickle_e2",
+            PropBone = 60309,
+            PropPlacement = {0.02, 0.01, -0.03, 10.0, 0.0, 90.0 },
+        }
+    },
