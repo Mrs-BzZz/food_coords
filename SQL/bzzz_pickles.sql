8eb7b858eca2fb126c8f9277a6a357a144f51dd5
BzZz <adelheid.kaszubi@gmail.com>
2026-05-31 22:11:23 +0200

    Create bzzz_pickles.sql

---
+++ SQL/bzzz_pickles.sql
@@ -0,0 +1,7 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+
+	('bzzz_prop_pickle_a2', 'Classic Dill Pickle', 1, 0, 1),
+	('bzzz_prop_pickle_b2', 'Garlic Crunch', 1, 0, 1),
+    ('bzzz_prop_pickle_c2', 'Hot Fire Pickle', 1, 0, 1),
+	('bzzz_prop_pickle_d2', 'Smoky BBQ Pickle', 1, 0, 1),
+	('bzzz_prop_pickle_e2', 'Sweet Honey Pickle', 1, 0, 1);
