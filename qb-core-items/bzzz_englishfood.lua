65671947f543c34687e293668ee7440f88e73b5d
BzZz <adelheid.kaszubi@gmail.com>
2026-09-10 00:26:50 +0200

    Create bzzz_englishfood.lua

---
+++ qb-core-items/bzzz_englishfood.lua
@@ -0,0 +1,417 @@
+-- Register items to qb-core/shared/items.lua
+
+bzzz_englishfood_fishchips_b = {
+    name = 'bzzz_englishfood_fishchips_b',
+    label = 'Fish & chips',
+    weight = 500,
+    type = 'item',
+    image = 'bzzz_englishfood_fishchips_b.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Fish & chips in box'
+},
+
+bzzz_englishfood_haggis_a = {
+    name = 'bzzz_englishfood_haggis_a',
+    label = 'Haggis with mashed potato',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_haggis_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Haggis with mashed potato'
+},
+
+bzzz_englishfood_tikkamasala_a = {
+    name = 'bzzz_englishfood_tikkamasala_a',
+    label = 'Chicken Tikka Masala',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_tikkamasala_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Chicken Tikka Masala'
+},
+
+bzzz_englishfood_gammon_a = {
+    name = 'bzzz_englishfood_gammon_a',
+    label = 'Gammon & Chips',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_gammon_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Gammon & Chips'
+},
+
+bzzz_englishfood_piemash_a = {
+    name = 'bzzz_englishfood_piemash_a',
+    label = 'Pie & Mash',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_piemash_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Pie & Mash'
+},
+
+bzzz_englishfood_roastbeef_a = {
+    name = 'bzzz_englishfood_roastbeef_a',
+    label = 'Roast Beef & Yorkshire Pudding',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_roastbeef_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Roast Beef & Yorkshire Pudding'
+},
+
+bzzz_englishfood_breakfast_a = {
+    name = 'bzzz_englishfood_breakfast_a',
+    label = 'English Breakfast',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_breakfast_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'English Breakfast'
+},
+
+bzzz_englishfood_beans_a = {
+    name = 'bzzz_englishfood_beans_a',
+    label = 'Beans on Toast',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_beans_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Beans on Toast'
+},
+
+bzzz_englishfood_fishchips_a = {
+    name = 'bzzz_englishfood_fishchips_a',
+    label = 'Fish & Chips',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_fishchips_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Fish & Chips'
+},
+
+bzzz_englishfood_bangersmash_a = {
+    name = 'bzzz_englishfood_bangersmash_a',
+    label = 'Bangers and Mash',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_bangersmash_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Bangers and Mash'
+},
+
+bzzz_englishfood_shepherdspie_a = {
+    name = 'bzzz_englishfood_shepherdspie_a',
+    label = 'Shepherd\'s Pie',
+    weight = 1000,
+    type = 'item',
+    image = 'bzzz_englishfood_shepherdspie_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Shepherd\'s Pie'
+},
+
+bzzz_englishfood_porkpie_a = {
+    name = 'bzzz_englishfood_porkpie_a',
+    label = 'Pork Pie',
+    weight = 700,
+    type = 'item',
+    image = 'bzzz_englishfood_porkpie_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Pork Pie'
+},
+
+bzzz_englishfood_sausageroll_a = {
+    name = 'bzzz_englishfood_sausageroll_a',
+    label = 'Sausage Roll',
+    weight = 700,
+    type = 'item',
+    image = 'bzzz_englishfood_sausageroll_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Sausage Roll'
+},
+
+bzzz_englishfood_sandwich_a = {
+    name = 'bzzz_englishfood_sandwich_a',
+    label = 'Bacon Sandwich',
+    weight = 500,
+    type = 'item',
+    image = 'bzzz_englishfood_sandwich_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Bacon Sandwich'
+},
+
+bzzz_englishfood_sandwich_c = {
+    name = 'bzzz_englishfood_sandwich_c',
+    label = 'Sausage Sandwich',
+    weight = 500,
+    type = 'item',
+    image = 'bzzz_englishfood_sandwich_c.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Sausage Sandwich'
+},
+
+bzzz_englishfood_toffee_a = {
+    name = 'bzzz_englishfood_toffee_a',
+    label = 'Sticky Toffee Pudding',
+    weight = 400,
+    type = 'item',
+    image = 'bzzz_englishfood_toffee_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Sticky Toffee Pudding'
+},
+
+bzzz_englishfood_sponge_a = {
+    name = 'bzzz_englishfood_sponge_a',
+    label = 'Victoria Sponge',
+    weight = 400,
+    type = 'item',
+    image = 'bzzz_englishfood_sponge_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Victoria Sponge'
+},
+
+bzzz_englishfood_scone_a = {
+    name = 'bzzz_englishfood_scone_a',
+    label = 'Scone',
+    weight = 400,
+    type = 'item',
+    image = 'bzzz_englishfood_scone_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Scone'
+},
+
+bzzz_englishfood_shortbread_a = {
+    name = 'bzzz_englishfood_shortbread_a',
+    label = 'Shortbread',
+    weight = 200,
+    type = 'item',
+    image = 'bzzz_englishfood_shortbread_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Shortbread'
+},
+
+bzzz_englishfood_jammiedodger_a = {
+    name = 'bzzz_englishfood_jammiedodger_a',
+    label = 'Jammie Dodger',
+    weight = 200,
+    type = 'item',
+    image = 'bzzz_englishfood_jammiedodger_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Jammie Dodger'
+},
+
+bzzz_englishfood_digestive_a = {
+    name = 'bzzz_englishfood_digestive_a',
+    label = 'Digestive Biscuits',
+    weight = 200,
+    type = 'item',
+    image = 'bzzz_englishfood_digestive_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Digestive Biscuits'
+},
+
+bzzz_englishfood_chips_a = {
+    name = 'bzzz_englishfood_chips_a',
+    label = 'Chips - Ready Salted',
+    weight = 100,
+    type = 'item',
+    image = 'bzzz_englishfood_chips_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Chips - Ready Salted'
+},
+
+bzzz_englishfood_chips_b = {
+    name = 'bzzz_englishfood_chips_b',
+    label = 'Chips - Cheese & Onion',
+    weight = 100,
+    type = 'item',
+    image = 'bzzz_englishfood_chips_b.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Chips - Cheese & Onion'
+},
+
+bzzz_englishfood_chips_c = {
+    name = 'bzzz_englishfood_chips_c',
+    label = 'Chips - Salt & Vinegar',
+    weight = 100,
+    type = 'item',
+    image = 'bzzz_englishfood_chips_c.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Chips - Salt & Vinegar'
+},
+
+bzzz_englishfood_applecrumble_a = {
+    name = 'bzzz_englishfood_applecrumble_a',
+    label = 'Apple Crumble',
+    weight = 400,
+    type = 'item',
+    image = 'bzzz_englishfood_applecrumble_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Apple Crumble'
+},
+
+bzzz_englishfood_cornishpasty_a = {
+    name = 'bzzz_englishfood_cornishpasty_a',
+    label = 'Cornish Pasty',
+    weight = 200,
+    type = 'item',
+    image = 'bzzz_englishfood_cornishpasty_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Cornish Pasty'
+},
+
+bzzz_englishfood_chocolate_b = {
+    name = 'bzzz_englishfood_chocolate_b',
+    label = 'KrisKat',
+    weight = 60,
+    type = 'item',
+    image = 'bzzz_englishfood_chocolate_b.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'KrisKat'
+},
+
+bzzz_englishfood_chocolate_a = {
+    name = 'bzzz_englishfood_chocolate_a',
+    label = 'Chocolate',
+    weight = 60,
+    type = 'item',
+    image = 'bzzz_englishfood_chocolate_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Chocolate'
+},
+
+bzzz_englishfood_beer_a = {
+    name = 'bzzz_englishfood_beer_a',
+    label = 'Beer',
+    weight = 300,
+    type = 'item',
+    image = 'bzzz_englishfood_beer_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Beer'
+},
+
+bzzz_englishfood_ironix_a = {
+    name = 'bzzz_englishfood_ironix_a',
+    label = 'Ironix',
+    weight = 150,
+    type = 'item',
+    image = 'bzzz_englishfood_ironix_a.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Ironix'
+},
+
+bzzz_englishfood_ironix_b = {
+    name = 'bzzz_englishfood_ironix_b',
+    label = 'Ironix Zero',
+    weight = 150,
+    type = 'item',
+    image = 'bzzz_englishfood_ironix_b.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Ironix Zero'
+},
+
+bzzz_englishfood_tea_b = {
+    name = 'bzzz_englishfood_tea_b',
+    label = 'English Tea',
+    weight = 150,
+    type = 'item',
+    image = 'bzzz_englishfood_tea_b.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'English Tea'
+},
